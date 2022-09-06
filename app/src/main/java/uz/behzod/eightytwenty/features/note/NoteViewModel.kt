2df37b6e4511c10f6edde772d6ca571af7e4af39
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:29:34 +0900

    [feat::] Update NoteViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewModel.kt
@@ -1,4 +1,45 @@
 package uz.behzod.eightytwenty.features.note
 
-class NoteViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteViewModel @Inject constructor() : ViewModel() {
+
+    private var _viewEffect = Channel<NoteViewEffect>(Channel.BUFFERED)
+    val viewEffect: Flow<NoteViewEffect> = _viewEffect.receiveAsFlow()
+
+    fun onEvent(event: NoteEvent) {
+        when(event) {
+            NoteEvent.CategoryEvent -> observeEffects(NoteViewEffect.CategoryViewEffect)
+            NoteEvent.DetailEvent -> observeEffects(NoteViewEffect.DetailViewEffect)
+            NoteEvent.NewNoteEvent -> observeEffects(NoteViewEffect.NewNoteViewEffect)
+            NoteEvent.SearchEvent -> observeEffects(NoteViewEffect.DetailViewEffect)
+        }
+    }
+
+    private fun observeEffects(effect: NoteViewEffect) {
+        viewModelScope.launch {
+            when(effect) {
+                NoteViewEffect.CategoryViewEffect -> {
+                    _viewEffect.trySend(NoteViewEffect.CategoryViewEffect)
+                }
+                NoteViewEffect.DetailViewEffect -> {
+                    _viewEffect.trySend(NoteViewEffect.DetailViewEffect)
+                }
+                NoteViewEffect.NewNoteViewEffect -> {
+                    _viewEffect.trySend(NoteViewEffect.NewNoteViewEffect)
+                }
+                NoteViewEffect.SearchViewEffect -> {
+                    _viewEffect.trySend(NoteViewEffect.SearchViewEffect)
+                }
+            }
+        }
+    }
 }
