e2d263848c4c97e9a5e720681270133a9d907cab
Behod Halil <behzoddev@gmail.com>
2022-09-06 21:21:10 +0900

    [feat::] Update NoteCategoryDaoTest.kt

--- app/src/androidTest/java/uz/behzod/eightytwenty/NoteCategoryDaoTest.kt
+++ app/src/androidTest/java/uz/behzod/eightytwenty/NoteCategoryDaoTest.kt
@@ -1,21 +1,25 @@
 package uz.behzod.eightytwenty
 
 import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.asLiveData
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import kotlinx.coroutines.runBlocking
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 import uz.behzod.eightytwenty.data.local.dao.NoteCategoryDao
 import uz.behzod.eightytwenty.data.local.db.EightyTwentyDatabase
+import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class NoteCategoryDaoTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var database: EightyTwentyDatabase
     private lateinit var categoryDao: NoteCategoryDao
@@ -26,7 +30,9 @@ class NoteCategoryDaoTest {
         database = Room.inMemoryDatabaseBuilder(
             context,
             EightyTwentyDatabase::class.java
-        ).build()
+        )
+            .allowMainThreadQueries()
+            .build()
         categoryDao = database.getNoteCategoryDao()
     }
 
@@ -39,26 +45,56 @@ class NoteCategoryDaoTest {
     fun insertAndGetCategories() = runBlocking {
         val entity = createNoteCategory()
         categoryDao.insert(entity)
-        val categories = categoryDao.fetchAllCategories()
+        val categories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        Assert.assertEquals(1, categories.size)
     }
 
     @Test
     fun updateAndGetCategories() = runBlocking {
+        val entity = createNoteCategory()
+        categoryDao.insert(entity)
+        val categories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        categories.forEach {
+            Assert.assertEquals("All", it.name)
+        }
+        val expected = NoteCategoryEntity(1, "Unread", 1)
+        categoryDao.update(expected)
+        val updatedCategories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        updatedCategories.forEach {
+            Assert.assertEquals("Unread", it.name)
+        }
 
     }
 
     @Test
     fun incrementNoteAndGetCategories() = runBlocking {
-
+        val entity = NoteCategoryEntity(1, "All", 0)
+        categoryDao.insert(entity)
+        categoryDao.incrementNoteCount(1)
+        val categories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        categories.forEach {
+            Assert.assertEquals(1, it.count)
+        }
     }
 
     @Test
     fun decrementNoteAndGetCategories() = runBlocking {
+        val entity = NoteCategoryEntity(1, "All", 1)
+        categoryDao.insert(entity)
+        categoryDao.decrementNoteCount(1)
 
+        val categories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        categories.forEach {
+            Assert.assertEquals(0, it.count)
+        }
     }
 
     @Test
     fun fetchAllCategories() = runBlocking {
+        val entity = createNoteCategory()
+        categoryDao.insert(entity)
 
+        val categories = categoryDao.fetchAllCategories().asLiveData().getOrAwaitValue()
+        Assert.assertEquals(1, categories.size)
     }
 }
