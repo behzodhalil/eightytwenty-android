76028e81bd2c557fa39f0a0e00ccbcbb93b02ba9
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:29:25 +0900

    [feat::] Update NoteFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note/NoteFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/NoteFragment.kt
@@ -1,4 +1,35 @@
 package uz.behzod.eightytwenty.features.note
 
-class NoteFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.databinding.FragmentNoteBinding
+
+@AndroidEntryPoint
+class NoteFragment : Fragment() {
+
+    private var _binding: FragmentNoteBinding? = null
+    private val binding: FragmentNoteBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNoteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
