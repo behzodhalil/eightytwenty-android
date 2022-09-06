421df28add531f15b4a3741691022987567f5d5f
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:29:06 +0900

    [feat::] Add NoteCategoryEntity.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/NoteCategoryEntity.kt
@@ -0,0 +1,33 @@
+package uz.behzod.eightytwenty.data.local.entities
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import uz.behzod.eightytwenty.utils.ext.Empty
+import uz.behzod.eightytwenty.utils.ext.Zero
+
+@Entity(
+    tableName = "note_category_table",
+    indices = [Index(value = ["note_category_name"], unique = true)]
+)
+@Parcelize
+data class NoteCategoryEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = Schema.ID)
+    val id: Long = Long.Zero,
+
+    @ColumnInfo(name = Schema.NAME)
+    val name: String = String.Empty,
+
+    @ColumnInfo(name = Schema.COUNT)
+    val count: Int = Int.Zero
+): Parcelable {
+    private object Schema {
+        const val ID = "note_category_id"
+        const val NAME = "note_category_name"
+        const val COUNT = "note_count"
+    }
+}
