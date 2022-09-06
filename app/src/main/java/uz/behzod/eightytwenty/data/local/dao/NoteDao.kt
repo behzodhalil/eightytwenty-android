d2064bbc6af285fb7b46e45b198d9287ec3ef2ea
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:29:09 +0900

    [feat::] Add NoteDao.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/dao/NoteDao.kt
@@ -0,0 +1,25 @@
+package uz.behzod.eightytwenty.data.local.dao
+
+import androidx.room.*
+import uz.behzod.eightytwenty.data.local.entities.NoteEntity
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: NoteEntity)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note: NoteEntity)
+
+    @Delete
+    suspend fun delete(note: NoteEntity)
+
+    @Transaction
+    @Query("SELECT * FROM note_table WHERE note_is_trashed = 1")
+    suspend fun fetchTrashedNotes(): List<NoteEntity>
+
+    @Transaction
+    @Query("SELECT * FROM note_table WHERE note_is_trashed <> 1")
+    suspend fun fetchAllNotes(): List<NoteEntity>
+}
