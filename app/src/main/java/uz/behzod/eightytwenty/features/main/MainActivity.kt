9b0b41ac1ffd30383506098b43c8ce776b399726
Behod Halil <behzoddev@gmail.com>
2022-09-06 13:10:12 +0900

    [feat::] Update MainActivity

--- app/src/main/java/uz/behzod/eightytwenty/features/main/MainActivity.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/main/MainActivity.kt
@@ -1,9 +1,13 @@
-package uz.behzod.eightytwenty
+package uz.behzod.eightytwenty.features.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
