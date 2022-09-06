fea4ca1f7aab13937de2a1f9370431b50da45321
Behod Halil <behzoddev@gmail.com>
2022-09-06 21:19:31 +0900

    [feat::] Update TestEntities.kt

--- app/src/androidTest/java/uz/behzod/eightytwenty/TestEntities.kt
+++ app/src/androidTest/java/uz/behzod/eightytwenty/TestEntities.kt
@@ -1,11 +1,35 @@
 package uz.behzod.eightytwenty
 
 import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
+import uz.behzod.eightytwenty.data.local.entities.NoteEntity
+import java.time.ZonedDateTime
 
 fun createNoteCategory(): NoteCategoryEntity {
     return NoteCategoryEntity(
         id = 1,
         name = "All",
         count = 1
+    )
+}
+
+fun createNote(): NoteEntity {
+    return NoteEntity(
+        id = 1,
+        title = "Google Manager",
+        description = "Using google with map",
+        timestamp = ZonedDateTime.now(),
+        isTrashed = false,
+        categoryId = 1
+    )
+}
+
+fun createTrashedNote(): NoteEntity {
+    return NoteEntity(
+        id = 1,
+        title = "Google Manager",
+        description = "Using google with map",
+        timestamp = ZonedDateTime.now(),
+        isTrashed = true,
+        categoryId = 1
     )
 }
