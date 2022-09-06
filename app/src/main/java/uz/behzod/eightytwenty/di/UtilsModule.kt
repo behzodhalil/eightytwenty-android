bd2156812b2b4488594627ed2dadf8d7f38af5df
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:29:40 +0900

    [feat::] Update UtilsModule.kt

--- app/src/main/java/uz/behzod/eightytwenty/di/UtilsModule.kt
+++ app/src/main/java/uz/behzod/eightytwenty/di/UtilsModule.kt
@@ -4,6 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import uz.behzod.eightytwenty.utils.providers.DispatcherProvider
+import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import uz.behzod.eightytwenty.utils.providers.StringProvider
 import uz.behzod.eightytwenty.utils.providers.StringProviderImpl
 
@@ -15,4 +17,9 @@ interface UtilsModule {
     fun providesStringProvider(
         provider: StringProviderImpl
     ): StringProvider
+
+    @Binds
+    fun providesDispatcherProvider(
+        provider: DispatcherProvider
+    ): IDispatcherProvider
 }
