618202b85528c9f0e08c988cb5f93c8035efa8cb
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:28:38 +0900

    [feat::] Add DispatcherProvider.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/providers/DispatcherProvider.kt
@@ -0,0 +1,22 @@
+package uz.behzod.eightytwenty.utils.providers
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+interface IDispatcherProvider {
+    val io: CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
+
+class DispatcherProvider: IDispatcherProvider {
+    override val io: CoroutineDispatcher
+        get() = Dispatchers.IO
+    override val main: CoroutineDispatcher
+        get() = Dispatchers.Main
+    override val default: CoroutineDispatcher
+        get() = Dispatchers.Default
+    override val unconfined: CoroutineDispatcher
+        get() = Dispatchers.Unconfined
+}
