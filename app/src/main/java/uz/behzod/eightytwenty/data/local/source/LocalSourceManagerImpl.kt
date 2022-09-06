fa15e42ed30ae706b0fce252827ed0454758deb7
Behzod Halil <behzoddev@gmail.com>
2022-09-06 15:28:55 +0900

    [feat::] Add LocalSourceManagerImpl.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/source/LocalSourceManagerImpl.kt
@@ -0,0 +1,61 @@
+package uz.behzod.eightytwenty.data.local.source
+
+import uz.behzod.eightytwenty.data.local.dao.NoteCategoryDao
+import uz.behzod.eightytwenty.data.local.dao.NoteDao
+import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
+import uz.behzod.eightytwenty.data.local.entities.NoteEntity
+import javax.inject.Inject
+
+class LocalSourceManagerImpl @Inject constructor(
+    private val noteDao: NoteDao,
+    private val noteCategoryDao: NoteCategoryDao
+): LocalSourceManager {
+
+    override suspend fun insertNote(note: NoteEntity) {
+        return noteDao.insert(note)
+    }
+
+    override suspend fun updateNote(note: NoteEntity) {
+        return noteDao.update(note)
+    }
+
+    override suspend fun deleteNote(note: NoteEntity) {
+        return noteDao.delete(note)
+    }
+
+    override suspend fun fetchTrashedNotes(): List<NoteEntity> {
+        return noteDao.fetchTrashedNotes()
+    }
+
+    override suspend fun fetchAllNotes(): List<NoteEntity> {
+        return noteDao.fetchAllNotes()
+    }
+
+    override suspend fun insertNoteCategory(category: NoteCategoryEntity) {
+        return noteCategoryDao.insert(category)
+    }
+
+    override suspend fun updateNoteCategory(category: NoteCategoryEntity) {
+        return noteCategoryDao.update(category)
+    }
+
+    override suspend fun deleteNoteCategory(category: NoteCategoryEntity) {
+        return noteCategoryDao.delete(category)
+    }
+
+    override suspend fun incrementNoteCount(noteCategoryId: Long) {
+        return noteCategoryDao.incrementNoteCount(noteCategoryId)
+    }
+
+    override suspend fun decrementNoteCount(noteCategoryId: Long) {
+        return noteCategoryDao.decrementNoteCount(noteCategoryId)
+    }
+
+    override suspend fun fetchAllCategories(): List<NoteCategoryEntity> {
+        return noteCategoryDao.fetchAllCategories()
+    }
+
+    override suspend fun fetchIfCategoryIdExists(noteCategoryId: Int): Boolean {
+        return noteCategoryDao.fetchIfCategoryIdExists(noteCategoryId)
+    }
+}
