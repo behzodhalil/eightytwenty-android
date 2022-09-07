1d268e1952e0fdd7578b95a6d459b141243b94f3
Behzod Halil <behzoddev@gmail.com>
2022-09-07 10:47:09 +0900

    [feat::] Add InsertNoteCategoryImpl.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/InsertNoteCategoryImpl.kt
@@ -0,0 +1,20 @@
+package uz.behzod.eightytwenty.domain.interactor.note_category
+
+import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.asEntity
+import uz.behzod.eightytwenty.domain.NoteCategoryDomainModel
+import uz.behzod.eightytwenty.domain.repository.NoteCategoryRepository
+import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
+import javax.inject.Inject
+
+class InsertNoteCategoryImpl @Inject constructor(
+    private val iNoteCategoryRepository: NoteCategoryRepository,
+    private val dispatcherProvider: IDispatcherProvider
+): InsertNoteCategory {
+
+    override suspend fun invoke(data: NoteCategoryDomainModel) {
+        return withContext(dispatcherProvider.io) {
+            iNoteCategoryRepository.insertNoteCategory(data.asEntity())
+        }
+    }
+}
