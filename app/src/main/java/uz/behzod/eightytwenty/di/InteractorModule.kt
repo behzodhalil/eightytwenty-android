242f7b300b814e438939aa871d8f036ffa265fda
Behzod Halil <behzoddev@gmail.com>
2022-09-07 10:47:17 +0900

    [feat::] Add InteractorModule.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/di/InteractorModule.kt
@@ -0,0 +1,54 @@
+package uz.behzod.eightytwenty.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import uz.behzod.eightytwenty.domain.interactor.note.*
+import uz.behzod.eightytwenty.domain.interactor.note_category.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface InteractorModule {
+
+    @Binds
+    @Singleton
+    fun providesInsertNote(interactor: InsertNoteImpl): InsertNote
+
+    @Binds
+    @Singleton
+    fun providesUpdateNote(interactor: UpdateNoteImpl): UpdateNote
+
+    @Binds
+    @Singleton
+    fun providesDeleteNote(interactor: DeleteNoteImpl): DeleteNote
+
+    @Binds
+    @Singleton
+    fun providesFetchTrashedNotes(interactor: FetchTrashedNotesImpl): FetchTrashedNotes
+
+    @Binds
+    @Singleton
+    fun providesFetchAllNotes(interactor: FetchNotesImpl): FetchNotesImpl
+
+    @Binds
+    @Singleton
+    fun providesInsertNoteCategory(interactor: InsertNoteCategoryImpl): InsertNoteCategory
+
+    @Binds
+    @Singleton
+    fun providesUpdateNoteCategory(interactor: UpdateNoteCategoryImpl): UpdateNoteCategory
+
+    @Binds
+    @Singleton
+    fun providesDeleteNoteCategory(interactor: DeleteNoteCategoryImpl): DeleteNoteCategory
+
+    @Binds
+    @Singleton
+    fun providesFetchAllCategories(interactor: FetchAllCategoriesImpl): FetchAllCategories
+
+    @Binds
+    @Singleton
+    fun providesFetchAllCategoriesAndNotes(interactor: FetchAllCategoriesAndNotesImpl): FetchAllCategoriesAndNotes
+}
