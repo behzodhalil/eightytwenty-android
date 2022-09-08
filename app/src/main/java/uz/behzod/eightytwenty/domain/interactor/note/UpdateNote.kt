05b6972381879612982d1e796862290e1e46acad
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:34:13 +0900

    [feat::] Update UpdateNote.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/UpdateNote.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/UpdateNote.kt
@@ -1,6 +1,6 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
-import uz.behzod.eightytwenty.domain.NoteDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 
 interface UpdateNote {
     suspend operator fun invoke(data: NoteDomainModel)
