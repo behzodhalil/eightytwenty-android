3d1e6e1530a02d875228995131e372a73f3b1567
Behod Halil <behzoddev@gmail.com>
2022-09-08 20:02:02 +0900

    [feat::] Add CategoryNoteAdapter.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteAdapter.kt
@@ -0,0 +1,54 @@
+package uz.behzod.eightytwenty.features.category_note
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import uz.behzod.eightytwenty.databinding.ViewHolderCategoryNoteBinding
+import uz.behzod.eightytwenty.domain.NoteCategoryDomainModel
+
+class CategoryNoteAdapter :
+    ListAdapter<NoteCategoryDomainModel, CategoryNoteAdapter.CategoryNoteViewHolder>(COMPARATOR) {
+
+    companion object {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<NoteCategoryDomainModel>() {
+            override fun areItemsTheSame(
+                oldItem: NoteCategoryDomainModel,
+                newItem: NoteCategoryDomainModel
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: NoteCategoryDomainModel,
+                newItem: NoteCategoryDomainModel
+            ): Boolean {
+                return oldItem.name == newItem.name && oldItem.count == newItem.count
+            }
+        }
+    }
+
+    inner class CategoryNoteViewHolder(val binding: ViewHolderCategoryNoteBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryNoteViewHolder {
+        val binding = ViewHolderCategoryNoteBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CategoryNoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryNoteViewHolder, position: Int) {
+        holder.apply {
+            currentList[position].let {
+                binding.apply {
+                    tvCategoryName.text = it.name
+                    tvCategoryNoteCount.text = it.count.toString()
+                }
+            }
+        }
+    }
+}
