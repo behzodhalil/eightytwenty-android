ea796780b9108e1580c240ba845e142feebad93b
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:27:31 +0900

    [feat::] Update FetchNotes.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/FetchNotes.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/FetchNotes.kt
@@ -1,7 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
 import kotlinx.coroutines.flow.Flow
-import uz.behzod.eightytwenty.domain.NoteDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 
 interface FetchNotes {
     operator fun invoke(): Flow<List<NoteDomainModel>>
