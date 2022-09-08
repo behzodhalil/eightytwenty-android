9f2977c32ea24da11672581acd141c1f47c47472
Behod Halil <behzoddev@gmail.com>
2022-09-08 20:08:16 +0900

    [feat::] Update NoteViewEffect.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewEffect.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewEffect.kt
@@ -1,7 +1,7 @@
 package uz.behzod.eightytwenty.features.note
 
 sealed interface NoteViewEffect {
-    object NewNoteViewEffect: NoteViewEffect
+    object NewNoteClickViewEffect: NoteViewEffect
     object CategoryViewEffect: NoteViewEffect
     object DetailViewEffect: NoteViewEffect
     object SearchViewEffect: NoteViewEffect
