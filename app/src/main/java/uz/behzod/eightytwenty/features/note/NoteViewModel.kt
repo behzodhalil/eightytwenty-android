efef7242b349cbc6103ef12692e5c4d6b6115fc0
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:32:38 +0900

    [feat::] Update NoteViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/NoteViewModel.kt
@@ -1,22 +1,49 @@
 package uz.behzod.eightytwenty.features.note
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.domain.interactor.note.FetchNotes
 import javax.inject.Inject
 
 @HiltViewModel
-class NoteViewModel @Inject constructor() : ViewModel() {
+class NoteViewModel @Inject constructor(
+    private val iFetchNotes: FetchNotes
+) : ViewModel() {
 
     private var _viewEffect = Channel<NoteViewEffect>(Channel.BUFFERED)
     val viewEffect: Flow<NoteViewEffect> = _viewEffect.receiveAsFlow()
 
+    private var _uiState: MutableStateFlow<NoteUIState> = MutableStateFlow(NoteUIState.Loading)
+    val uiState: Flow<NoteUIState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            Log.d("NoteViewModel","fetchNotes is called in viewModelScope")
+            iFetchNotes.invoke().collect { result ->
+                Log.d("NoteViewModel","fetchNotes is collected")
+                Log.d("NoteViewModel","${result[0]}")
+                _uiState.value = NoteUIState.Loading
+                if (result.isNullOrEmpty()) {
+                    Log.d("NoteViewModel","NoteUIState value is empty")
+                    _uiState.value = NoteUIState.Empty
+                } else {
+                    _uiState.value = NoteUIState.Success(result)
+                    Log.d("NoteViewModel","NoteUIState value is success")
+                }
+            }
+        }
+    }
+
     fun onEvent(event: NoteEvent) {
-        when(event) {
+        when (event) {
             NoteEvent.CategoryEvent -> onObserveEffects(NoteViewEffect.CategoryViewEffect)
             NoteEvent.DetailEvent -> onObserveEffects(NoteViewEffect.DetailViewEffect)
             NoteEvent.NewNoteEvent -> onObserveEffects(NoteViewEffect.NewNoteClickViewEffect)
@@ -26,7 +53,7 @@ class NoteViewModel @Inject constructor() : ViewModel() {
 
     private fun onObserveEffects(effect: NoteViewEffect) {
         viewModelScope.launch {
-            when(effect) {
+            when (effect) {
                 NoteViewEffect.CategoryViewEffect -> {
                     _viewEffect.trySend(NoteViewEffect.CategoryViewEffect)
                 }
@@ -42,4 +69,6 @@ class NoteViewModel @Inject constructor() : ViewModel() {
             }
         }
     }
+
+
 }
