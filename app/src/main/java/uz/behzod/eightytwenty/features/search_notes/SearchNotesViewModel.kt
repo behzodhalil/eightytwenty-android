0f83d351bf11ed7ef84ec9ba831bbf810fa06fd3
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:33:56 +0900

    [feat::] Add SearchNotesViewModel.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/search_notes/SearchNotesViewModel.kt
@@ -0,0 +1,11 @@
+package uz.behzod.eightytwenty.features.search_notes
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchNotesViewModel @Inject constructor(
+
+): ViewModel() {
+}
