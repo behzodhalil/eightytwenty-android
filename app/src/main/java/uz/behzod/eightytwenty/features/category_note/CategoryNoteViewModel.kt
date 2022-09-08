cd473a00b6d150e2ac99dc3e3923e5a88524a31b
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:26:56 +0900

    [feat::] Update CategoryNoteViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteViewModel.kt
@@ -2,7 +2,8 @@ package uz.behzod.eightytwenty.features.category_note
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class CategoryNoteViewModel: ViewModel() {
+class CategoryNoteViewModel @Inject constructor(): ViewModel() {
 }
