31c03890390da5af971f1a86d7559c2bfea0bb02
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:34:19 +0900

    [feat::] Update UpdateNoteCategory.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/UpdateNoteCategory.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/UpdateNoteCategory.kt
@@ -1,6 +1,6 @@
 package uz.behzod.eightytwenty.domain.interactor.note_category
 
-import uz.behzod.eightytwenty.domain.NoteCategoryDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteCategoryDomainModel
 
 interface UpdateNoteCategory {
     suspend operator fun invoke(data: NoteCategoryDomainModel)
