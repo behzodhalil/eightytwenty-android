9aafbcecbe552fba394e50a6d8dc8b9ec8646e48
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:27:07 +0900

    [feat::] Update DeleteNoteCategory.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/DeleteNoteCategory.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/DeleteNoteCategory.kt
@@ -1,6 +1,6 @@
 package uz.behzod.eightytwenty.domain.interactor.note_category
 
-import uz.behzod.eightytwenty.domain.NoteCategoryDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteCategoryDomainModel
 
 interface DeleteNoteCategory {
     suspend operator fun invoke(data: NoteCategoryDomainModel)
