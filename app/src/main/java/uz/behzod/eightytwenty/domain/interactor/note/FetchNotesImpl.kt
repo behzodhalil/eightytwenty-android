22ebb4944c44d9cf676c6da013dbb1780c52ddb4
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:27:53 +0900

    [feat::] Update FetchNotesImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/FetchNotesImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/FetchNotesImpl.kt
@@ -1,11 +1,10 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
+import android.util.Log
+import kotlinx.coroutines.flow.*
+import uz.behzod.eightytwenty.data.local.entities.asDomain
 import uz.behzod.eightytwenty.data.local.entities.asListOfDomain
-import uz.behzod.eightytwenty.domain.NoteDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.domain.repository.NoteRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
@@ -17,9 +16,11 @@ class FetchNotesImpl @Inject constructor(
 
     override fun invoke(): Flow<List<NoteDomainModel>> {
         return flow {
-            iNoteRepository.fetchAllNotes()
-                .map { it.asListOfDomain() }
-                .flowOn(dispatcherProvider.io)
-        }
+            iNoteRepository.fetchAllNotes().collect {
+                if (it.isNotEmpty()) {
+                    emit(it.asListOfDomain())
+                }
+            }
+        }.flowOn(dispatcherProvider.io)
     }
 }
