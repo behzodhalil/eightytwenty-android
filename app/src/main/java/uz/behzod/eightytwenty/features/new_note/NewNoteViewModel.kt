345615b28e6daaaaadbeb58a3af77de97bb3bd55
Behod Halil <behzoddev@gmail.com>
2022-09-08 20:07:27 +0900

    [feat::] Update NewNoteViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteViewModel.kt
@@ -1,4 +1,40 @@
 package uz.behzod.eightytwenty.features.new_note
 
-class NewNoteViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.domain.interactor.note.InsertNote
+import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
+import javax.inject.Inject
+
+@HiltViewModel
+class NewNoteViewModel @Inject constructor(
+    private val insertNote: InsertNote,
+    private val dispatcherProvider: IDispatcherProvider
+) : ViewModel() {
+
+    private var _viewEffect: Channel<NewNoteViewEffect> = Channel(Channel.BUFFERED)
+    val viewEffect: Flow<NewNoteViewEffect> = _viewEffect.receiveAsFlow()
+
+    fun onListenerEvents(event: NewNoteEvent) {
+        when(event) {
+            NewNoteEvent.RedoEvent -> onObserverEffects(NewNoteViewEffect.RedoViewEffect)
+            NewNoteEvent.SavedEvent -> onObserverEffects(NewNoteViewEffect.SavedViewEffect)
+            NewNoteEvent.UndoEvent -> onObserverEffects(NewNoteViewEffect.UndoViewEffect)
+        }
+    }
+
+    private fun onObserverEffects(effect: NewNoteViewEffect) {
+        viewModelScope.launch {
+            when(effect) {
+                NewNoteViewEffect.RedoViewEffect -> _viewEffect.trySend(NewNoteViewEffect.RedoViewEffect)
+                NewNoteViewEffect.SavedViewEffect -> _viewEffect.trySend(NewNoteViewEffect.SavedViewEffect)
+                NewNoteViewEffect.UndoViewEffect -> _viewEffect.trySend(NewNoteViewEffect.UndoViewEffect)
+            }
+        }
+    }
 }
