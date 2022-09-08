f48fd3fa23b2a0c7384c29b58cc1e16a2ce173ed
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:32:26 +0900

    [feat::] Update NoteRepository.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/repository/NoteRepository.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/NoteRepository.kt
@@ -2,6 +2,7 @@ package uz.behzod.eightytwenty.domain.repository
 
 import kotlinx.coroutines.flow.Flow
 import uz.behzod.eightytwenty.data.local.entities.NoteEntity
+import uz.behzod.eightytwenty.utils.Resource
 
 interface NoteRepository {
     suspend fun insertNote(note: NoteEntity)
@@ -9,4 +10,5 @@ interface NoteRepository {
     suspend fun deleteNote(note: NoteEntity)
     fun fetchTrashedNotes(): Flow<List<NoteEntity>>
     fun fetchAllNotes(): Flow<List<NoteEntity>>
+    fun fetchNotesByCategoryId(categoryId: Long): Flow<List<NoteEntity>>
 }
