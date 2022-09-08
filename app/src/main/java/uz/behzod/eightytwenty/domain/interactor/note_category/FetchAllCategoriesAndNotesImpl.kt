632940e3b0d8dbace1da6eca9ebb18548c7ded57
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:27:26 +0900

    [feat::] Update FetchAllCategoriesAndNotesImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/FetchAllCategoriesAndNotesImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/FetchAllCategoriesAndNotesImpl.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import uz.behzod.eightytwenty.data.local.entities.asListOfDomain
-import uz.behzod.eightytwenty.domain.repository.CategoryAndNotesDomainModel
+import uz.behzod.eightytwenty.domain.model.CategoryAndNotesDomainModel
 import uz.behzod.eightytwenty.domain.repository.NoteCategoryRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
