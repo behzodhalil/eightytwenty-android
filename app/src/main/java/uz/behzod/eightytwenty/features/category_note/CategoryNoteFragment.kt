9b70e196473c794feb41aa2cad19eee107ff197f
Behod Halil <behzoddev@gmail.com>
2022-09-08 20:02:16 +0900

    [feat::] Update CategoryNoteFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteFragment.kt
@@ -1,4 +1,53 @@
 package uz.behzod.eightytwenty.features.category_note
 
-class CategoryNoteFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.databinding.FragmentCategoryNoteBinding
+
+@AndroidEntryPoint
+class CategoryNoteFragment : Fragment() {
+
+    private var _binding: FragmentCategoryNoteBinding? = null
+    private val binding: FragmentCategoryNoteBinding get() = _binding!!
+    private val viewModel: CategoryNoteViewModel by viewModels()
+    private lateinit var adapter: CategoryNoteAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCategoryNoteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupUI() {
+
+    }
+
+    private fun onNavigateNewNote() {
+        binding.ivBack.setOnClickListener {
+            findNavController().navigate(R.id.action_categoryNoteFragment_to_newNoteFragment)
+        }
+    }
+
+    private fun onNavigateNotesByName() {
+
+    }
 }
