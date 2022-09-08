2773c47a290319996b05c8b4f8c99266f84d6ed1
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:34:28 +0900

    [feat::] Update UpdateNoteImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/UpdateNoteImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/UpdateNoteImpl.kt
@@ -2,7 +2,7 @@ package uz.behzod.eightytwenty.domain.interactor.note
 
 import kotlinx.coroutines.withContext
 import uz.behzod.eightytwenty.data.local.entities.asEntity
-import uz.behzod.eightytwenty.domain.NoteDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.domain.repository.NoteRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
