820ffd33cdc8f9d0b8714697cc728ccb006a0198
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:34:24 +0900

    [feat::] Update UpdateNoteCategoryImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/UpdateNoteCategoryImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/UpdateNoteCategoryImpl.kt
@@ -2,7 +2,7 @@ package uz.behzod.eightytwenty.domain.interactor.note_category
 
 import kotlinx.coroutines.withContext
 import uz.behzod.eightytwenty.data.local.entities.asEntity
-import uz.behzod.eightytwenty.domain.NoteCategoryDomainModel
+import uz.behzod.eightytwenty.domain.model.NoteCategoryDomainModel
 import uz.behzod.eightytwenty.domain.repository.NoteCategoryRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
