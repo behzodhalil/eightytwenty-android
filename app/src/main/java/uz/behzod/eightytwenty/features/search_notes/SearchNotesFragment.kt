a3edb9907356c4a12fff498a9f2459517bb88c53
Behod Halil <behzoddev@gmail.com>
2022-09-08 23:33:51 +0900

    [feat::] Add SearchNotesFragment.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/search_notes/SearchNotesFragment.kt
@@ -0,0 +1,24 @@
+package uz.behzod.eightytwenty.features.search_notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchNotesFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
