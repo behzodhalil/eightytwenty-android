fe6b6642a2670911ec889511dfdf606a9ff22c9f
Behod Halil <behzoddev@gmail.com>
2022-09-08 20:07:16 +0900

    [feat::] Update NewNoteFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
@@ -1,4 +1,34 @@
 package uz.behzod.eightytwenty.features.new_note
 
-class NewNoteFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.databinding.FragmentNewNoteBinding
+
+@AndroidEntryPoint
+class NewNoteFragment: Fragment() {
+
+    private var _binding: FragmentNewNoteBinding? = null
+    private val binding: FragmentNewNoteBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNewNoteBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
