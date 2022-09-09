e7960fb389eefd4af60d58c74d788e939d330b8d
Behod Halil <behzoddev@gmail.com>
2022-09-09 11:00:15 +0900

    [feat::] Update NoteDomainModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/model/NoteDomainModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/model/NoteDomainModel.kt
@@ -1,9 +1,12 @@
 package uz.behzod.eightytwenty.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import uz.behzod.eightytwenty.utils.ext.Empty
 import uz.behzod.eightytwenty.utils.ext.Zero
 import java.time.ZonedDateTime
 
+@Parcelize
 data class NoteDomainModel(
     val id: Long = Long.Zero,
     val title: String = String.Empty,
@@ -11,4 +14,4 @@ data class NoteDomainModel(
     val timestamp: ZonedDateTime = ZonedDateTime.now(),
     val isTrashed: Boolean = false,
     val categoryId: Long = Long.Zero
-)
+) : Parcelable
