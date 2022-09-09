163b04ab9e17af2e65196c870f4ef0199344a411
Behod Halil <behzoddev@gmail.com>
2022-09-09 11:00:01 +0900

    [feat::] Update FetchAllCategories.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/FetchAllCategories.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note_category/FetchAllCategories.kt
@@ -2,7 +2,8 @@ package uz.behzod.eightytwenty.domain.interactor.note_category
 
 import kotlinx.coroutines.flow.Flow
 import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
+import uz.behzod.eightytwenty.domain.model.NoteCategoryDomainModel
 
 interface FetchAllCategories {
-    operator fun invoke(): Flow<List<NoteCategoryEntity>>
+    operator fun invoke(): Flow<List<NoteCategoryDomainModel>>
 }
