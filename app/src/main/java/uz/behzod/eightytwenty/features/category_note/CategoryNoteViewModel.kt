a44938b63325713d1925f171eb79b8ff0e7719b2
Behod Halil <behzoddev@gmail.com>
2022-09-09 10:59:56 +0900

    [feat::] Update CategoryNoteViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/category_note/CategoryNoteViewModel.kt
@@ -1,9 +1,41 @@
 package uz.behzod.eightytwenty.features.category_note
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.domain.interactor.note_category.FetchAllCategories
+import uz.behzod.eightytwenty.domain.interactor.note_category.InsertNoteCategory
+import uz.behzod.eightytwenty.domain.model.NoteCategoryDomainModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryNoteViewModel @Inject constructor(): ViewModel() {
+class CategoryNoteViewModel @Inject constructor(
+    private val iInsertCategoryNote: InsertNoteCategory,
+    private val iFetchAllCategory: FetchAllCategories
+) : ViewModel() {
+
+    private var _uiState: MutableStateFlow<CategoryNoteUIState> =
+        MutableStateFlow(CategoryNoteUIState.Loading)
+    val uiState: Flow<CategoryNoteUIState> = _uiState.asStateFlow()
+
+    fun insertNoteCategory(value: NoteCategoryDomainModel) {
+        viewModelScope.launch {
+            iInsertCategoryNote.invoke(value)
+        }
+    }
+
+    init {
+        iFetchAllCategory.invoke()
+            .onEach { result ->
+            _uiState.value = CategoryNoteUIState.Loading
+            if (result.isEmpty()) {
+                _uiState.value = CategoryNoteUIState.Empty
+            } else {
+                _uiState.value = CategoryNoteUIState.Success(result)
+            } }
+            .launchIn(viewModelScope)
+
+    }
 }
