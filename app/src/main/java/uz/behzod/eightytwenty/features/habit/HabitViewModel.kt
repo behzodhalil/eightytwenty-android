6168ef7f8819f7e5f625a9e51d841f5d1345d22e
Behod Halil <behzoddev@gmail.com>
2022-09-10 23:18:46 +0900

    [feat::] Add HabitViewModel.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitViewModel.kt
@@ -0,0 +1,9 @@
+package uz.behzod.eightytwenty.features.habit
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HabitViewModel @Inject constructor(): ViewModel() {
+}
