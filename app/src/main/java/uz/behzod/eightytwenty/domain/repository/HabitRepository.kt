82bc4e356eabfff73d333b1ea3dcb7806439c137
Behod Halil <behzoddev@gmail.com>
2022-09-10 16:41:49 +0900

    [feat::] Add HabitRepository.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/HabitRepository.kt
@@ -0,0 +1,14 @@
+package uz.behzod.eightytwenty.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.HabitEntity
+import uz.behzod.eightytwenty.domain.model.HabitDomainModel
+
+interface HabitRepository {
+    suspend fun insertHabit(habit: HabitEntity)
+    suspend fun updateHabit(habit: HabitEntity)
+    suspend fun deleteHabit(habit: HabitEntity)
+    fun fetchHabitByUid(uid: Long): Flow<HabitEntity>
+    fun fetchAllHabits(): Flow<List<HabitEntity>>
+    fun fetchHabitsByDate(timestamp: Long): Flow<List<HabitEntity>>
+}
