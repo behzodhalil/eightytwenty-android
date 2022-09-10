e1624d74532ab376a4ab05c3d541454443feda05
Behod Halil <behzoddev@gmail.com>
2022-09-10 16:40:35 +0900

    [feat::] Update LocalSourceManagerImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/source/LocalSourceManagerImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/source/LocalSourceManagerImpl.kt
@@ -1,16 +1,19 @@
 package uz.behzod.eightytwenty.data.source
 
 import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.dao.HabitDao
 import uz.behzod.eightytwenty.data.local.dao.NoteCategoryDao
 import uz.behzod.eightytwenty.data.local.dao.NoteDao
 import uz.behzod.eightytwenty.data.local.entities.CategoryAndNotes
+import uz.behzod.eightytwenty.data.local.entities.HabitEntity
 import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
 import uz.behzod.eightytwenty.data.local.entities.NoteEntity
 import javax.inject.Inject
 
 class LocalSourceManagerImpl @Inject constructor(
     private val noteDao: NoteDao,
-    private val noteCategoryDao: NoteCategoryDao
+    private val noteCategoryDao: NoteCategoryDao,
+    private val habitDao: HabitDao
 ): LocalSourceManager {
 
     override suspend fun insertNote(note: NoteEntity) {
@@ -75,5 +78,29 @@ class LocalSourceManagerImpl @Inject constructor(
 
     override fun fetchAllCategoriesAndNotes(): Flow<List<CategoryAndNotes>> {
         return noteCategoryDao.fetchAllCategoriesWithNotes()
+    }
+
+    override suspend fun insertHabit(habit: HabitEntity) {
+        return habitDao.insertHabit(habit)
+    }
+
+    override suspend fun updateHabit(habit: HabitEntity) {
+        return habitDao.updateHabit(habit)
+    }
+
+    override suspend fun deleteHabit(habit: HabitEntity) {
+        return habitDao.deleteHabit(habit)
+    }
+
+    override fun fetchHabitByUid(uid: Long): Flow<HabitEntity> {
+        return habitDao.fetchHabitByUid(uid)
+    }
+
+    override fun fetchAllHabits(): Flow<List<HabitEntity>> {
+        return habitDao.fetchAllHabits()
+    }
+
+    override fun fetchHabitsByDate(timestamp: Long): Flow<List<HabitEntity>> {
+        return habitDao.fetchHabitsByDate(timestamp)
     }
 }
