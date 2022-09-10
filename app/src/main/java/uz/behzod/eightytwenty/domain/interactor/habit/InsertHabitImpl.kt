be88debba32bcc78f264e173e84d6773d94df03f
Behod Halil <behzoddev@gmail.com>
2022-09-10 18:21:52 +0900

    [feat::] Update InsertHabitImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/InsertHabitImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/InsertHabitImpl.kt
@@ -1,13 +1,20 @@
 package uz.behzod.eightytwenty.domain.interactor.habit
 
+import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.asEntity
 import uz.behzod.eightytwenty.domain.model.HabitDomainModel
+import uz.behzod.eightytwenty.domain.repository.HabitRepository
+import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
 
 class InsertHabitImpl @Inject constructor(
-
+    private val repository: HabitRepository,
+    private val dispatcherProvider: IDispatcherProvider
 ): InsertHabit {
 
     override suspend fun invoke(habit: HabitDomainModel) {
-        TODO("Not yet implemented")
+       return withContext(dispatcherProvider.io) {
+           repository.insertHabit(habit.asEntity())
+       }
     }
 }
