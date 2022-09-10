e258786b3039cc33c8ad08984478501f9aa27f07
Behod Halil <behzoddev@gmail.com>
2022-09-10 23:18:59 +0900

    [feat::] Update NoteDetailFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailFragment.kt
@@ -2,9 +2,7 @@ package uz.behzod.eightytwenty.features.note_detail
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -15,27 +13,18 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentNoteDetailBinding
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.utils.view.viewBinding
 import java.time.ZonedDateTime
 
 @AndroidEntryPoint
-class NoteDetailFragment : Fragment() {
+class NoteDetailFragment : Fragment(R.layout.fragment_note_detail) {
 
-    private var _binding: FragmentNoteDetailBinding? = null
-    private val binding: FragmentNoteDetailBinding get() = _binding!!
+    private val binding by viewBinding(FragmentNoteDetailBinding::bind)
     private val viewModel: NoteDetailViewModel by viewModels()
     private val args: NoteDetailFragmentArgs by navArgs()
-    private lateinit var data: NoteDomainModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentNoteDetailBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,13 +34,6 @@ class NoteDetailFragment : Fragment() {
     private fun setupUI() {
         val uid = args.noteId
         fetchNoteByUid(uid)
-
-        onUpdate()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     private fun fetchNoteByUid(uid: Long) {
@@ -70,10 +52,11 @@ class NoteDetailFragment : Fragment() {
 
                         }
                         is NoteDetailUIState.Success -> {
-
+                            val data = state.data
                             Log.d("NoteDetailFragment", "Detail data is ${state.data}")
-                            initNote(state.data)
-                            data = state.data
+
+                            initNote(data)
+                            updateNote(param = data)
 
                         }
                     }
@@ -82,26 +65,25 @@ class NoteDetailFragment : Fragment() {
         }
     }
 
-    private fun onUpdate() {
-        binding.btnSaveOrCancel.setOnClickListener {
-            updateNote()
-        }
-    }
 
-    private fun updateNote() {
+    private fun updateNote(param: NoteDomainModel) {
         lifecycleScope.launch {
-            data.title = binding.etTitle.text.toString()
-            data.description = binding.etDesc.text.toString()
-            data.timestamp = ZonedDateTime.now()
+            binding.btnSaveOrCancel.setOnClickListener {
+                param.title = binding.etTitle.text.toString()
+                param.description = binding.etDesc.text.toString()
+                param.timestamp = ZonedDateTime.now()
 
-            viewModel.updateNote(
-                data
-            ).run {
-                Toast.makeText(requireContext(), "Note is updated", Toast.LENGTH_SHORT).show()
-                val action = NoteDetailFragmentDirections.actionNoteDetailFragmentToNoteFragment()
-                Log.d("TAG", "Note detail value is ${getNotes()}")
-                findNavController().navigate(action)
+                viewModel.updateNote(
+                    param
+                ).run {
+                    Toast.makeText(requireContext(), "Note is updated", Toast.LENGTH_SHORT).show()
+                    val action =
+                        NoteDetailFragmentDirections.actionNoteDetailFragmentToNoteFragment()
+                    Log.d("TAG", "Note detail value is ${getNotes()}")
+                    findNavController().navigate(action)
+                }
             }
+
 
         }
 
