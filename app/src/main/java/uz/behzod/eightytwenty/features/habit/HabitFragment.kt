db3240639165e6daf1f132cd9898fb04d7db94a3
Behod Halil <behzoddev@gmail.com>
2022-09-10 23:18:37 +0900

    [feat::] Add HabitFragment.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitFragment.kt
@@ -0,0 +1,10 @@
+package uz.behzod.eightytwenty.features.habit
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HabitFragment: Fragment() {
+
+    
+}
