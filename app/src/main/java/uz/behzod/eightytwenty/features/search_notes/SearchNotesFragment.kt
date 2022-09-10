9516679c96888d476ac07f68c177235f44fa97f8
Behod Halil <behzoddev@gmail.com>
2022-09-10 16:42:40 +0900

    [feat::] Update SearchNotesFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/search_notes/SearchNotesFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/search_notes/SearchNotesFragment.kt
@@ -1,24 +1,84 @@
 package uz.behzod.eightytwenty.features.search_notes
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.databinding.FragmentSearchNotesBinding
 
 @AndroidEntryPoint
-class SearchNotesFragment: Fragment() {
+class SearchNotesFragment : Fragment(R.layout.fragment_search_notes) {
+
+    private var _binding: FragmentSearchNotesBinding? = null
+    private val binding: FragmentSearchNotesBinding get() = _binding!!
+
+    private val viewModel: SearchNotesViewModel by viewModels()
+
+    private lateinit var adapter: SearchNotesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentSearchNotesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        search()
+    }
+
+
+    private fun initRecyclerView() {
+        adapter = SearchNotesAdapter()
+        binding.rvNote.adapter = adapter
+    }
+
+    private fun search() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.searchView.clearFocus()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    searchNotes(newText)
+                }
+                return false
+            }
+        })
+    }
+
+    private fun searchNotes(query: String) = lifecycleScope.launch {
+        val searchQuery = "%$query%"
+
+        viewModel.searchNotes(searchQuery)
+        viewModel.uiState.collect { result ->
+            when (result) {
+                is SearchNotesUIState.Empty -> {
+
+                }
+                is SearchNotesUIState.Failure -> {
+
+                }
+                is SearchNotesUIState.Loading -> {
+
+                }
+                is SearchNotesUIState.Success -> {
+                    adapter.submitList(result.data)
+                    Log.d("","Result data is ${result.data}")
+                }
+            }
+        }
     }
 }
