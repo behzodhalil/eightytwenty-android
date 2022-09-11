f61ccd61c474eb3b69002b3e91792e9a5a473aaf
Behod Halil <behzoddev@gmail.com>
2022-09-11 16:47:11 +0900

    [feat::] Update EightyTwentyDatabase.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/db/EightyTwentyDatabase.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/db/EightyTwentyDatabase.kt
@@ -3,14 +3,20 @@ package uz.behzod.eightytwenty.data.local.db
 import android.content.Context
 import androidx.room.*
 import uz.behzod.eightytwenty.data.local.dao.HabitDao
+import uz.behzod.eightytwenty.data.local.dao.HabitRecommendDao
 import uz.behzod.eightytwenty.data.local.dao.NoteCategoryDao
 import uz.behzod.eightytwenty.data.local.dao.NoteDao
+import uz.behzod.eightytwenty.data.local.entities.HabitEntity
+import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity
 import uz.behzod.eightytwenty.data.local.entities.NoteCategoryEntity
 import uz.behzod.eightytwenty.data.local.entities.NoteEntity
 
 @Database(
-    entities = [NoteEntity::class, NoteCategoryEntity::class],
-    version = 2
+    entities = [
+        NoteEntity::class, NoteCategoryEntity::class,
+        HabitEntity::class, HabitRecommendEntity::class],
+    version = 4,
+    exportSchema = false
 )
 @TypeConverters(ZonedDateTimeConverter::class)
 abstract class EightyTwentyDatabase : RoomDatabase() {
@@ -18,6 +24,7 @@ abstract class EightyTwentyDatabase : RoomDatabase() {
     abstract fun getNoteDao(): NoteDao
     abstract fun getNoteCategoryDao(): NoteCategoryDao
     abstract fun getHabitDao(): HabitDao
+    abstract fun getHabitRecommendDao(): HabitRecommendDao
 
     companion object {
         @Volatile
@@ -32,6 +39,8 @@ abstract class EightyTwentyDatabase : RoomDatabase() {
             context.applicationContext,
             EightyTwentyDatabase::class.java,
             "eighty_database"
-        ).build()
+        ).fallbackToDestructiveMigration()
+            .build()
+
     }
 }
