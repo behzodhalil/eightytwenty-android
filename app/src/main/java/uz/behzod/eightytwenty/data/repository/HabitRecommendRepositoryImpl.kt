110ea0482fdb65ff1f1c046ebfc5bd788884a50f
Behod Halil <behzoddev@gmail.com>
2022-09-11 16:49:51 +0900

    [feat::] Add HabitRecommendRepositoryImpl.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/repository/HabitRecommendRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package uz.behzod.eightytwenty.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity
+import uz.behzod.eightytwenty.data.source.LocalSourceManager
+import uz.behzod.eightytwenty.domain.repository.HabitRecommendRepository
+import javax.inject.Inject
+
+class HabitRecommendRepositoryImpl @Inject constructor(
+    private val sourceManager: LocalSourceManager
+) : HabitRecommendRepository {
+
+    override suspend fun insertHabitRecommend(habitRecommend: HabitRecommendEntity) {
+        return sourceManager.insertHabitRecommend(habitRecommend)
+    }
+
+    override suspend fun updateHabitRecommend(habitRecommend: HabitRecommendEntity) {
+        return sourceManager.updateHabitRecommend(habitRecommend)
+    }
+
+    override suspend fun deleteHabitRecommend(habitRecommend: HabitRecommendEntity) {
+        return sourceManager.deleteHabitRecommend(habitRecommend)
+    }
+
+    override fun fetchHabitRecommendsByCategory(category: String):Flow<List<HabitRecommendEntity>> {
+        return sourceManager.fetchHabitRecommendsByCategory(category)
+    }
+}
