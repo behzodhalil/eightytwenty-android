62da228696d4e02c9a0c47188a1302ae7596ce3a
Behod Halil <behzoddev@gmail.com>
2022-09-11 16:49:44 +0900

    [feat::] Add HabitRecommendRepository.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/HabitRecommendRepository.kt
@@ -0,0 +1,11 @@
+package uz.behzod.eightytwenty.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity
+
+interface HabitRecommendRepository {
+    suspend fun insertHabitRecommend(habitRecommend: HabitRecommendEntity)
+    suspend fun updateHabitRecommend(habitRecommend: HabitRecommendEntity)
+    suspend fun deleteHabitRecommend(habitRecommend: HabitRecommendEntity)
+    fun fetchHabitRecommendsByCategory(category: String): Flow<List<HabitRecommendEntity>>
+}
