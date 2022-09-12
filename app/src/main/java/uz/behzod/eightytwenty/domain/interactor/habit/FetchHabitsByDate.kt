772b0efb6ccbb276e8d93e71587271ae5aa763cd
Behod Halil <behzoddev@gmail.com>
2022-09-12 23:29:39 +0900

    [feat::] Update FetchHabitsByDate.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/FetchHabitsByDate.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/FetchHabitsByDate.kt
@@ -4,5 +4,5 @@ import kotlinx.coroutines.flow.Flow
 import uz.behzod.eightytwenty.domain.model.HabitDomainModel
 
 interface FetchHabitsByDate {
-    operator fun invoke(timestamp: Long): Flow<List<HabitDomainModel>>
+    operator fun invoke(timestamp: String): Flow<List<HabitDomainModel>>
 }
