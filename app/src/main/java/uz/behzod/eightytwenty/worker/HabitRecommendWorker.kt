f7ecffd3f8389b186a1d43220a7f670a0d2b0aae
Behod Halil <behzoddev@gmail.com>
2022-09-12 20:57:04 +0900

    [feat::] Update HabitRecommendWorker.kt

--- app/src/main/java/uz/behzod/eightytwenty/worker/HabitRecommendWorker.kt
+++ app/src/main/java/uz/behzod/eightytwenty/worker/HabitRecommendWorker.kt
@@ -12,9 +12,11 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import uz.behzod.eightytwenty.data.local.db.EightyTwentyDatabase
 import uz.behzod.eightytwenty.data.local.entities.Frequency
+import uz.behzod.eightytwenty.data.local.entities.PerDayGoalType
 import uz.behzod.eightytwenty.data.local.entities.asEntity
 import uz.behzod.eightytwenty.domain.interactor.habit_recommend.InsertHabitRecommends
 import uz.behzod.eightytwenty.domain.model.HabitRecommendDomainModel
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import javax.inject.Inject
 
@@ -34,72 +36,78 @@ class HabitRecommendWorker @AssistedInject constructor(
             perDayGoalCount = 1,
             endGoalCount = 60,
             frequency = Frequency.WEEK,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            timestamp = 2022L,
+            perDayGoalType = PerDayGoalType.ONCE,
             color = "Green",
             isComplete = false,
-            category = "Our choice"
+            category = "Our Choice"
         ), HabitRecommendDomainModel(
             title = "Выпить стакан воды",
             description = "Выпить стакан воды",
             perDayGoalCount = 1,
             endGoalCount = 60,
             frequency = Frequency.DAILY,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            timestamp = 2022L,
+            perDayGoalType = PerDayGoalType.CUP,
             color = "Blue",
             isComplete = false,
-            category = "Our choice"
+            category = "Our Choice"
         ), HabitRecommendDomainModel(
             title = "Встать раньше 7 утра",
             description = "Встать раньше 7 утра",
             perDayGoalCount = 1,
             endGoalCount = 60,
             frequency = Frequency.WEEK,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            timestamp = 2022L,
+            perDayGoalType = PerDayGoalType.ONCE,
             color = "Green",
             isComplete = false,
-            category = "Our choice"
+            category = "Health"
         ), HabitRecommendDomainModel(
             title = "Почистить зубы",
             description = "Почистить зубы",
             perDayGoalCount = 2,
             endGoalCount = 60,
             frequency = Frequency.WEEK,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            timestamp = 2022L,
+            perDayGoalType = PerDayGoalType.ONCE,
             color = "Light Green",
             isComplete = false,
-            category = "Our choice"
+            category = "Health"
         ), HabitRecommendDomainModel(
             title = "Встать раньше 7 утра",
             description = "Встать раньше 7 утра",
             perDayGoalCount = 1,
             endGoalCount = 60,
             frequency = Frequency.WEEK,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            timestamp = 2022L,
+            perDayGoalType = PerDayGoalType.ONCE,
             color = "Green",
             isComplete = false,
-            category = "Our choice"
+            category = "Our Choice"
         ), HabitRecommendDomainModel(
             title = "Читать",
-            description = "Читать ",
+            description = "Читать",
             perDayGoalCount = 25,
             endGoalCount = 100,
             frequency = Frequency.WEEK,
-            timestamp = ZonedDateTime.now().toString().toLong(),
+            perDayGoalType = PerDayGoalType.HOUR,
+            timestamp = 2022L,
             color = "Blue",
             isComplete = false,
-            category = "Our choice"
+            category = "Study"
         )
     )
 
     override suspend fun doWork() = coroutineScope {
-        try {
-            val database = EightyTwentyDatabase.invoke(applicationContext)
-            database.getHabitRecommendDao()
-                .insertHabitRecommends(habitRecommends.map { it.asEntity() })
-            Log.d("Worker", "doWork method was called successfully")
-            Result.success()
-        } catch (e: Exception) {
-            Result.failure()
+        withContext(Dispatchers.IO) {
+            try {
+                val database = EightyTwentyDatabase.invoke(applicationContext)
+                database.getHabitRecommendDao().insertHabitRecommends(habitRecommends.map { it.asEntity() })
+                Result.success()
+            } catch (e: Exception) {
+                Result.failure()
+            }
         }
     }
 }
