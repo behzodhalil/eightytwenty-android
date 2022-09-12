4e533af1aa352dba73bfa15c12fae4e7f375df08
Behod Halil <behzoddev@gmail.com>
2022-09-12 13:11:05 +0900

    [ui::] Update HabitRecommendAdapter.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendAdapter.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendAdapter.kt
@@ -1,18 +1,24 @@
 package uz.behzod.eightytwenty.features.habit_recommend
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import uz.behzod.eightytwenty.databinding.ViewHolderRecommendHabitBinding
 import uz.behzod.eightytwenty.domain.model.HabitRecommendDomainModel
 
-class HabitRecommendAdapter {
+class HabitRecommendAdapter(private val onClickListener: (data: HabitRecommendDomainModel) -> Unit) :
+    ListAdapter<HabitRecommendDomainModel, HabitRecommendAdapter.HabitRecommendViewHolder>(
+        COMPARATOR
+    ) {
 
     inner class HabitRecommendViewHolder(
-        binding: ViewHolderRecommendHabitBinding
-    ): RecyclerView.ViewHolder(binding.root)
+        val binding: ViewHolderRecommendHabitBinding
+    ) : RecyclerView.ViewHolder(binding.root)
 
     companion object {
-        private val COMPARATOR = object: DiffUtil.ItemCallback<HabitRecommendDomainModel>() {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<HabitRecommendDomainModel>() {
             override fun areItemsTheSame(
                 oldItem: HabitRecommendDomainModel,
                 newItem: HabitRecommendDomainModel
@@ -25,6 +31,27 @@ class HabitRecommendAdapter {
                 newItem: HabitRecommendDomainModel
             ): Boolean {
                 return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitRecommendViewHolder {
+        val binding = ViewHolderRecommendHabitBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return HabitRecommendViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HabitRecommendViewHolder, position: Int) {
+        holder.apply {
+            currentList[position].let { data ->
+                binding.apply {
+                    tvHabitTitle.text = data.title
+                    tvHabitCount.text = data.perDayGoalCount.toString()
+                    root.setOnClickListener {
+                        onClickListener(data)
+                    }
+                }
             }
         }
     }
