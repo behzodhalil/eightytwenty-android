9ffccdcfaa587af2ce7d1f3209efac04d0d07f26
Behod Halil <behzoddev@gmail.com>
2022-09-12 20:56:42 +0900

    [feat::] Update HabitRecommendEntity.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/entities/HabitRecommendEntity.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/HabitRecommendEntity.kt
@@ -3,7 +3,6 @@ package uz.behzod.eightytwenty.data.local.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import uz.behzod.eightytwenty.data.local.dao.HabitRecommendDao
 import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity.Companion.TABLE_NAME
 import uz.behzod.eightytwenty.domain.model.HabitRecommendDomainModel
 import uz.behzod.eightytwenty.utils.ext.Empty
@@ -25,6 +24,9 @@ data class HabitRecommendEntity(
     @ColumnInfo(name = END_GOAL_COUNT)
     val endGoalCount: Long = Long.Zero,
 
+    @ColumnInfo(name = GOAL_TYPE )
+    val perDayGoalType: PerDayGoalType = PerDayGoalType.ONCE,
+
     @ColumnInfo(name = FREQUENCY)
     val frequency: Frequency = Frequency.DAILY,
 
@@ -45,13 +47,14 @@ data class HabitRecommendEntity(
     val uid: Long = Long.Zero
 ) {
     companion object {
-        const val TABLE_NAME = "habit_recommend_table_name"
+        const val TABLE_NAME = "habit_recommend_table"
         private const val TITLE = "title"
         private const val DESCRIPTION = "description"
         private const val PER_DAY_GOAL_COUNT = "per_day_goal_count"
         private const val END_GOAL_COUNT = "end_goal_count"
         private const val FREQUENCY = "frequency"
         private const val TIMESTAMP = "timestamp"
+        private const val GOAL_TYPE = "per_day_goal_type"
         private const val COLOR = "color"
         private const val IS_COMPLETE = "is_complete"
         private const val CATEGORY = "category"
@@ -64,6 +67,7 @@ fun HabitRecommendEntity.asDomain(): HabitRecommendDomainModel {
         this.title,
         this.description,
         this.perDayGoalCount,
+        this.perDayGoalType,
         this.endGoalCount,
         this.frequency,
         this.timestamp,
@@ -80,6 +84,7 @@ fun HabitRecommendDomainModel.asEntity(): HabitRecommendEntity {
         this.description,
         this.perDayGoalCount,
         this.endGoalCount,
+        this.perDayGoalType,
         this.frequency,
         this.timestamp,
         this.color,
