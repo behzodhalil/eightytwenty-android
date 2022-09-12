d772abf931dcc56c18cf943230cf51d5c29f5828
Behod Halil <behzoddev@gmail.com>
2022-09-12 20:58:17 +0900

    [feat::] Add lists perDayGoalTypes

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_habit/PerDayGoalType.kt
@@ -0,0 +1,11 @@
+package uz.behzod.eightytwenty.features.new_habit
+
+import uz.behzod.eightytwenty.data.local.entities.PerDayGoalType
+
+val perDayGoalType = listOf(
+    "Час",
+    "Раз",
+    "Стакан",
+    "Страница",
+    "Минута"
+)
