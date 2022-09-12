32c47e0ef0ae3f73a72164448b7f04374f2a4bb7
Behod Halil <behzoddev@gmail.com>
2022-09-12 23:30:17 +0900

    [feat::] Update HabitFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitFragment.kt
@@ -1,15 +1,24 @@
 package uz.behzod.eightytwenty.features.habit
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.PagerSnapHelper
 import com.tejpratapsingh.recyclercalendar.model.RecyclerCalendarConfiguration
+import com.tejpratapsingh.recyclercalendar.utilities.CalendarUtils
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentHabitBinding
 import uz.behzod.eightytwenty.utils.view.viewBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 @AndroidEntryPoint
@@ -17,6 +26,10 @@ class HabitFragment : Fragment(R.layout.fragment_habit) {
 
     private val binding by viewBinding(FragmentHabitBinding::bind)
     private lateinit var horizontalAdapter: HorizontalCalendarAdapter
+
+    private lateinit var adapter: HabitAdapter
+
+    private val viewModel: HabitViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,8 +39,11 @@ class HabitFragment : Fragment(R.layout.fragment_habit) {
     }
 
     private fun setupUI() {
+        initRecyclerView()
+        fetchHabitsByDate()
         onNavigateHabitRecommend()
     }
+
     private fun setupRecyclerConfiguration() {
 
         val date = Date()
@@ -56,7 +72,14 @@ class HabitFragment : Fragment(R.layout.fragment_habit) {
 
             dateSelectListener = object : HorizontalCalendarAdapter.OnDateSelected {
                 override fun onDateSelected(date: Date) {
-
+                    val format = SimpleDateFormat("YYYY-MM-DD")
+                    val timestamp = CalendarUtils.dateStringFromFormat(
+                        locale = configuration.calendarLocale,
+                        date = date,
+                        format = "yyyy-MM-dd"
+                    )
+                    Log.d("Tag","Current date is $timestamp")
+                    viewModel.fetchHabitsByDate(timestamp!!)
                 }
             }
         )
@@ -65,6 +88,36 @@ class HabitFragment : Fragment(R.layout.fragment_habit) {
 
         val snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(binding.rvCalendar)
+    }
+
+    private fun initRecyclerView() {
+        adapter = HabitAdapter()
+        binding.rvHabits.adapter = adapter
+    }
+
+    private fun fetchHabitsByDate() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    when(state) {
+                        is HabitUIState.Empty -> {
+
+                        }
+                        is HabitUIState.Failure -> {
+
+                        }
+                        is HabitUIState.Loading -> {
+
+                        }
+                        is HabitUIState.Success -> {
+                            Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+                            adapter.submitList(state.data)
+                            Log.d("Tag","Result data is ${state.data}")
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun onNavigateHabitRecommend() {
