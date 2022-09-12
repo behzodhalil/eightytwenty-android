9899882da108b2e1a01a5740a7977ad066de1a97
Behod Halil <behzoddev@gmail.com>
2022-09-12 13:11:35 +0900

    [ui::] Update HabitRecommendRepository.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/repository/HabitRecommendRepository.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/HabitRecommendRepository.kt
@@ -5,6 +5,7 @@ import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity
 
 interface HabitRecommendRepository {
     suspend fun insertHabitRecommend(habitRecommend: HabitRecommendEntity)
+    suspend fun insertHabitRecommends(list: List<HabitRecommendEntity>)
     suspend fun updateHabitRecommend(habitRecommend: HabitRecommendEntity)
     suspend fun deleteHabitRecommend(habitRecommend: HabitRecommendEntity)
     fun fetchHabitRecommendsByCategory(category: String): Flow<List<HabitRecommendEntity>>
