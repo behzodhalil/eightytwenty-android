aceb3384df03631116bdb2b38d75d9795a1249ae
Behod Halil <behzoddev@gmail.com>
2022-09-12 13:13:31 +0900

    [feat::] Update EightyTwentyApplication.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/EightyTwentyApplication.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/EightyTwentyApplication.kt
@@ -1,7 +1,20 @@
 package uz.behzod.eightytwenty.features
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class EightyTwentyApplication: Application()
+class EightyTwentyApplication: Application(),Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .setWorkerFactory(workerFactory).build()
+    }
+}
