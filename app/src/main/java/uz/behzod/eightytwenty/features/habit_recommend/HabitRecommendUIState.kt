b2d6d5c6c91f5d0c15bc0a603eefb966db2c7f7c
Behod Halil <behzoddev@gmail.com>
2022-09-12 13:11:46 +0900

    [ui::] Update HabitRecommendUIState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendUIState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendUIState.kt
@@ -1,3 +1,10 @@
 package uz.behzod.eightytwenty.features.habit_recommend
 
-sealed class HabitRecommendUIState
+import uz.behzod.eightytwenty.domain.model.HabitRecommendDomainModel
+
+sealed class HabitRecommendUIState {
+    data class Success(val data: List<HabitRecommendDomainModel>): HabitRecommendUIState()
+    data class Failure(val message: String): HabitRecommendUIState()
+    object Empty: HabitRecommendUIState()
+    object Loading: HabitRecommendUIState()
+}
