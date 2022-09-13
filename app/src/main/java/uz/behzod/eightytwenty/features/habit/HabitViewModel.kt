36ca0c3aef829e38ff38760b9f38f59cf84fa584
Behod Halil <behzoddev@gmail.com>
2022-09-13 09:15:11 +0900

    [feat::] Update HabitViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitViewModel.kt
@@ -1,10 +1,11 @@
 package uz.behzod.eightytwenty.features.habit
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.util.Log
+import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import uz.behzod.eightytwenty.domain.interactor.habit.FetchHabitsByDate
+import uz.behzod.eightytwenty.domain.model.HabitDomainModel
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,14 +16,35 @@ class HabitViewModel @Inject constructor(
     private var _uiState: MutableStateFlow<HabitUIState> = MutableStateFlow(HabitUIState.Loading)
     val uiState: Flow<HabitUIState> = _uiState.asStateFlow()
 
+    val mutableDates : MutableLiveData<String> = MutableLiveData()
+
+    val habits: LiveData<List<HabitDomainModel>> = Transformations.switchMap(mutableDates) { date ->
+        iFetchHabitsByDate.invoke(date).asLiveData()
+
+    }
+
+    fun searchByTimestamp(timestamp: String) {
+        mutableDates.value = timestamp
+    }
+
     fun fetchHabitsByDate(date: String) {
         iFetchHabitsByDate.invoke(date).onEach { habit ->
-            _uiState.value = HabitUIState.Loading
-            if (habit.isNotEmpty()) {
-                _uiState.value = HabitUIState.Success(habit)
-            } else {
-                _uiState.value = HabitUIState.Empty
+            _uiState.update {
+               HabitUIState.Loading
             }
-        }.launchIn(viewModelScope)
+            if (habit.isNotEmpty()) {
+                _uiState.update {
+                    HabitUIState.Success(habit)
+                }
+            } else if (habit.isNullOrEmpty()) {
+                _uiState.update {
+                    HabitUIState.Empty
+                }
+            }
+        }.catch {
+            Log.d("Tag","Error is occured")
+        }
+            .launchIn(viewModelScope)
     }
+
 }
