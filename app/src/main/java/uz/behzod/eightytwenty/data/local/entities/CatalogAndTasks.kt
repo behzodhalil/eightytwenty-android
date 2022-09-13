4eb94f10539287a5505cfcbfb1c0e6e11e59b39a
Behod Halil <behzoddev@gmail.com>
2022-09-13 20:06:08 +0900

    [feat::] Add CatalogAndTasks.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/CatalogAndTasks.kt
@@ -0,0 +1,13 @@
+package uz.behzod.eightytwenty.data.local.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class CatalogAndTasks(
+    @Embedded val catalogEntity: TaskCatalogEntity,
+    @Relation(
+        parentColumn = "catalog_uid",
+        entityColumn = "task_catalog_uid"
+    )
+    val tasks: List<TaskEntity>
+)
