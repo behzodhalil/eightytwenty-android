dfc915cc8aab9ee497f25b62997ac97a961042f9
Behzod Halil <behzoddev@gmail.com>
2022-09-13 18:01:34 +0900

    [feat::] Update HabitDetailViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit_detail/HabitDetailViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit_detail/HabitDetailViewModel.kt
@@ -1,4 +1,30 @@
 package uz.behzod.eightytwenty.features.habit_detail
 
-class HabitDetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import uz.behzod.eightytwenty.domain.interactor.habit.FetchHabitByUid
+import uz.behzod.eightytwenty.domain.model.HabitDomainModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HabitDetailViewModel @Inject constructor(
+    private val iFetchHabitByUid: FetchHabitByUid
+) : ViewModel() {
+
+    private var _uiState: MutableStateFlow<HabitDetailUiState> =
+        MutableStateFlow(HabitDetailUiState.Loading)
+    val uiState: Flow<HabitDetailUiState> = _uiState.asStateFlow()
+
+    fun fetchHabitByUid(uid: Long) {
+        iFetchHabitByUid.invoke(uid).onEach { habit: HabitDomainModel? ->
+            _uiState.value = HabitDetailUiState.Loading
+            if (habit!= null) {
+                _uiState.value = HabitDetailUiState.Success(habit)
+            } else {
+                _uiState.value = HabitDetailUiState.Empty
+            }
+        }.launchIn(viewModelScope)
+    }
 }
