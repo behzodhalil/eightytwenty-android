57286acaa10c86b77225b84dc3e9e935f26dfcd6
Behzod Halil <behzoddev@gmail.com>
2022-09-14 11:50:30 +0900

    [feat::] Update EightyTwentyDatabase.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/db/EightyTwentyDatabase.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/db/EightyTwentyDatabase.kt
@@ -3,22 +3,19 @@ package uz.behzod.eightytwenty.data.local.db
 import android.content.Context
 import androidx.room.*
 import androidx.room.migration.AutoMigrationSpec
-import androidx.sqlite.db.SupportSQLiteDatabase
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import uz.behzod.eightytwenty.data.local.callback.HabitRecommendCallback
 import uz.behzod.eightytwenty.data.local.dao.*
 import uz.behzod.eightytwenty.data.local.entities.*
-import uz.behzod.eightytwenty.worker.HabitRecommendWorker
 
 @Database(
     entities = [
         NoteEntity::class, NoteCategoryEntity::class,
         HabitEntity::class, HabitRecommendEntity::class,
         TaskEntity::class, TaskCatalogEntity::class],
-    version = 2,
+    version = 4,
     autoMigrations = [
         AutoMigration(
-            from = 1, to = 2
+            from = 3, to = 4, spec = SpecMigration::class
         )
     ],
     exportSchema = true
@@ -32,29 +29,6 @@ abstract class EightyTwentyDatabase : RoomDatabase() {
     abstract fun getHabitRecommendDao(): HabitRecommendDao
     abstract fun getTaskDao(): TaskDao
     abstract fun getTaskCatalogDao(): TaskCatalogDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: EightyTwentyDatabase? = null
-        private val LOCK = Any()
-
-        operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
-            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
-        }
-
-        private fun buildDatabase(context: Context) = Room.databaseBuilder(
-            context.applicationContext,
-            EightyTwentyDatabase::class.java,
-            "eighty_database"
-        ).addCallback(object : RoomDatabase.Callback() {
-            override fun onOpen(db: SupportSQLiteDatabase) {
-                super.onOpen(db)
-                val request = OneTimeWorkRequestBuilder<HabitRecommendWorker>().build()
-                WorkManager.getInstance(context).enqueue(request)
-            }
-        }).fallbackToDestructiveMigration().build()
-
-    }
 }
 
 
