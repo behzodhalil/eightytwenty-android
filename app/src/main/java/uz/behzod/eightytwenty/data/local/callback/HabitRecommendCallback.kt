1c08a85ef54185df6211ef883b8bfe4f6d20a196
Behzod Halil <behzoddev@gmail.com>
2022-09-14 11:50:44 +0900

    [feat::] Add HabitRecommendCallback.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/callback/HabitRecommendCallback.kt
@@ -0,0 +1,105 @@
+package uz.behzod.eightytwenty.data.local.callback
+
+import android.util.Log
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.data.local.db.EightyTwentyDatabase
+import uz.behzod.eightytwenty.data.local.entities.Frequency
+import uz.behzod.eightytwenty.data.local.entities.HabitRecommendEntity
+import uz.behzod.eightytwenty.data.local.entities.PerDayGoalType
+import uz.behzod.eightytwenty.data.local.entities.asEntity
+import uz.behzod.eightytwenty.di.CallbackScope
+import uz.behzod.eightytwenty.domain.model.HabitRecommendDomainModel
+import javax.inject.Inject
+import javax.inject.Provider
+
+class HabitRecommendCallback @Inject constructor(
+    private val database: Provider<EightyTwentyDatabase>,
+    @CallbackScope private val scope: CoroutineScope
+) : RoomDatabase.Callback() {
+
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+
+        Log.d("Callback","Callback is called")
+        val dao = database.get().getHabitRecommendDao()
+
+        scope.launch {
+            dao.insertHabitRecommends(habitRecommends.map { it.asEntity() })
+        }
+    }
+
+}
+
+private val habitRecommends = listOf(
+    HabitRecommendDomainModel(
+        title = "Встать раньше 7 утра",
+        description = "Встать раньше 7 утра",
+        perDayGoalCount = 1,
+        endGoalCount = 60,
+        frequency = Frequency.WEEK,
+        timestamp = 2022L,
+        perDayGoalType = PerDayGoalType.ONCE,
+        color = "Green",
+        isComplete = false,
+        category = "Our Choice"
+    ), HabitRecommendDomainModel(
+        title = "Выпить стакан воды",
+        description = "Выпить стакан воды",
+        perDayGoalCount = 1,
+        endGoalCount = 60,
+        frequency = Frequency.DAILY,
+        timestamp = 2022L,
+        perDayGoalType = PerDayGoalType.CUP,
+        color = "Blue",
+        isComplete = false,
+        category = "Our Choice"
+    ), HabitRecommendDomainModel(
+        title = "Встать раньше 7 утра",
+        description = "Встать раньше 7 утра",
+        perDayGoalCount = 1,
+        endGoalCount = 60,
+        frequency = Frequency.WEEK,
+        timestamp = 2022L,
+        perDayGoalType = PerDayGoalType.ONCE,
+        color = "Green",
+        isComplete = false,
+        category = "Health"
+    ), HabitRecommendDomainModel(
+        title = "Почистить зубы",
+        description = "Почистить зубы",
+        perDayGoalCount = 2,
+        endGoalCount = 60,
+        frequency = Frequency.WEEK,
+        timestamp = 2022L,
+        perDayGoalType = PerDayGoalType.ONCE,
+        color = "Light Green",
+        isComplete = false,
+        category = "Health"
+    ), HabitRecommendDomainModel(
+        title = "Встать раньше 7 утра",
+        description = "Встать раньше 7 утра",
+        perDayGoalCount = 1,
+        endGoalCount = 60,
+        frequency = Frequency.WEEK,
+        timestamp = 2022L,
+        perDayGoalType = PerDayGoalType.ONCE,
+        color = "Green",
+        isComplete = false,
+        category = "Our Choice"
+    ), HabitRecommendDomainModel(
+        title = "Читать",
+        description = "Читать",
+        perDayGoalCount = 25,
+        endGoalCount = 100,
+        frequency = Frequency.WEEK,
+        perDayGoalType = PerDayGoalType.HOUR,
+        timestamp = 2022L,
+        color = "Blue",
+        isComplete = false,
+        category = "Study"
+    )
+)
