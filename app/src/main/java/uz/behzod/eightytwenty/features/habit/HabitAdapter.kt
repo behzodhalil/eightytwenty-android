fe2302da6891b6afb6c1d419be6580086f13e854
Behod Halil <behzoddev@gmail.com>
2022-09-15 10:56:48 +0900

    [feat::] Update HabitAdapter.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitAdapter.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitAdapter.kt
@@ -2,34 +2,16 @@ package uz.behzod.eightytwenty.features.habit
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import uz.behzod.eightytwenty.databinding.ViewHolderHabitBinding
 import uz.behzod.eightytwenty.domain.model.HabitDomainModel
 
-class HabitAdapter (private val onClickListener: (data: HabitDomainModel) -> Unit) : ListAdapter<HabitDomainModel, HabitAdapter.HabitViewHolder>(COMPARATOR) {
+class HabitAdapter(private val onClickListener: (data: HabitDomainModel) -> Unit) :
+    ListAdapter<HabitDomainModel, HabitAdapter.HabitViewHolder>(HabitDiffUtil) {
 
     inner class HabitViewHolder(val binding: ViewHolderHabitBinding) :
         RecyclerView.ViewHolder(binding.root)
-
-    companion object {
-        private val COMPARATOR = object : DiffUtil.ItemCallback<HabitDomainModel>() {
-            override fun areItemsTheSame(
-                oldItem: HabitDomainModel,
-                newItem: HabitDomainModel
-            ): Boolean {
-                return oldItem.uid == newItem.uid
-            }
-
-            override fun areContentsTheSame(
-                oldItem: HabitDomainModel,
-                newItem: HabitDomainModel
-            ): Boolean {
-                return oldItem == newItem
-            }
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
         val binding = ViewHolderHabitBinding.inflate(
