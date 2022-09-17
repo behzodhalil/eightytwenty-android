070679876de6391fa8545df8b3029483045e2f15
Behzod Halil <behzoddev@gmail.com>
2022-09-17 16:53:27 +0900

    [refactor::] Update InsertHabit.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/InsertHabit.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/habit/InsertHabit.kt
@@ -1,7 +1,8 @@
 package uz.behzod.eightytwenty.domain.interactor.habit
 
+import uz.behzod.eightytwenty.data.local.entities.ScheduleEntity
 import uz.behzod.eightytwenty.domain.model.HabitDomainModel
 
 interface InsertHabit {
-    suspend operator fun invoke(habit: HabitDomainModel)
+    suspend operator fun invoke(habit: HabitDomainModel, scheduleEntity: List<ScheduleEntity>)
 }
