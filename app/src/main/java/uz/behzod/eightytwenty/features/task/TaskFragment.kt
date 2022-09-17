8dd53611186fac91f218f0d007a5486470831704
Behzod Halil <behzoddev@gmail.com>
2022-09-17 20:05:34 +0900

    [refactor::] Update TaskFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task/TaskFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task/TaskFragment.kt
@@ -1,4 +1,35 @@
 package uz.behzod.eightytwenty.features.task
 
-class TaskFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.databinding.FragmentTaskBinding
+import uz.behzod.eightytwenty.utils.view.viewBinding
+
+@AndroidEntryPoint
+class TaskFragment : Fragment(R.layout.fragment_task) {
+
+    private val binding by viewBinding(FragmentTaskBinding::bind)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        onNavigateNewTask()
+    }
+
+    private fun onNavigateNewTask() {
+        binding.btnNewNote.setOnClickListener {
+            val route = TaskFragmentDirections.actionTaskFragmentToNewTaskFragment()
+            findNavController().navigate(route)
+        }
+    }
+
 }
