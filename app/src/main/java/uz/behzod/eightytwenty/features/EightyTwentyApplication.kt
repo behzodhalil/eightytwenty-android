fe7909baad8aac079ec70ee40d560ccfaa277929
Behzod Halil <behzoddev@gmail.com>
2022-09-17 20:05:42 +0900

    [refactor::] Update EightyTwentyApplication.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/EightyTwentyApplication.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/EightyTwentyApplication.kt
@@ -3,8 +3,17 @@ package uz.behzod.eightytwenty.features
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 import javax.inject.Inject
+
+private const val METHOD_COUNT = 1
+private const val METHOD_OFFSET = 5
+private const val TAG = ""
 
 @HiltAndroidApp
 class EightyTwentyApplication: Application(),Configuration.Provider {
@@ -12,9 +21,29 @@ class EightyTwentyApplication: Application(),Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    override fun onCreate() {
+        super.onCreate()
+        val prettyFormatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)
+            .methodCount(METHOD_COUNT)
+            .methodOffset(METHOD_OFFSET)
+            .tag(TAG)
+            .build()
+
+        Logger.addLogAdapter(AndroidLogAdapter(prettyFormatStrategy))
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority, "-$tag", message, t)
+            }
+        })
+
+    }
+
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setMinimumLoggingLevel(android.util.Log.INFO)
             .setWorkerFactory(workerFactory).build()
     }
 }
+
