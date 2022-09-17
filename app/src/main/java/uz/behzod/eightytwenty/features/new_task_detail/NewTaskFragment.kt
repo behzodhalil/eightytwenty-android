f6b31f503dccbdc7eb2b3339e1817b1e8ca778d4
Behzod Halil <behzoddev@gmail.com>
2022-09-17 20:04:33 +0900

    [refactor::] Update NewTaskFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskFragment.kt
@@ -1,8 +1,42 @@
 package uz.behzod.eightytwenty.features.new_task_detail
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.datetime.datePicker
+import com.afollestad.materialdialogs.datetime.dateTimePicker
+import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
+import com.google.android.material.dialog.MaterialDialogs
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.data.local.entities.TaskEntity
+import uz.behzod.eightytwenty.databinding.FragmentNewTaskDetailBinding
+import uz.behzod.eightytwenty.utils.view.viewBinding
+import java.time.LocalTime
 
 @AndroidEntryPoint
-class NewTaskFragment: Fragment() {
+class NewTaskFragment: Fragment(R.layout.fragment_new_task_detail) {
+
+    private val binding by viewBinding(FragmentNewTaskDetailBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    
+    private fun setupUi() {
+        
+    }
+    
+    private fun initDateOfCompletion() {
+        binding.btnSelectDateOfCompletion.setOnClickListener { 
+            MaterialDialog(requireContext()).show { 
+                lifecycleOwner(viewLifecycleOwner)
+                dateTimePicker(
+                    requireFutureDateTime = true,
+                    currentDateTime =
+                )
+            }
+        }
+    }
 }
