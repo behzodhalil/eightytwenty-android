38bf1d5f9087b4065344fca3892ab2bacef341f6
Behzod Halil <behzoddev@gmail.com>
2022-09-17 20:04:39 +0900

    [refactor::] Update TaskDao.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/dao/TaskDao.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/dao/TaskDao.kt
@@ -2,6 +2,7 @@ package uz.behzod.eightytwenty.data.local.dao
 
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.TaskAndSchedule
 import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 
 @Dao
@@ -20,4 +21,7 @@ interface TaskDao {
 
     @Query("SELECT * FROM task_entity_table_name WHERE task_is_complete <>1 AND task_is_trashed <>1 ORDER BY task_timestamp")
     fun fetchTasks(): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM task_entity_table_name ORDER BY task_title")
+    fun fetchTaskAndSchedule(): Flow<List<TaskAndSchedule>>
 }
