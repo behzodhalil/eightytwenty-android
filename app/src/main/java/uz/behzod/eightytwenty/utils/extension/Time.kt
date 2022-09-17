909cf143add3d8480bee1be0ebf6e8fc7fe2f9ba
Behzod Halil <behzoddev@gmail.com>
2022-09-17 20:06:50 +0900

    [feat::] Add Time.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/extension/Time.kt
@@ -0,0 +1,81 @@
+package uz.behzod.eightytwenty.utils.extension
+
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.util.*
+
+fun LocalTime.asZonedDateTime(): ZonedDateTime {
+    return LocalDate
+        .now().atStartOfDay(ZoneId.systemDefault())
+        .with(this)
+}
+
+/**
+ *   An extension function used to
+ *   determine if the ZonedDateTime object
+ *   is same as the date today
+ *
+ *   @return true if the date matches from the current date
+ */
+fun ZonedDateTime.isToday(): Boolean {
+    return LocalDate.now().isEqual(this.toLocalDate())
+}
+
+/**
+ *   An extension function used to
+ *   determine if the ZonedDateTime object
+ *   is the same as the next day
+ *   @return true if the ZonedDateTime object is the
+ *           same as the next day
+ */
+fun ZonedDateTime.isTomorrow(): Boolean {
+    return LocalDate.now().plusDays(1).compareTo(this.toLocalDate()) == 0
+}
+
+/**
+ *   An extension function used to
+ *   determine if the ZonedDateTime object
+ *   is the same as the previous day
+ *   @return true if the ZonedDateTime object is the
+ *           same as the previous day
+ */
+fun ZonedDateTime.isYesterday(): Boolean {
+    return LocalDate.now().minusDays(SUBTRACT).compareTo(this.toLocalDate()) == 0
+}
+
+/**
+ *   An extension function used to determine
+ *   if the ZonedDateTime object is before
+ *   the current datetime
+ *   @return true if the current ZonedDateTime object
+ *              is before the current date-time
+ */
+fun ZonedDateTime.isBeforeNow(): Boolean {
+    return this.isBefore(ZonedDateTime.now())
+}
+
+/**
+ *   An extension function used to determine
+ *   if the ZonedDateTime object is after
+ *   the current datetime
+ *   @return true if the current ZonedDateTime object
+ *           is after the current date-time
+ */
+fun ZonedDateTime.isAfterNow(): Boolean {
+    return this.isAfter(ZonedDateTime.now())
+}
+
+/**
+ *  An extension function used to convert the
+ *  current instance of ZonedDateTime to
+ *  a legacy Calendar instance
+ *  @return calendar instance with the same values as
+ *          this instance
+ */
+fun ZonedDateTime.toCalendar(): Calendar {
+    return GregorianCalendar.from(this)
+}
+
+private const val SUBTRACT = 1L
