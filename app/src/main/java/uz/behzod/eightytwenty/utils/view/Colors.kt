b80252e89ce4768326998d2eff6af019eab11ae7
Behzod Halil <behzoddev@gmail.com>
2022-09-17 16:51:39 +0900

    [feat::] Add Colors.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/view/Colors.kt
@@ -0,0 +1,16 @@
+package uz.behzod.eightytwenty.utils.view
+
+import uz.behzod.eightytwenty.R
+
+object Colors {
+
+    val list = listOf(
+        R.color.habit_1,
+        R.color.habit_2,
+        R.color.habit_3,
+        R.color.habit_4,
+        R.color.habit_5,
+        R.color.habit_6,
+        R.color.habit_7
+    )
+}
