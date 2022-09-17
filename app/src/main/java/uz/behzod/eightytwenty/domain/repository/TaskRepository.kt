46b4eb987e2518acddf774361de9e4ed7cf93070
Behzod Halil <behzoddev@gmail.com>
2022-09-17 16:54:36 +0900

    [refactor::] Update TaskRepository.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/repository/TaskRepository.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/TaskRepository.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 
 interface TaskRepository {
-    suspend fun insertTask(task: TaskEntity)
+    suspend fun insertTask(task: TaskEntity): Long
     suspend fun updateTask(task: TaskEntity)
     suspend fun deleteTask(task: TaskEntity)
     fun fetchTaskByUid(uid: Long): Flow<List<TaskEntity>>
