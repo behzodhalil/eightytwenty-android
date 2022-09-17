ca091c1e2e49d8db94499259cfabfde459a40344
Behzod Halil <behzoddev@gmail.com>
2022-09-17 16:51:54 +0900

    [feat::] Add NewTaskFragment.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskFragment.kt
@@ -0,0 +1,8 @@
+package uz.behzod.eightytwenty.features.new_task_detail
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NewTaskFragment: Fragment() {
+}
