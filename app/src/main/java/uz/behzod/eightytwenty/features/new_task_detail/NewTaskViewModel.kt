9c1c1b64dd943a84e0c2f615d0fde22558e9aa8e
Behzod Halil <behzoddev@gmail.com>
2022-09-17 16:51:58 +0900

    [feat::] Add NewTaskViewModel.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskViewModel.kt
@@ -0,0 +1,9 @@
+package uz.behzod.eightytwenty.features.new_task_detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class NewTaskViewModel @Inject constructor(): ViewModel() {
+}
