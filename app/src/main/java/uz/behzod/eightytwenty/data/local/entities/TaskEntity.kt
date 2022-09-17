699bf10b51229ab7eb69f0a4c95ed1e25a7cb91b
Behod Halil <behzoddev@gmail.com>
2022-09-17 11:21:24 +0900

    [refactor::] Update TaskEntity.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/entities/TaskEntity.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/TaskEntity.kt
@@ -1,45 +1,32 @@
 package uz.behzod.eightytwenty.data.local.entities
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
 
 @Entity(
-    tableName = "task_entity_table_name"
+    tableName = "task_entity_table_name",
+    foreignKeys = [ForeignKey(
+        entity = TaskCatalogEntity::class,
+        parentColumns = ["catalog_uid"],
+        childColumns = ["task_catalog_uid"],
+        onDelete = CASCADE
+    )],
+    indices = [Index(value = ["task_schedule_uid"], unique = true),
+        Index(value = ["task_catalog_uid"], unique = true)]
 )
 data class TaskEntity(
-    @ColumnInfo(name = TITLE)
-    val title: String = String.Empty,
-
-    @ColumnInfo(name = DUE_DATE)
-    val dueDate: String = String.Empty,
-
-    @ColumnInfo(name = FREQUENCY)
-    val frequency: Frequency = Frequency.DAILY,
-
-    @ColumnInfo(name = TIMESTAMP)
-    val timestamp: String = String.Empty,
-
-    @ColumnInfo(name = DEADLINE)
-    val deadline: String = String.Empty,
-
-    @ColumnInfo(name = IS_COMPLETE)
-    val isComplete: Boolean = false,
-
-    @ColumnInfo(name = IS_TRASHED)
-    val isTrashed: Boolean = false,
-
-    @ColumnInfo(name = CATALOG_NAME)
-    val catalogName: String = String.Empty,
-
-    @ColumnInfo(name = CATALOG_UID)
-    val catalogUid: Long = Long.Zero,
-
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = UID)
-    val uid: Long = Long.Zero,
+    @ColumnInfo(name = TITLE) val title: String = String.Empty,
+    @ColumnInfo(name = DUE_DATE) val dueDate: String = String.Empty,
+    @ColumnInfo(name = FREQUENCY) val frequency: Frequency = Frequency.DAILY,
+    @ColumnInfo(name = TIMESTAMP) val timestamp: String = String.Empty,
+    @ColumnInfo(name = DEADLINE) val deadline: String = String.Empty,
+    @ColumnInfo(name = IS_COMPLETE) val isComplete: Boolean = false,
+    @ColumnInfo(name = IS_TRASHED) val isTrashed: Boolean = false,
+    @ColumnInfo(name = CATALOG_UID) val catalogUid: Long = Long.Zero,
+    @ColumnInfo(name = SCHEDULE_UID) val scheduleUid: Long = Long.Zero,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = UID) val uid: Long = Long.Zero,
 ) {
     companion object {
         private const val TITLE = "task_title"
@@ -49,8 +36,8 @@ data class TaskEntity(
         private const val DEADLINE = "task_deadline"
         private const val IS_COMPLETE = "task_is_complete"
         private const val IS_TRASHED = "task_is_trashed"
-        private const val CATALOG_NAME = "task_catalog_name"
         private const val CATALOG_UID = "task_catalog_uid"
-        private const val UID ="uid"
+        private const val SCHEDULE_UID = "task_schedule_uid"
+        private const val UID = "task_uid"
     }
 }
