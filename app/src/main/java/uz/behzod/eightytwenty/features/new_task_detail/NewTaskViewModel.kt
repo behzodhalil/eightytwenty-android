653543aaf4e6bada4a782c99d4030b38fda5670e
Behod Halil <behzoddev@gmail.com>
2022-09-19 07:36:36 +0900

    [refactor::] Update NewTaskViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task_detail/NewTaskViewModel.kt
@@ -2,8 +2,35 @@ package uz.behzod.eightytwenty.features.new_task_detail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import uz.behzod.eightytwenty.data.local.entities.TaskEntity
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class NewTaskViewModel @Inject constructor(): ViewModel() {
+
+    private var _task: MutableStateFlow<TaskEntity> = MutableStateFlow(value = TaskEntity())
+    val task: StateFlow<TaskEntity> = _task.asStateFlow()
+
+    fun setTask(task: TaskEntity?) {
+        task?.let {
+            _task.value = it
+        }
+    }
+
+    fun getTask(): TaskEntity? {
+        return task.value
+    }
+
+    fun setEndDate(endDate: ZonedDateTime?) {
+        val task = getTask()
+        task?.let {
+            it.endDate = endDate
+            setTask(task)
+        }
+    }
+
 }
