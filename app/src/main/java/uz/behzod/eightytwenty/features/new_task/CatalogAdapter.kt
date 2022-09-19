ec5902844d116adbe2a69cbb319474349900f8cf
Behzod Halil <behzoddev@gmail.com>
2022-09-19 17:49:35 +0900

    [feat::] Add CatalogAdapter.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task/CatalogAdapter.kt
@@ -0,0 +1,33 @@
+package uz.behzod.eightytwenty.features.new_task
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import uz.behzod.eightytwenty.data.local.entities.TaskCatalogEntity
+import uz.behzod.eightytwenty.databinding.DialogCatalogBinding
+import uz.behzod.eightytwenty.databinding.ViewHolderCatalogBinding
+
+class CatalogAdapter :
+    ListAdapter<TaskCatalogEntity, CatalogAdapter.CatalogViewHolder>(CatalogDiffUtil) {
+
+    inner class CatalogViewHolder(val binding: ViewHolderCatalogBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatalogViewHolder {
+        val binding = ViewHolderCatalogBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return CatalogViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CatalogViewHolder, position: Int) {
+        holder.apply {
+            currentList[position].let {
+                binding.apply {
+                    tvCategoryName.text = it.name
+                }
+            }
+        }
+    }
+}
