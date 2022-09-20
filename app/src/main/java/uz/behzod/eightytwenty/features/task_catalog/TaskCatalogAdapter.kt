c41202f1ec695a695133f73ce0d39e144ebbcbef
Behod Halil <behzoddev@gmail.com>
2022-09-20 16:09:22 +0900

    [feat::] Add TaskCatalogAdapter.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/task_catalog/TaskCatalogAdapter.kt
@@ -0,0 +1,33 @@
+package uz.behzod.eightytwenty.features.task_catalog
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import uz.behzod.eightytwenty.data.local.entities.TaskCatalogEntity
+import uz.behzod.eightytwenty.databinding.ViewHolderCatalogBinding
+
+class TaskCatalogAdapter :
+    ListAdapter<TaskCatalogEntity, TaskCatalogAdapter.TaskCatalogViewHolder>(TaskCatalogDiffUtil) {
+
+    inner class TaskCatalogViewHolder(
+        val binding: ViewHolderCatalogBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskCatalogViewHolder {
+        val binding = ViewHolderCatalogBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false
+        )
+        return TaskCatalogViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskCatalogViewHolder, position: Int) {
+        holder.apply {
+            currentList[position].let {
+                binding.apply {
+                    tvCategoryName.text = it.name
+                }
+            }
+        }
+    }
+}
