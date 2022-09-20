3d5599c80b4fcb526136a44fc389b51880e69a39
Behod Halil <behzoddev@gmail.com>
2022-09-20 19:37:53 +0900

    [feat::] Add SearchCatalogViewModel.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/search_catalog/SearchCatalogViewModel.kt
@@ -0,0 +1,30 @@
+package uz.behzod.eightytwenty.features.search_catalog
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
+import uz.behzod.eightytwenty.domain.interactor.task_catalog.SearchTaskCatalog
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchCatalogViewModel @Inject constructor(
+    private val iSearchCatalog: SearchTaskCatalog
+) : ViewModel() {
+
+    private var _uiState: MutableStateFlow<SearchCatalogUiState> =
+        MutableStateFlow(SearchCatalogUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    fun searchCatalog(catalogName: String) {
+        iSearchCatalog.invoke(catalogName).onEach {
+            _uiState.value = SearchCatalogUiState.Loading
+            if (it.isNotEmpty()) {
+                _uiState.value = SearchCatalogUiState.Success(it)
+            } else {
+                _uiState.value = SearchCatalogUiState.Empty
+            }
+        }
+    }
+}
