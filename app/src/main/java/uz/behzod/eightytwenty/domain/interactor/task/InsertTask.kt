8a4d3eb67d20df0e9bc74e2bef236a5dae326ad8
Behod Halil <behzoddev@gmail.com>
2022-09-21 13:45:30 +0900

    [refactor::] Update InsertTask.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/task/InsertTask.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/task/InsertTask.kt
@@ -1,4 +1,8 @@
 package uz.behzod.eightytwenty.domain.interactor.task
 
+import uz.behzod.eightytwenty.data.local.entities.TaskEntity
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+
 interface InsertTask {
+    suspend operator fun invoke(task: TaskEntity, list: List<NoteDomainModel>)
 }
