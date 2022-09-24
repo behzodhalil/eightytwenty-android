d61f2c2146822d64d07384e597c19464aca03db8
Behod Halil <behzoddev@gmail.com>
2022-09-24 17:04:29 +0900

    [refactor::] Update CategoryAndNotesUiState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note/CategoryAndNotesUiState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/CategoryAndNotesUiState.kt
@@ -1,9 +1,10 @@
 package uz.behzod.eightytwenty.features.note
 
 import uz.behzod.eightytwenty.data.local.entities.CategoryAndNotes
+import uz.behzod.eightytwenty.domain.model.CategoryAndNotesDomainModel
 
 sealed class CategoryAndNotesUiState {
-    data class Success(val data: List<CategoryAndNotes>): CategoryAndNotesUiState()
+    data class Success(val data: List<CategoryAndNotesDomainModel>): CategoryAndNotesUiState()
     data class Failure(val msg: String): CategoryAndNotesUiState()
     object Empty: CategoryAndNotesUiState()
     object Loading: CategoryAndNotesUiState()
