cf30bb04c40228bca96cc0147b8357213c2c859a
Behod Halil <behzoddev@gmail.com>
2022-09-24 17:03:03 +0900

    [refactor::] Update NewTaskFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskFragment.kt
@@ -2,21 +2,28 @@ package uz.behzod.eightytwenty.features.new_task
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.datetime.datePicker
 import com.afollestad.materialdialogs.datetime.dateTimePicker
 import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.data.local.entities.Frequency
+import uz.behzod.eightytwenty.data.local.entities.ScheduleEntity
 import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 import uz.behzod.eightytwenty.databinding.FragmentNewTaskDetailBinding
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.utils.constants.*
 import uz.behzod.eightytwenty.utils.extension.*
 import uz.behzod.eightytwenty.utils.formatter.DateTimeFormatter
 import uz.behzod.eightytwenty.utils.formatter.StringFormatter
 import uz.behzod.eightytwenty.utils.view.viewBinding
+import java.time.LocalDate
 import java.time.ZonedDateTime
 
 
@@ -30,6 +37,13 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
     private var reminder: ZonedDateTime? = null
     private var daysOfWeekModel: Int = Int.Zero
     private var frequencyTypeModel: Int = Int.Zero
+    private val timestamp: ZonedDateTime =
+        ZonedDateTime.now()
+    private var noteTitle: String = ""
+    private var noteDesc: String = ""
+    private var noteTimestamp: String = ""
+    private var noteIsTrashed: Boolean = false
+    private var catalogUid: Long = Long.Zero
 
     private var isShown = true
 
@@ -46,10 +60,11 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
         onNavigateCatalog()
         onNavigateToNewNote()
         displayNewNote()
+        insertTaskWithScheduleAndNote()
     }
 
     private fun setupUi() {
-
+        binding.tvCreateTimestamp.text = timestamp.toString()
     }
 
     private fun initDateOfCompletion() {
@@ -216,8 +231,7 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
             "TaskCatalogUid",
             viewLifecycleOwner
         ) { _, bundle ->
-            val result = bundle.getString("TaskCatalogUid")
-
+            catalogUid = bundle.getLong("TaskCatalogUid")
         }
     }
 
@@ -246,21 +260,85 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
             "NoteTitle",
             viewLifecycleOwner
         ) { _, bundle ->
-            binding.viewHolderNote.tvTitle.text = bundle.getString("NoteTitle")
+            noteTitle = bundle.getString("NoteTitle") ?: ""
+            binding.viewHolderNote.tvTitle.text = noteTitle
         }
         supportFragmentManager.setFragmentResultListener(
             "NoteDescription",
             viewLifecycleOwner
         ) { _, bundle ->
-            binding.viewHolderNote.tvDesc.text = bundle.getString("NoteDescription")
+            noteDesc = bundle.getString("NoteDescription") ?: ""
+            binding.viewHolderNote.tvDesc.text = noteDesc
         }
         supportFragmentManager.setFragmentResultListener(
             "NoteTimestamp",
             viewLifecycleOwner
         ) { _, bundle ->
-            binding.viewHolderNote.tvDate.text = bundle.getString("NoteTimestamp")
+            noteTimestamp = bundle.getString("NoteTimestamp") ?: ""
+            binding.viewHolderNote.tvDate.text = noteTimestamp
         }
         binding.cvNote.gone()
         binding.viewHolderNote.root.show()
     }
+
+    private fun insertTaskWithScheduleAndNote() {
+        binding.btnSaveNewTask.setOnClickListener {
+            setTaskAndNotesAndSchedules()
+            lifecycleScope.launch {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is NewTaskUiState.Empty -> {
+                            Toast.makeText(requireContext(), "Task is empty", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        is NewTaskUiState.Loading -> {
+
+                        }
+                        is NewTaskUiState.Success -> {
+                            Toast.makeText(requireContext(), "Task is saved", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun setTaskAndNotesAndSchedules() {
+        val scheduleEntity = ScheduleEntity(
+            frequencyTypes = frequencyTypeModel,
+            daysOfWeek = daysOfWeekModel,
+            dateOfCompletion = LocalDate.now().toString()
+        )
+
+        val note = NoteDomainModel(
+            title = noteTitle,
+            description = noteDesc,
+            timestamp = noteTimestamp.asZoneDateTime(),
+            isTrashed = false
+        )
+        val schedules = arrayListOf<ScheduleEntity>()
+
+        schedules.add(scheduleEntity)
+
+        val notes = arrayListOf<NoteDomainModel>()
+
+        notes.add(note)
+
+        viewModel.insertTaskWithScheduleAndNote(
+            TaskEntity(
+                title = binding.etNewTaskTitle.text.toString(),
+                endDate = endDate,
+                frequency = Frequency.DAILY,
+                reminderDateTime = reminder,
+                timestamp = timestamp,
+                isComplete = false,
+                isTrashed = false,
+                catalogUid = catalogUid
+            ), notes, schedules
+        )
+
+    }
+
 }
