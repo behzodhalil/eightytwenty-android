238a1c8b524732d211aa88d6d6906aaa78358f17
Behod Halil <behzoddev@gmail.com>
2022-09-24 17:01:01 +0900

    [refactor::] Update TaskViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task/TaskViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task/TaskViewModel.kt
@@ -1,9 +1,32 @@
 package uz.behzod.eightytwenty.features.task
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.domain.interactor.task.FetchTasks
 import javax.inject.Inject
 
 @HiltViewModel
-class TaskViewModel @Inject constructor(): ViewModel() {
+class TaskViewModel @Inject constructor(
+    private val iFetchTasks: FetchTasks
+): ViewModel() {
+
+    private var _uiState: MutableStateFlow<TaskUiState> = MutableStateFlow(loading())
+    val uiState: Flow<TaskUiState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            iFetchTasks.invoke().collect {
+                _uiState.value = TaskUiState.Loading
+                if (it.isEmpty()) {
+                    _uiState.value = empty()
+                } else {
+                    _uiState.value = TaskUiState.Success(it)
+                }
+            }
+        }
+    }
+
 }
