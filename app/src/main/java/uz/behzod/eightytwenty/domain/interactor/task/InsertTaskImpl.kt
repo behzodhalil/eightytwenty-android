4c7f5d24d71d6501147e6633368639fc1eba1daf
Behod Halil <behzoddev@gmail.com>
2022-09-24 17:03:45 +0900

    [refactor::] Update InsertTaskImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/task/InsertTaskImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/task/InsertTaskImpl.kt
@@ -1,26 +1,40 @@
 package uz.behzod.eightytwenty.domain.interactor.task
 
 import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.ScheduleEntity
 import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 import uz.behzod.eightytwenty.data.local.entities.asEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.domain.repository.NoteRepository
+import uz.behzod.eightytwenty.domain.repository.ScheduleRepository
 import uz.behzod.eightytwenty.domain.repository.TaskRepository
+import uz.behzod.eightytwenty.utils.extension.debugger
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
 import javax.inject.Inject
 
 class InsertTaskImpl @Inject constructor(
     private val repository: TaskRepository,
     private val noteRepository: NoteRepository,
+    private val scheduleRepository: ScheduleRepository,
     private val dispatchers: IDispatcherProvider
 ) : InsertTask {
 
-    override suspend fun invoke(task: TaskEntity, list: List<NoteDomainModel>) {
+    override suspend fun invoke(
+        task: TaskEntity,
+        noteList: List<NoteDomainModel>,
+        scheduleList: List<ScheduleEntity>
+    ) {
         return withContext(dispatchers.io) {
             val data = repository.insertTask(task)
-            list.forEach {
+            scheduleList.forEach {
+                scheduleRepository.insertSchedule(it.copy(taskUid = data))
+            }
+            noteList.forEach {
                 noteRepository.insertNote(it.copy(taskUid = data).asEntity())
             }
+
+            debugger { "Inserted with note and task is $scheduleList and $noteList" }
         }
     }
+
 }
