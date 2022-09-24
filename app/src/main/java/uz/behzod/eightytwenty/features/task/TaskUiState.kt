14140efae2e3185180b2b8c847950196097c4901
Behod Halil <behzoddev@gmail.com>
2022-09-24 17:00:51 +0900

    [refactor::] Update TaskUiState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task/TaskUiState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task/TaskUiState.kt
@@ -1,3 +1,22 @@
 package uz.behzod.eightytwenty.features.task
 
-sealed class TaskUiState
+import uz.behzod.eightytwenty.data.local.entities.TaskEntity
+
+sealed class TaskUiState {
+    data class Success(val data: List<TaskEntity>): TaskUiState()
+    data class Failure(val msg: String): TaskUiState()
+    object Empty: TaskUiState()
+    object Loading: TaskUiState()
+}
+
+fun success(data: List<TaskEntity>): TaskUiState {
+    return TaskUiState.Success(data)
+}
+
+fun loading(): TaskUiState {
+    return TaskUiState.Loading
+}
+
+fun empty(): TaskUiState {
+    return TaskUiState.Empty
+}
