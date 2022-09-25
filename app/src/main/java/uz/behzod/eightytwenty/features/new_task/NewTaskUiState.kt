996c039723af44084810a52d4961f9d7c4f2d572
Behod Halil <behzoddev@gmail.com>
2022-09-26 08:01:15 +0900

    [refactor::] Update NewTaskUiState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskUiState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskUiState.kt
@@ -1,5 +1,6 @@
 package uz.behzod.eightytwenty.features.new_task
 
+import uz.behzod.eightytwenty.data.local.entities.AttachmentEntity
 import uz.behzod.eightytwenty.data.local.entities.ScheduleEntity
 import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
@@ -8,7 +9,8 @@ sealed class NewTaskUiState {
     data class Success(
         val data: TaskEntity,
         val schedule: List<ScheduleEntity>,
-        val note: List<NoteDomainModel>
+        val note: List<NoteDomainModel>,
+        val attachments: List<AttachmentEntity>
     ) : NewTaskUiState()
     object Empty: NewTaskUiState()
     object Loading: NewTaskUiState()
