a048454b83120d20da470f598236f939acb25eef
Behod Halil <behzoddev@gmail.com>
2022-09-26 08:01:51 +0900

    [feat::] Add AttachmentRepositoryImpl.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/repository/AttachmentRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package uz.behzod.eightytwenty.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.AttachmentEntity
+import uz.behzod.eightytwenty.data.source.LocalSourceManager
+import uz.behzod.eightytwenty.domain.repository.AttachmentRepository
+import javax.inject.Inject
+
+class AttachmentRepositoryImpl @Inject constructor(
+    private val sourceManager: LocalSourceManager
+): AttachmentRepository {
+
+    override suspend fun insertAttachment(attachment: AttachmentEntity) {
+        return sourceManager.insertAttachment(attachment)
+    }
+
+    override suspend fun updateAttachment(attachment: AttachmentEntity) {
+        return sourceManager.updateAttachment(attachment)
+    }
+
+    override suspend fun deleteAttachment(attachment: AttachmentEntity) {
+        return sourceManager.deleteAttachment(attachment)
+    }
+
+    override fun fetchAttachmentByUid(attachmentUid: Long) {
+        return sourceManager.fetchAttachmentByUid(attachmentUid)
+    }
+
+    override fun fetchAttachments(): Flow<List<AttachmentEntity>> {
+        return sourceManager.fetchAttachments()
+    }
+}
