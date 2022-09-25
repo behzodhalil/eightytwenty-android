d9ce7fb8504507bc15980e32de36321f763ade34
Behod Halil <behzoddev@gmail.com>
2022-09-26 08:01:44 +0900

    [feat::] Add AttachmentRepository.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/AttachmentRepository.kt
@@ -0,0 +1,17 @@
+package uz.behzod.eightytwenty.domain.repository
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.AttachmentEntity
+
+interface AttachmentRepository {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAttachment(attachment: AttachmentEntity)
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateAttachment(attachment: AttachmentEntity)
+    suspend fun deleteAttachment(attachment: AttachmentEntity)
+    fun fetchAttachmentByUid(attachmentUid: Long)
+    fun fetchAttachments(): Flow<List<AttachmentEntity>>
+}
