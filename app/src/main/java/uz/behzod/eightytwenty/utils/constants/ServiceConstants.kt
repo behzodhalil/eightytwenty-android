99b079a5bbd6c26d09c62df79a6a8a6bf455d3d4
Behzod Halil <behzoddev@gmail.com>
2022-09-26 17:10:49 +0900

    [feat::] Add ServiceConstants.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/constants/ServiceConstants.kt
@@ -0,0 +1,9 @@
+package uz.behzod.eightytwenty.utils.constants
+
+object  ServiceConstants {
+    const val SERVICE_ACTION_START = "SERVICE_ACTION_START"
+    const val SERVICE_ACTION_CANCEL = "SERVICE_ACTION_CANCEL"
+    const val SERVICE_ACTION_WITH_BROADCAST = "SERVICE_ACTION_WITH_BROADCAST"
+    const val SERVICE_EXTRA_UID = "SERVICE_EXTRA_UID"
+    const val FILE_ATTACHMENT = "FILE_ATTACHMENT"
+}
