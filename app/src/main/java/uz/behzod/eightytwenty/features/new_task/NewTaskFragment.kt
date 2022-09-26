b6650f9b6f5bb90027ff744793ec7114afab18f3
Behzod Halil <behzoddev@gmail.com>
2022-09-26 17:10:01 +0900

    [refactor::] Update NewTaskFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_task/NewTaskFragment.kt
@@ -1,11 +1,19 @@
 package uz.behzod.eightytwenty.features.new_task
 
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.datetime.datePicker
 import com.afollestad.materialdialogs.datetime.dateTimePicker
@@ -19,12 +27,21 @@ import uz.behzod.eightytwenty.data.local.entities.TaskEntity
 import uz.behzod.eightytwenty.databinding.FragmentNewTaskDetailBinding
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.utils.constants.*
+import uz.behzod.eightytwenty.utils.constants.BroadcastConstants.BROADCAST_ACTION_IMPORT_SUCCESS
+import uz.behzod.eightytwenty.utils.constants.BroadcastConstants.BROADCAST_INTENT_DATA
+import uz.behzod.eightytwenty.utils.constants.BroadcastConstants.BROADCAST_INTENT_EXTRA
+import uz.behzod.eightytwenty.utils.constants.BroadcastConstants.BROADCAST_INTENT_STATUS
+import uz.behzod.eightytwenty.utils.constants.ServiceConstants.SERVICE_ACTION_START
+import uz.behzod.eightytwenty.utils.constants.ServiceConstants.SERVICE_ACTION_WITH_BROADCAST
+import uz.behzod.eightytwenty.utils.constants.ServiceConstants.SERVICE_EXTRA_UID
 import uz.behzod.eightytwenty.utils.extension.*
 import uz.behzod.eightytwenty.utils.formatter.DateTimeFormatter
 import uz.behzod.eightytwenty.utils.formatter.StringFormatter
+import uz.behzod.eightytwenty.utils.services.FileImportService
 import uz.behzod.eightytwenty.utils.view.viewBinding
 import java.time.LocalDate
 import java.time.ZonedDateTime
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -45,29 +62,60 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
     private var noteIsTrashed: Boolean = false
     private var catalogUid: Long = Long.Zero
 
+    private var attachmentName = String.Empty
+
     private var isShown = true
 
+    private lateinit var attachmentLauncher: ActivityResultLauncher<Intent>
     private var task: TaskEntity? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        attachmentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val attachmentId = UUID.randomUUID().toString()
+                    context?.startService(Intent(context, FileImportService::class.java).apply {
+                        action = SERVICE_ACTION_START
+                        data = it.data?.data
+                        putExtra(SERVICE_EXTRA_UID, attachmentId)
+                    })
+                }
+            }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initDateOfCompletion()
-        initReminder()
-        initRepeat()
         initSchedule()
 
-        onNavigateCatalog()
+        setupUi()
+
+        showCompletionDate()
+        showReminder()
+        showRepeat()
+        showFolder()
+        showNewNote()
+
         onNavigateToNewNote()
-        displayNewNote()
+        onOpenDocument()
+
         insertTaskWithScheduleAndNote()
     }
 
-    private fun setupUi() {
-        binding.tvCreateTimestamp.text = timestamp.toString()
+    override fun onStart() {
+        super.onStart()
+
+        LocalBroadcastManager.getInstance(requireContext())
+            .registerReceiver(receiver, IntentFilter(SERVICE_ACTION_WITH_BROADCAST))
+
     }
 
-    private fun initDateOfCompletion() {
+    private fun setupUi() {
+        binding.tvTimestamp.text = timestamp.toString()
+    }
+
+    private fun showCompletionDate() {
         binding.btnSelectDateOfCompletion.setOnClickListener {
             MaterialDialog(requireContext()).show {
                 lifecycleOwner(viewLifecycleOwner)
@@ -86,26 +134,25 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
         }
     }
 
-    private fun initReminder() {
-        with(binding.btnSelectReminder) {
-            setOnClickListener {
-                MaterialDialog(requireContext()).show {
-                    lifecycleOwner(viewLifecycleOwner)
-                    dateTimePicker(
-                        requireFutureDateTime = true,
-                        currentDateTime = ZonedDateTime.now().toCalendar()
-                    ) { _, timestamp ->
-                        reminder = timestamp.toZonedDateTime()
-                    }
-                    positiveButton(R.string.text_btn_done) {
-                        text = reminder?.format(DateTimeFormatter.asTime(requireContext(), true))
-                    }
+    private fun showReminder() {
+        binding.btnSelectReminder.setOnClickListener {
+            MaterialDialog(requireContext()).show {
+                lifecycleOwner(viewLifecycleOwner)
+                dateTimePicker(
+                    requireFutureDateTime = true,
+                    currentDateTime = ZonedDateTime.now().toCalendar()
+                ) { _, timestamp ->
+                    reminder = timestamp.toZonedDateTime()
+                }
+                positiveButton(R.string.text_btn_done) {
+                    binding.btnSelectReminder.text =
+                        reminder?.format(DateTimeFormatter.asTime(requireContext(), true))
                 }
             }
         }
     }
 
-    private fun initRepeat() {
+    private fun showRepeat() {
         with(binding) {
             btnSelectRepeat.setOnClickListener {
                 isShown = if (isShown) {
@@ -120,10 +167,11 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
                     btnCancelSchedule.gone()
                     true
                 }
-
             }
         }
+
     }
+
 
     private fun initSchedule() = with(binding.viewHolderSchedule) {
 
@@ -200,17 +248,15 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
 
     }
 
-    private fun onNavigateCatalog() {
-        with(binding.btnSelectFolder) {
-            setOnClickListener {
-                val dialog = CatalogDialog()
-                transaction(dialog)
-                dialog.setFetchingNameAndUid(object : CatalogDialog.CatalogDialogListener {
-                    override fun fetchNameAndUid() {
-                        onUpdateCatalogNameAndUid()
-                    }
-                })
-            }
+    private fun showFolder() {
+        binding.btnSelectFolder.setOnClickListener {
+            val dialog = CatalogDialog()
+            transaction(dialog)
+            dialog.setFetchingNameAndUid(object : CatalogDialog.CatalogDialogListener {
+                override fun fetchNameAndUid() {
+                    onUpdateCatalogNameAndUid()
+                }
+            })
         }
     }
 
@@ -242,7 +288,7 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
         }
     }
 
-    private fun displayNewNote() {
+    private fun showNewNote() {
         binding.cvNote.setOnClickListener {
             val screen = NewNoteDialog()
             transaction(screen)
@@ -252,7 +298,6 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
                 }
             })
         }
-
     }
 
     private fun listenerNewNote() {
@@ -261,7 +306,7 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
             viewLifecycleOwner
         ) { _, bundle ->
             noteTitle = bundle.getString("NoteTitle") ?: ""
-            binding.viewHolderNote.tvTitle.text = noteTitle
+            setNoteTitle(noteTitle)
         }
         supportFragmentManager.setFragmentResultListener(
             "NoteDescription",
@@ -275,8 +320,12 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
             viewLifecycleOwner
         ) { _, bundle ->
             noteTimestamp = bundle.getString("NoteTimestamp") ?: ""
-            binding.viewHolderNote.tvDate.text = noteTimestamp
+            setNoteTimestamp(noteTimestamp)
         }
+        showSingleNote()
+    }
+
+    private fun showSingleNote() {
         binding.cvNote.gone()
         binding.viewHolderNote.root.show()
     }
@@ -341,19 +390,57 @@ class NewTaskFragment : Fragment(R.layout.fragment_new_task_detail) {
 
     }
 
-    private fun onNavigateToAttachment() {
-
-    }
-
-    private fun showAttachmentUi() {
+    private fun onOpenDocument() {
         val isReadPermission = viewModel.readStorePermission()
 
         if (isReadPermission) {
-            MaterialDialog(requireContext()).show {
-                lifecycleOwner(viewLifecycleOwner)
+            attachmentLauncher.launch(
+                Intent(Intent.ACTION_OPEN_DOCUMENT).setType(INTENT_OPEN_DOCUMENT_TYPE)
+            )
+        }
+    }
 
+    private fun setNoteTitle(title: String) {
+        binding.viewHolderNote.tvTitle.text = title
+    }
+
+    private fun setNoteTimestamp(timestamp: String) {
+        binding.viewHolderNote.tvDate.text = timestamp
+    }
+
+    private fun setNoteDesc(description: String) {
+
+    }
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == SERVICE_ACTION_WITH_BROADCAST) {
+                when (intent.getStringExtra(BROADCAST_INTENT_STATUS)) {
+                    BROADCAST_ACTION_IMPORT_SUCCESS -> {
+                        if (intent.hasExtra(BROADCAST_INTENT_DATA)
+                            && intent.hasExtra(BROADCAST_INTENT_EXTRA)
+                        ) {
+
+                            val id = intent.getStringExtra(BROADCAST_INTENT_DATA)
+                            attachmentName =
+                                intent.getStringExtra(BROADCAST_INTENT_EXTRA) ?: ""
+
+                            setAttachmentName(attachmentName)
+
+                        }
+                    }
+                }
             }
         }
     }
 
+    private fun setAttachmentName(name: String) {
+        binding.cvAttachment.gone()
+        binding.viewHolderAttachment.root.show()
+        binding.viewHolderAttachment.tvAttachmentName.text = name
+    }
+
+    companion object {
+        private const val INTENT_OPEN_DOCUMENT_TYPE = "*/*"
+    }
 }
