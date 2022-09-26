89a7aea5aa3e57bbb1b1fb86025aae9c09d6be2f
Behzod Halil <behzoddev@gmail.com>
2022-09-26 17:10:44 +0900

    [feat::] Add FragmentResultApiConstants.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/constants/FragmentResultApiConstants.kt
@@ -0,0 +1,16 @@
+package uz.behzod.eightytwenty.utils.constants
+
+object FragmentResultApiConstants {
+    const val KEY_NOTE_TITLE = "key_note_title"
+    const val ARG_NOTE_TITLE = "arg_note_title"
+    const val KEY_NOTE_DESC = "key_note_desc"
+    const val ARG_NOTE_DESC = "arg_note_desc"
+    const val KEY_NOTE_TIMESTAMP = "key_note_timestamp"
+    const val ARG_NOTE_TIMESTAMP = "arg_note_timestamp"
+
+    const val KEY_TASK_UID = "key_task_uid"
+    const val ARG_TASK_UID = "arg_task_uid"
+    const val KEY_TASK_NAME = "key_task_name"
+    const val ARG_TASK_NAME = "arg_task_name"
+
+}
