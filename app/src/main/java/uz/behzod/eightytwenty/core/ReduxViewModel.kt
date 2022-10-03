6d2084d74b1d8cf6293b28d892dc1e2b472494ad
behzoddev <behzoddev@gmail.com>
2022-10-03 18:02:26 +0900

    [refactor::] Update ReduxViewModel.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/core/ReduxViewModel.kt
@@ -0,0 +1,26 @@
+package uz.behzod.eightytwenty.core
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import uz.behzod.eightytwenty.core.state.ViewEffect
+import uz.behzod.eightytwenty.core.state.ViewState
+
+abstract class ReduxViewModel<VS: ViewState,VE: ViewEffect>(initialState: VS): ViewModel() {
+
+    private var _state: MutableStateFlow<VS> = MutableStateFlow(initialState)
+    val state: StateFlow<VS> = _state.asStateFlow()
+
+    private var _viewEffect: Channel<VE> = Channel(Channel.BUFFERED)
+    val viewEffect: Flow<VE> = _viewEffect.receiveAsFlow()
+
+    val currentState: VS get() = state.value
+
+    protected fun modifyState (
+        action: (initialState: VS) -> VS
+    ): VS {
+        return _state.updateAndGet(action)
+    }
+
+    
+}
