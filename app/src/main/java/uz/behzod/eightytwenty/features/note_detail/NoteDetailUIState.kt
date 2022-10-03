bda91eca97713d5310de8d7c83002740445d9399
behzoddev <behzoddev@gmail.com>
2022-10-03 18:04:28 +0900

    [refactor::] Update NoteDetailUIState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailUIState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailUIState.kt
@@ -1,7 +1,10 @@
 package uz.behzod.eightytwenty.features.note_detail
 
+import uz.behzod.eightytwenty.core.state.ViewState
 import uz.behzod.eightytwenty.data.local.entities.NoteEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.utils.extension.Empty
+import java.sql.Timestamp
 
 sealed class NoteDetailUIState {
     data class Success(val data: NoteDomainModel): NoteDetailUIState()
@@ -9,3 +12,10 @@ sealed class NoteDetailUIState {
     object Empty: NoteDetailUIState()
     object Loading: NoteDetailUIState()
 }
+
+data class NoteDetailViewState(
+    val note: NoteDomainModel? = null,
+    val isLoading: Boolean = false,
+    val isFailure: Boolean = false,
+    val isSuccess: Boolean = false,
+): ViewState
