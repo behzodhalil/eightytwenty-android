21e873c0dc2bb67d169cf7a718259b2a97bd216f
behzoddev <behzoddev@gmail.com>
2022-10-03 18:01:34 +0900

    [refactor::] Update NewNoteFragment

--- app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentNewNoteBinding
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
-import uz.behzod.eightytwenty.utils.extension.debugger
-import uz.behzod.eightytwenty.utils.extension.navController
-import uz.behzod.eightytwenty.utils.extension.supportFragmentManager
+import uz.behzod.eightytwenty.utils.extension.*
 import uz.behzod.eightytwenty.utils.view.viewBinding
 import java.time.ZonedDateTime
 
@@ -22,71 +27,60 @@ import java.time.ZonedDateTime
 class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
 
     private val binding by viewBinding(FragmentNewNoteBinding::bind)
-    private val viewModel: NewNoteViewModel by viewModels()
+    private val viewModel: NewNoteWithReduxViewModel by viewModels()
     private val args: NewNoteFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupUI()
+        setupUi()
+        observerState()
     }
 
     private fun setupUI() {
         initTimestamp()
-        insertNote()
     }
 
     private fun initTimestamp() {
         binding.tvDate.text = ZonedDateTime.now().toString()
     }
 
-    private fun insertNote() {
+    private fun setupUi() {
+        binding.etTitle.addTextChangedListener { viewModel.modifyTitle(it.asStringOrEmpty()) }
+        binding.etDesc.addTextChangedListener { viewModel.modifyDesc(it.asStringOrEmpty()) }
+        binding.tvDate.addTextChangedListener { viewModel.modifyTimestamp(it.asStringOrEmpty().asZoneDateTime()) }
+
+        viewModel.modifyIsTrashed(false)
+
+        viewModel.modifyGroupUid(args.categoryId)
+
         binding.btnSaveOrCancel.setOnClickListener {
-            viewModel.insertNote(createNote()).run {
-                Toast.makeText(requireContext(), "Note is saved", Toast.LENGTH_SHORT).show()
-                if (navController.previousBackStackEntry?.destination?.id == R.id.noteFragment) {
-                    if (navController.currentDestination?.id == R.id.newNoteFragment) {
-                        findNavController().navigate(R.id.action_newNoteFragment_to_noteFragment)
-                    }
+            viewModel.insertNote()
+        }
+    }
 
-                }
-                if (navController.previousBackStackEntry?.destination?.id == R.id.newTaskFragment) {
-                    if (navController.currentDestination?.id == R.id.newNoteFragment) {
-                        findNavController().navigate(R.id.action_newNoteFragment_to_newTaskFragment)
-                    }
+    private fun observerState() {
+        viewModel.state
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle,Lifecycle.State.STARTED)
+            .onEach { state -> renderState(state) }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
+    }
 
-                }
-
+    private fun renderState(state: NewNoteViewState) {
+        if (state.isSuccess) {
+            if(navController.currentDestination?.id == R.id.newNoteFragment) {
+                val route = NewNoteFragmentDirections.actionNewNoteFragmentToNoteFragment()
+                findNavController().navigate(route)
             }
+        }
+
+        if (state.isFailure) {
+            Toast.makeText(requireContext(), "Note is empty", Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    private fun createNote(): NoteDomainModel {
-        val title = binding.etTitle.text.toString()
-        val description = binding.etDesc.text.toString()
-        val timestamp = ZonedDateTime.now()
-        val isTrashed = false
-        val categoryId = args.categoryId
-
-        return NoteDomainModel(
-            title = title,
-            description = description,
-            timestamp = timestamp,
-            isTrashed = isTrashed,
-            categoryId = categoryId
-        )
-    }
-
-    private fun setNoteForTask() {
-        val title = binding.etTitle.text.toString()
-        val description = binding.etDesc.text.toString()
-
-        supportFragmentManager.setFragmentResult(
-            "NoteTitle",
-            bundleOf("NoteTitle" to title)
-        )
-    }
 
 
 }
