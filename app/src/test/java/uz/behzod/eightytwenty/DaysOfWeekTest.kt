67b83ea40c349f67bcfecc179db4036ab4fa8426
behzoddev <behzoddev@gmail.com>
2022-10-04 21:34:11 +0900

    [test::] Update DaysOfWeekTest.kt

--- app/src/test/java/uz/behzod/eightytwenty/DaysOfWeekTest.kt
+++ app/src/test/java/uz/behzod/eightytwenty/DaysOfWeekTest.kt
@@ -3,6 +3,9 @@ package uz.behzod.eightytwenty
 import org.junit.Assert
 import org.junit.Test
 import uz.behzod.eightytwenty.data.local.entities.ScheduleEntity
+import uz.behzod.eightytwenty.utils.constants.BIT_VALUE_OF_FRIDAY
+import uz.behzod.eightytwenty.utils.constants.BIT_VALUE_OF_MONDAY
+import uz.behzod.eightytwenty.utils.constants.BIT_VALUE_OF_SUNDAY
 import java.time.DayOfWeek
 
 class DaysOfWeekTest {
@@ -10,7 +13,7 @@ class DaysOfWeekTest {
     @Test
     fun `check days of week with bit value`() {
         val daysAsBit =
-            ScheduleEntity.BIT_VALUE_OF_MONDAY + ScheduleEntity.BIT_VALUE_OF_FRIDAY + ScheduleEntity.BIT_VALUE_OF_SUNDAY
+            BIT_VALUE_OF_MONDAY + BIT_VALUE_OF_FRIDAY + BIT_VALUE_OF_SUNDAY
 
         Assert.assertTrue(
             ScheduleEntity.parseDaysOfWeek(daysAsBit).contains(DayOfWeek.MONDAY.value)
