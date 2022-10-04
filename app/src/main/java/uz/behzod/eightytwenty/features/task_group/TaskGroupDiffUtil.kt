3df7a46917f6fa6a7f1ad62428e7f9483de28b10
behzoddev <behzoddev@gmail.com>
2022-10-04 21:36:15 +0900

    [refactor::] Update TaskGroupViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task_group/TaskGroupDiffUtil.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task_group/TaskGroupDiffUtil.kt
@@ -1,9 +1,9 @@
-package uz.behzod.eightytwenty.features.task_catalog
+package uz.behzod.eightytwenty.features.task_group
 
 import androidx.recyclerview.widget.DiffUtil
 import uz.behzod.eightytwenty.data.local.entities.TaskCatalogEntity
 
-object TaskCatalogDiffUtil: DiffUtil.ItemCallback<TaskCatalogEntity>() {
+object TaskGroupDiffUtil: DiffUtil.ItemCallback<TaskCatalogEntity>() {
 
     override fun areItemsTheSame(oldItem: TaskCatalogEntity, newItem: TaskCatalogEntity): Boolean {
         return oldItem.uid == newItem.uid
