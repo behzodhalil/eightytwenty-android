46c1cbc57e888e9eda3a1a1e5947a2ee21f62caa
behzoddev <behzoddev@gmail.com>
2022-10-04 21:36:04 +0900

    [refactor::] Update ViewExt.kt

--- app/src/main/java/uz/behzod/eightytwenty/utils/extension/ViewExt.kt
+++ app/src/main/java/uz/behzod/eightytwenty/utils/extension/ViewExt.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import android.graphics.Paint
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
-
+import androidx.fragment.app.Fragment
 
 
 val Context.layoutInflaterService: Any
@@ -33,6 +34,13 @@ fun View.gone(): View {
     return this
 }
 
+fun Context.showMessage(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+fun Fragment.showMessage(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), message, duration).show()
+}
 /**
  * An extension function used to set the strikethrough effect of the drawn text
  * in the view, and the status determines whether to add or remove the effect
