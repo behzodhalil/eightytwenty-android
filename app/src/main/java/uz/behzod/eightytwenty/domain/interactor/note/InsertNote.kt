91fd32c3d21663fa8707ce4ba74446c6a3ac047d
behzoddev <behzoddev@gmail.com>
2022-10-06 14:06:47 +0900

    [refactor::] Update InsertNote.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/InsertNote.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/InsertNote.kt
@@ -1,7 +1,8 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
+import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 
 interface InsertNote {
-    suspend operator fun invoke(data: NoteDomainModel)
+    suspend operator fun invoke(data: NoteDomainModel,images: List<NoteImageEntity>)
 }
