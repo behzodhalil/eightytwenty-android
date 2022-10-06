1be8be0dcb46ec01ea4de8cfebc29a5227146d7b
behzoddev <behzoddev@gmail.com>
2022-10-06 12:31:06 +0900

    [refactor::] Update FragmentExt.kt

--- app/src/main/java/uz/behzod/eightytwenty/utils/extension/FragmentExt.kt
+++ app/src/main/java/uz/behzod/eightytwenty/utils/extension/FragmentExt.kt
@@ -3,10 +3,19 @@ package uz.behzod.eightytwenty.utils.extension
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 
 val Fragment.navController: NavController
     get() = this.findNavController()
+
+fun Fragment.navigateTo(route: NavDirections) {
+    this.findNavController().navigate(directions = route)
+}
+
+fun Fragment.navigateTo(resId: Int) {
+    this.findNavController().navigate(resId = resId)
+}
 
 val Fragment.packageManager
     get() = activity?.packageManager
