9cd864cff95533eff6c561d793274d6f01ca40b8
behzoddev <behzoddev@gmail.com>
2022-10-06 14:08:25 +0900

    [refactor::] Update NewNoteViewState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteViewState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteViewState.kt
@@ -1,6 +1,8 @@
 package uz.behzod.eightytwenty.features.new_note
 
+import android.net.Uri
 import uz.behzod.eightytwenty.core.state.ViewState
+import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
 import java.time.ZonedDateTime
@@ -10,6 +12,8 @@ data class NewNoteViewState(
     val description: String = String.Empty,
     val timestamp: ZonedDateTime = ZonedDateTime.now(),
     val isTrashed: Boolean = false,
+    val uri: Uri? = null,
+    val image: NoteImageEntity? = null,
     val categoryUid: Long = Long.Zero,
     val isLoading: Boolean = false,
     val isFailure: Boolean = false,
