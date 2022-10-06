f12596d2323c75dafc9c100d7f71d97c047d653d
behzoddev <behzoddev@gmail.com>
2022-10-06 14:08:16 +0900

    [refactor::] Update NewNoteFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/new_note/NewNoteFragment.kt
@@ -1,25 +1,23 @@
 package uz.behzod.eightytwenty.features.new_note
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.core.os.bundleOf
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentNewNoteBinding
-import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.utils.extension.*
+import uz.behzod.eightytwenty.utils.helper.BitmapHelper
 import uz.behzod.eightytwenty.utils.view.viewBinding
 import java.time.ZonedDateTime
 
@@ -29,6 +27,10 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
     private val binding by viewBinding(FragmentNewNoteBinding::bind)
     private val viewModel: NewNoteWithReduxViewModel by viewModels()
     private val args: NewNoteFragmentArgs by navArgs()
+
+    private val takePictureLauncher = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { state -> onTakePictureListener(state)}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -58,6 +60,15 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
         binding.btnSaveOrCancel.setOnClickListener {
             viewModel.insertNote()
         }
+
+        binding.ivImage.setOnClickListener {
+            val screen = ImagePickerFragment().apply {
+                setTakePictureListener {
+                    onTakePicture()
+                }
+            }
+            transaction(screen)
+        }
     }
 
     private fun observerState() {
@@ -71,16 +82,33 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
         if (state.isSuccess) {
             if(navController.currentDestination?.id == R.id.newNoteFragment) {
                 val route = NewNoteFragmentDirections.actionNewNoteFragmentToNoteFragment()
-                findNavController().navigate(route)
+                navigateTo(route)
             }
         }
 
         if (state.isFailure) {
-            Toast.makeText(requireContext(), "Note is empty", Toast.LENGTH_SHORT).show()
+            showMessage("Note is empty")
         }
 
     }
 
+    private fun onTakePicture() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val uri = BitmapHelper.saveImage(requireContext())
+            viewModel.modifyUri(uri)
+            takePictureLauncher.launch(viewModel.currentState.uri)
+        }
 
+    }
+
+    private fun onPickFromGallery() {}
+    private fun onPickFromGalleryListener() {}
+    private fun onTakePictureListener(state: Boolean) {
+        if (state) {
+            showMessage("Photo was taken")
+        } else {
+            showMessage("No photo was taken")
+        }
+    }
 
 }
