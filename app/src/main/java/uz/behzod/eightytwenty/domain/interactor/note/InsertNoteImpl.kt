b46144762bfb871a5a10b59755c130094290f6cc
behzoddev <behzoddev@gmail.com>
2022-10-06 14:06:57 +0900

    [refactor::] Update InsertNoteImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/InsertNoteImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/InsertNoteImpl.kt
@@ -1,8 +1,10 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
 import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 import uz.behzod.eightytwenty.data.local.entities.asEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.domain.repository.ImageRepository
 import uz.behzod.eightytwenty.domain.repository.NoteCategoryRepository
 import uz.behzod.eightytwenty.domain.repository.NoteRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
@@ -11,13 +13,17 @@ import javax.inject.Inject
 class InsertNoteImpl @Inject constructor(
     private val iNoteRepository: NoteRepository,
     private val iNoteCategoryRepository: NoteCategoryRepository,
+    private val iImageRepository: ImageRepository,
     private val dispatcherProvider: IDispatcherProvider
 ): InsertNote {
 
-    override suspend fun invoke(data: NoteDomainModel) {
+    override suspend fun invoke(data: NoteDomainModel, images: List<NoteImageEntity>) {
         return withContext(dispatcherProvider.io) {
             iNoteCategoryRepository.incrementNoteCount(data.categoryId)
-            iNoteRepository.insertNote(data.asEntity())
+            val noteUid = iNoteRepository.insertNote(data.asEntity())
+            images.forEach {
+                iImageRepository.insertNoteImage(it.copy(noteUid = noteUid))
+            }
         }
     }
 }
