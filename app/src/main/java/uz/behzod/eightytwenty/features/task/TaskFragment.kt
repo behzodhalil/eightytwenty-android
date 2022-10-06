034a3d2b4d0fa75515aa3d706f2add6e25c4fdb2
behzoddev <behzoddev@gmail.com>
2022-10-06 14:09:48 +0900

    [refactor::] Update TaskFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task/TaskFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task/TaskFragment.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentTaskBinding
-import uz.behzod.eightytwenty.utils.extension.debugger
 import uz.behzod.eightytwenty.utils.extension.navController
 import uz.behzod.eightytwenty.utils.view.viewBinding
 
