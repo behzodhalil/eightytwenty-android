639afd0bcc68034465ae906466675d8e8a993419
behzoddev <behzoddev@gmail.com>
2022-10-10 21:25:06 +0900

    [refactor::] Update HabitRecommendViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit_recommend/HabitRecommendViewModel.kt
@@ -1,39 +1,55 @@
 package uz.behzod.eightytwenty.features.habit_recommend
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.onStart
+import uz.behzod.eightytwenty.core.ReduxViewModel
 import uz.behzod.eightytwenty.domain.interactor.habit_recommend.FetchHabitRecommendsByCategory
+import uz.behzod.eightytwenty.utils.extension.printDebug
+import uz.behzod.eightytwenty.utils.extension.printFailure
 import javax.inject.Inject
 
 @HiltViewModel
 class HabitRecommendViewModel @Inject constructor(
-    private val iFetchHabitRecommendsByCategory: FetchHabitRecommendsByCategory
-) : ViewModel() {
+    private  val iFetchHabitRecommendsByCategory: FetchHabitRecommendsByCategory
+) :
+    ReduxViewModel<HabitRecommendState, HabitRecommendAction>(initialState = HabitRecommendState()) {
 
-    private val _uiState: MutableStateFlow<HabitRecommendUIState> =
-        MutableStateFlow(HabitRecommendUIState.Loading)
-    val uiState = _uiState.asStateFlow()
+    fun fetchHabitRecommendsByCategory() {
+        val category = currentState.category
 
-
-    fun fetchHabitRecommendByCategory(category: String) {
-        viewModelScope.launch {
-            iFetchHabitRecommendsByCategory.invoke(category)
-                .onEach {
-                    _uiState.value = HabitRecommendUIState.Loading
-                    if (it.isNotEmpty()) {
-                        _uiState.value = HabitRecommendUIState.Success(it)
-                    } else {
-                        _uiState.value = HabitRecommendUIState.Empty
+        iFetchHabitRecommendsByCategory.invoke(category)
+            .onStart { modifyState { state -> state.copy(isLoading = true) }}
+            .onEach { habitRecommends ->
+                if (habitRecommends.isNotEmpty()) {
+                    modifyState { state ->
+                        state.copy(
+                            habits = habitRecommends,
+                            isSuccess = true,
+                            isLoading = false,
+                            isFailure = false
+                        )
                     }
+
+                    printDebug { "Result is $habitRecommends" }
+                } else {
+                    modifyState { state ->
+                        state.copy(
+                            isSuccess = false,
+                            isLoading = false,
+                            isFailure = true
+                        )
+                    }
+                    printFailure { "Fetching data is failed" }
                 }
-                .launchIn(viewModelScope)
-        }
+            }
+            .launchIn(viewModelScope)
+    }
+
+    fun modifyCategory(value: String) {
+        modifyState { state -> state.copy(category = value) }
     }
 
 }
