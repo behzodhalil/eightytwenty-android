541f8f866f6e5ea799706424e746f7f7316ff159
behzoddev <behzoddev@gmail.com>
2022-10-10 21:16:07 +0900

    [feat::] Add BitmapCache.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/view/BitmapCache.kt
@@ -0,0 +1,42 @@
+package uz.behzod.eightytwenty.utils.view
+
+import android.graphics.Bitmap
+import android.util.LruCache
+
+
+// Create the object to working with memory cache
+object BitmapCache {
+
+    private var memoryCache: LruCache<String, Bitmap>
+
+    init {
+        // Define the maximum memory of the JVM
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+
+        val cacheSize = maxMemory / 4
+
+        memoryCache = object : LruCache<String, Bitmap>(cacheSize) {
+            override fun sizeOf(key: String?, value: Bitmap?): Int {
+                return value?.byteCount?.div(1024) ?: 0
+            }
+        }
+    }
+
+    // This function is used to add a bitmap to the memory cache
+    fun addBitmapToMemoryCache(key: String, bitmap: Bitmap) {
+        if (getBitmapFromMemoryCache(key) == null) {
+            memoryCache.put(key, bitmap)
+        }
+    }
+
+    // This function is used to get bitmap/image from the cache
+    fun getBitmapFromMemoryCache(key: String): Bitmap? {
+        return memoryCache.get(key)
+    }
+
+    // This function is used to clear the memory cache
+    fun clearMemoryCache() {
+        return memoryCache.evictAll()
+    }
+
+}
