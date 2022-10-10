ea2988eacd9eb018a18a5053d5fbb2840117da96
behzoddev <behzoddev@gmail.com>
2022-10-10 21:23:36 +0900

    [feat::] Add InsertNoteImageImpl.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/image/InsertNoteImageImpl.kt
@@ -0,0 +1,19 @@
+package uz.behzod.eightytwenty.domain.interactor.image
+
+import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
+import uz.behzod.eightytwenty.domain.repository.ImageRepository
+import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
+import javax.inject.Inject
+
+class InsertNoteImageImpl @Inject constructor(
+    private val imageRepository: ImageRepository,
+    private val dispatcher: IDispatcherProvider
+): InsertNoteImage {
+
+    override suspend fun execute(value: NoteImageEntity): Long {
+        return withContext(dispatcher.io) {
+            imageRepository.insertNoteImage(value)
+        }
+    }
+}
