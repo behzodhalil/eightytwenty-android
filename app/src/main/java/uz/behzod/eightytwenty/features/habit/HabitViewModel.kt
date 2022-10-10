33e9a563d3a33172ebb9e08af9c71409f3051777
behzoddev <behzoddev@gmail.com>
2022-10-10 21:29:16 +0900

    [refactor::] Update HabitViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/habit/HabitViewModel.kt
@@ -16,21 +16,10 @@ class HabitViewModel @Inject constructor(
     private var _uiState: MutableStateFlow<HabitUIState> = MutableStateFlow(HabitUIState.Loading)
     val uiState: Flow<HabitUIState> = _uiState.asStateFlow()
 
-    val mutableDates : MutableLiveData<String> = MutableLiveData()
-
-    val habits: LiveData<List<HabitDomainModel>> = Transformations.switchMap(mutableDates) { date ->
-        iFetchHabitsByDate.invoke(date).asLiveData()
-
-    }
-
-    fun searchByTimestamp(timestamp: String) {
-        mutableDates.value = timestamp
-    }
-
     fun fetchHabitsByDate(date: String) {
         iFetchHabitsByDate.invoke(date).onEach { habit ->
             _uiState.update {
-               HabitUIState.Loading
+                HabitUIState.Loading
             }
             if (habit.isNotEmpty()) {
                 _uiState.update {
@@ -41,10 +30,7 @@ class HabitViewModel @Inject constructor(
                     HabitUIState.Empty
                 }
             }
-        }.catch {
-            Log.d("Tag","Error is occured")
-        }
-            .launchIn(viewModelScope)
+        }.launchIn(viewModelScope)
     }
 
 }
