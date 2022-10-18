5a83d49fb512375f419f9fecb49459a75e909602
behzoddev <behzoddev@gmail.com>
2022-10-18 13:36:09 +0900

    [refactor::] Update NoteDetailFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailFragment.kt
@@ -1,24 +1,21 @@
 package uz.behzod.eightytwenty.features.note_detail
 
+import android.media.Image
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentNoteDetailBinding
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.features.new_note.ImageAdapter
+import uz.behzod.eightytwenty.utils.extension.printDebug
 import uz.behzod.eightytwenty.utils.view.viewBinding
 import java.time.ZonedDateTime
 
@@ -26,21 +23,29 @@ import java.time.ZonedDateTime
 class NoteDetailFragment : Fragment(R.layout.fragment_note_detail) {
 
     private val binding by viewBinding(FragmentNoteDetailBinding::bind)
-    private val viewModel: NoteDetailReduxViewModel by viewModels()
+    private val viewModel: NoteDetailViewModel by viewModels()
     private val args: NoteDetailFragmentArgs by navArgs()
+    private lateinit var imageAdapter: ImageAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUI()
+        setupView()
         observerState()
     }
 
     private fun setupUI() {
         val uid = args.noteId
-        viewModel.fetchNoteByUid(uid)
+        viewModel.fetchNoteRelationByUid(uid)
         //fetchNoteByUid(uid)
     }
 
+    private fun setupView() {
+        imageAdapter = ImageAdapter()
+        binding.rvImage.adapter = imageAdapter
+
+
+    }
     /*private fun fetchNoteByUid(uid: Long) {
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -109,9 +114,17 @@ class NoteDetailFragment : Fragment(R.layout.fragment_note_detail) {
             .launchIn(viewLifecycleOwner.lifecycleScope)
     }
 
-    private fun renderState(state: NoteDetailViewState) {
+    private fun renderState(state: NoteDetailState) {
         val note = state.note
 
+        note?.let {
+            binding.etTitle.setText(it.note.title)
+            binding.etDesc.setText(it.note.description)
+            binding.tvDate.text = it.note.timestamp.toString()
+            imageAdapter.submitList(it.images)
+            printDebug { "[NoteDetailFragment]: Fetched images are ${it.images}" }
+
+        }
     }
 
     private fun getNotes(): NoteDomainModel {
