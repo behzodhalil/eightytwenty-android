f54c74de4bf55771a77b0121fcd24130aa8d3bad
behzoddev <behzoddev@gmail.com>
2022-10-18 13:36:27 +0900

    [refactor::] Update NoteDetailViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailViewModel.kt
@@ -1,49 +1,44 @@
 package uz.behzod.eightytwenty.features.note_detail
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import uz.behzod.eightytwenty.core.ReduxViewModel
 import uz.behzod.eightytwenty.data.local.dao.NoteDao
-import uz.behzod.eightytwenty.data.local.entities.asEntity
-import uz.behzod.eightytwenty.data.source.LocalSourceManager
+import uz.behzod.eightytwenty.data.local.entities.isNotEmpty
+import uz.behzod.eightytwenty.domain.interactor.note.DefaultFetchNoteRelationByUid
 import uz.behzod.eightytwenty.domain.interactor.note.FetchNoteById
-import uz.behzod.eightytwenty.domain.interactor.note.UpdateNote
+import uz.behzod.eightytwenty.domain.interactor.note.FetchNoteRelationByUid
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
-import uz.behzod.eightytwenty.domain.repository.NoteRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteDetailViewModel @Inject constructor(
     private val iUpdateNote: NoteDao,
-    private val iFetchNoteById: FetchNoteById
-) : ViewModel() {
+    private val iFetchNoteById: FetchNoteById,
+    private val defaultFetchNoteRelationByUid: FetchNoteRelationByUid
+): ReduxViewModel<NoteDetailState,NoteDetailViewEffect>(NoteDetailState()) {
 
-    private var _uiState: MutableStateFlow<NoteDetailUIState> =
-        MutableStateFlow(NoteDetailUIState.Loading)
-    val uiState: StateFlow<NoteDetailUIState> = _uiState.asStateFlow()
 
-    fun fetchNoteByUid(uid: Long) {
-        viewModelScope.launch {
-            _uiState.value = NoteDetailUIState.Loading
-            iFetchNoteById.invoke(uid).collect { data: NoteDomainModel? ->
-                if (data != null) {
-                    _uiState.value = NoteDetailUIState.Success(data)
-                } else {
-                    _uiState.value = NoteDetailUIState.Empty
-                }
 
+    fun fetchNoteRelationByUid(uid: Long) {
+        defaultFetchNoteRelationByUid.execute(noteUid = uid).onEach {
+            modifyState { state -> state.copy(isLoading = true) }
+            if (it.isNotEmpty()) {
+                modifyState { state -> state.copy(
+                    isLoading = false,
+                    isSuccess = true,
+                    isFailure = false,
+                    note = it
+                ) }
+            } else {
+                modifyState { state -> state.copy(
+                    isLoading = false,
+                    isSuccess = false,
+                    isFailure = true
+                ) }
             }
-        }
+        }.launchIn(viewModelScope)
     }
-
-    fun updateNote(data: NoteDomainModel) {
-        viewModelScope.launch {
-            iUpdateNote.update(data.asEntity())
-        }
-    }
-
 }
