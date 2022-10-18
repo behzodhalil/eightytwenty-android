e52c6a9e7f3a38141dffbf11d029fd84bf0dacb4
behzoddev <behzoddev@gmail.com>
2022-10-18 13:32:46 +0900

    [refactor::] Update InsertNoteImage.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/image/InsertNoteImage.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/image/InsertNoteImage.kt
@@ -3,5 +3,5 @@ package uz.behzod.eightytwenty.domain.interactor.image
 import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 
 interface InsertNoteImage {
-    suspend fun execute(value: NoteImageEntity): Long
+    suspend fun execute(value: NoteImageEntity)
 }
