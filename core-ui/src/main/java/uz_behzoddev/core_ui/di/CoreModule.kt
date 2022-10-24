a0b78105b8f748f733912d3c3c0f692ca0d99703
behzoddev <behzoddev@gmail.com>
2022-10-24 22:10:46 +0900

    [refactor::] Update UndoStateListener

--- core-ui/src/main/java/uz_behzoddev/core_ui/di/CoreModule.kt
+++ core-ui/src/main/java/uz_behzoddev/core_ui/di/CoreModule.kt
@@ -1,24 +1,9 @@
 package uz_behzoddev.core_ui.di
 
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import uz_behzoddev.core_ui.view.edit_text.DefaultUndoRedoHistory
-import uz_behzoddev.core_ui.view.edit_text.UndoRedoHistory
-import uz_behzoddev.core_ui.view.undoredo.DefaultUndoRedoManager
-import uz_behzoddev.core_ui.view.undoredo.UndoRedoManager
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface CoreModule {
-
-    @get: Binds
-    @get: Singleton
-    val DefaultUndoRedoHistory.bindUndoRedoHistory: UndoRedoHistory
-
-    @get: Binds
-    @get: Singleton
-    val DefaultUndoRedoManager.bindUndoRedoManager: UndoRedoManager
-}
+interface CoreModule
