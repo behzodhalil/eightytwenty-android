cce11c68d8c0fe3539da67c0f4484c1d759b458f
Behzod Halil <behzoddev@gmail.com>
2022-10-24 17:55:49 +0900

    [refactor::] Update UndoRedoListener.kt

--- core-ui/src/main/java/uz_behzoddev/core_ui/view/edit_text/UndoRedoListener.kt
+++ core-ui/src/main/java/uz_behzoddev/core_ui/view/edit_text/UndoRedoListener.kt
@@ -1,45 +1,29 @@
 package uz_behzoddev.core_ui.view.edit_text
 
+import android.content.Context
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
+import android.widget.EditText
+import androidx.lifecycle.LifecycleObserver
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 class UndoRedoListener(
-    private val isChanged: Boolean
+    @ApplicationContext context: Context,
+    lifecycleObserver: LifecycleObserver,
+    private val editText: EditText,
+
 ) : TextWatcher {
 
-    // CharSequence:
-    // Represents a readable sequence of [Char] values.
-    private var afterText: CharSequence? = null
-    private var beforeText: CharSequence? = null
-    private var lastActionType = ActionType.Undefined
-    private var lastActionTime: Long = 0L
-
-    override fun beforeTextChanged(text: CharSequence, start: Int, end: Int, count: Int) {
-        beforeText = text.subSequence(startIndex = start, endIndex = start + count)
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        TODO("Not yet implemented")
     }
 
-    override fun onTextChanged(text: CharSequence, start: Int, end: Int,count: Int) {
-        afterText = text.subSequence(startIndex = start, endIndex = start + count)
-
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        TODO("Not yet implemented")
     }
 
     override fun afterTextChanged(p0: Editable?) {
         TODO("Not yet implemented")
-    }
-
-    private fun makeBatch(start: Int) {
-        val actType = getActionType()
-
-    }
-
-    private fun getActionType(): ActionType {
-        return if(!TextUtils.isEmpty(beforeText) && TextUtils.isEmpty(afterText)) {
-            ActionType.Delete
-        } else if(TextUtils.isEmpty(beforeText) && !TextUtils.isEmpty(afterText)) {
-            ActionType.Insert
-        } else {
-            ActionType.Paste
-        }
     }
 }
