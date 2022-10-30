cd70b7ebc1894a0e7a48d8577ad22dbd17d57f6e
behzoddev <behzoddev@gmail.com>
2022-10-30 12:47:21 +0900

    [refactor::] Update DeleteNoteImpl.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/DeleteNoteImpl.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/note/DeleteNoteImpl.kt
@@ -1,8 +1,10 @@
 package uz.behzod.eightytwenty.domain.interactor.note
 
 import kotlinx.coroutines.withContext
+import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 import uz.behzod.eightytwenty.data.local.entities.asEntity
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.domain.repository.ImageRepository
 import uz.behzod.eightytwenty.domain.repository.NoteCategoryRepository
 import uz.behzod.eightytwenty.domain.repository.NoteRepository
 import uz.behzod.eightytwenty.utils.providers.IDispatcherProvider
@@ -11,12 +13,17 @@ import javax.inject.Inject
 class DeleteNoteImpl @Inject constructor(
     private val iNoteRepository: NoteRepository,
     private val iNoteCategoryRepository: NoteCategoryRepository,
+    private val defaultImageRepository: ImageRepository,
     private val dispatcherProvider: IDispatcherProvider
 ): DeleteNote {
 
-    override suspend fun invoke(data: NoteDomainModel) {
+    override suspend fun invoke(data: NoteDomainModel,images: List<NoteImageEntity>) {
         return withContext(dispatcherProvider.io) {
             iNoteRepository.deleteNote(data.asEntity())
+            images.forEach { image ->
+                image.noteUid = data.id
+                defaultImageRepository.deleteNoteImage(image)
+            }
             if (!data.isTrashed) {
                 iNoteCategoryRepository.decrementNoteCount(data.categoryId)
             }
