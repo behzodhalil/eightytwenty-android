a5aa8d0d0f62d088dc373171cb734e52bd564d71
behzoddev <behzoddev@gmail.com>
2022-11-03 21:14:46 +0900

    [refactor::] Update NoteDetailState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note_detail/NoteDetailState.kt
@@ -3,11 +3,13 @@ package uz.behzod.eightytwenty.features.note_detail
 import uz.behzod.eightytwenty.core.state.ViewState
 import uz.behzod.eightytwenty.data.local.entities.NoteImageEntity
 import uz.behzod.eightytwenty.data.local.entities.NoteRelation
+import uz.behzod.eightytwenty.domain.model.NoteDomainModel
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
 import java.time.ZonedDateTime
 
 data class NoteDetailState(
+    val notes: List<NoteDomainModel> = emptyList(),
     val note: NoteRelation? = null,
     val isLoading: Boolean = false,
     val isFailure: Boolean = false,
@@ -16,10 +18,12 @@ data class NoteDetailState(
     val isUpdateFailed: Boolean = false,
     val isDeleted: Boolean = false,
     val isDeleteFailed: Boolean = false,
+    val isMoved: Boolean = false,
+    val isMoveFailed: Boolean = false,
     val title: String = String.Empty,
     val description: String = String.Empty,
     val categoryUid: Long = Long.Zero,
     val timestamp: ZonedDateTime = ZonedDateTime.now(),
     val images: List<NoteImageEntity> = emptyList(),
     val uid: Long = Long.Zero
-): ViewState
+) : ViewState
