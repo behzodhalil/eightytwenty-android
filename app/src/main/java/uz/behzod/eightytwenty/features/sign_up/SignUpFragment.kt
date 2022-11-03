02c9f9768485dcb39d26bd1c94bae396909cfe03
behzoddev <behzoddev@gmail.com>
2022-11-03 21:17:05 +0900

    [refactor::] Update SignUpFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpFragment.kt
@@ -1,4 +1,29 @@
 package uz.behzod.eightytwenty.features.sign_up
 
-class SignUpFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.databinding.FragmentSignUpBinding
+import uz.behzod.eightytwenty.utils.view.viewBinding
+
+@AndroidEntryPoint
+class SignUpFragment: Fragment(R.layout.fragment_sign_up) {
+
+    private val binding: FragmentSignUpBinding by viewBinding(FragmentSignUpBinding::bind)
+    private val viewModel: SignUpViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupView() {
+
+    }
+
+    private fun insertUser() {
+        viewModel.insertUser()
+    }
 }
