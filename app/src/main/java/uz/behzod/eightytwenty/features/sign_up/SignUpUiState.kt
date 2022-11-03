d2fb8ee8ee607963b7883d79da8bef4018975584
behzoddev <behzoddev@gmail.com>
2022-11-03 21:17:12 +0900

    [refactor::] Update SignUpUiState.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpUiState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpUiState.kt
@@ -1,4 +1,16 @@
 package uz.behzod.eightytwenty.features.sign_up
 
-class SignUpUiState {
-}
+import uz.behzod.eightytwenty.core.state.ViewState
+import uz.behzod.eightytwenty.utils.extension.Zero
+
+data class SignUpUiState(
+    val email: String = "",
+    val password: String = "",
+    val name: String = "",
+    val uid: Long = Long.Zero,
+    val isSuccess: Boolean = false,
+    val isFailed: Boolean = false,
+    val isLoaded: Boolean = false,
+    val isSaved: Boolean = false,
+    val isSaveFailed: Boolean = false
+): ViewState
