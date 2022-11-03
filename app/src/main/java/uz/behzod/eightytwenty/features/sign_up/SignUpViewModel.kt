d42b7b0dbdb76346103dd6a9fd959d99e15a91a8
behzoddev <behzoddev@gmail.com>
2022-11-03 21:17:21 +0900

    [refactor::] Update SignUpViewModel.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpViewModel.kt
@@ -1,4 +1,69 @@
 package uz.behzod.eightytwenty.features.sign_up
 
-class SignUpViewModel {
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import uz.behzod.eightytwenty.core.ReduxViewModel
+import uz.behzod.eightytwenty.data.local.entities.UserEntity
+import uz.behzod.eightytwenty.domain.interactor.login.CreateUserWithEmailAndPassword
+import uz.behzod.eightytwenty.domain.interactor.login.InsertUser
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val createUserWithEmailAndPassword: CreateUserWithEmailAndPassword,
+    private val firebaseAuth: FirebaseAuth,
+    private val defaultInsertUser: InsertUser
+) : ReduxViewModel<SignUpUiState>(
+    initialState = SignUpUiState()
+) {
+
+    fun createUser() {
+        val email = currentState.email
+        val password = currentState.password
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    modifyState { state ->
+                        state.copy(
+                            isSuccess = true
+                        )
+                    }
+                } else {
+                    modifyState { state ->
+                        state.copy(
+                            isSuccess = false,
+                            isFailed = true
+                        )
+                    }
+                }
+            }
+    }
+
+    fun insertUser() {
+        viewModelScope.launch {
+            val name = currentState.name
+            val uid = currentState.uid
+
+            runCatching {
+                defaultInsertUser.execute(
+                    UserEntity(name = name, uid = uid)
+                )
+            }.onSuccess {
+                modifyState { state ->
+                    state.copy(
+                        isSaved = true
+                    )
+                }
+            }.onFailure {
+                modifyState { state ->
+                    state.copy(
+                        isSaveFailed = false
+                    )
+                }
+            }
+
+        }
+    }
 }
