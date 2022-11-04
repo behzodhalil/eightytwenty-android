9d34f04e3ef4681ee746770d9b33de339f26523e
behzoddev <behzoddev@gmail.com>
2022-11-04 15:04:54 +0900

    [refactor::] Add checking current user is exists in app

--- app/src/main/java/uz/behzod/eightytwenty/features/sign_in/SignInFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/sign_in/SignInFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -17,6 +18,7 @@ import uz.behzod.eightytwenty.utils.extension.navigateTo
 import uz.behzod.eightytwenty.utils.extension.printDebug
 import uz.behzod.eightytwenty.utils.view.viewBinding
 import uz.behzoddev.ui_toast.errorMessage
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
@@ -24,10 +26,21 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
     private val binding: FragmentSignInBinding by viewBinding(FragmentSignInBinding::bind)
     private val viewModel: SignInViewModel by viewModels()
 
+    @Inject
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (auth.currentUser != null && auth.currentUser?.isAnonymous == false) {
+            val route = SignInFragmentDirections.actionSignInFragmentToNoteFragment()
+            navigateTo(route)
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupView()
         observeState()
+        navigateToSignUp()
     }
 
     private fun setupView() {
@@ -64,6 +77,13 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
     private fun signIn() {
         binding.btnNext.setOnClickListener {
             viewModel.signIn()
+        }
+    }
+
+    private fun navigateToSignUp() {
+        binding.tvSignUp.setOnClickListener {
+            val route = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
+            navigateTo(route)
         }
     }
 }
