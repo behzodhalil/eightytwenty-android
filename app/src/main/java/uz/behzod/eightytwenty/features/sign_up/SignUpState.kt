825248e06a6c9bc2ef66455627ba5d4b4b0acc31
behzoddev <behzoddev@gmail.com>
2022-11-04 15:05:11 +0900

    [refactor::] Change state names

--- app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/sign_up/SignUpState.kt
@@ -3,7 +3,7 @@ package uz.behzod.eightytwenty.features.sign_up
 import uz.behzod.eightytwenty.core.state.ViewState
 import uz.behzod.eightytwenty.utils.extension.Zero
 
-data class SignUpUiState(
+data class SignUpState(
     val email: String = "",
     val password: String = "",
     val name: String = "",
@@ -11,6 +11,6 @@ data class SignUpUiState(
     val isSuccess: Boolean = false,
     val isFailed: Boolean = false,
     val isLoaded: Boolean = false,
-    val isSaved: Boolean = false,
-    val isSaveFailed: Boolean = false
+    val isCreated: Boolean = false,
+    val isCreateFailed: Boolean = false
 ): ViewState
