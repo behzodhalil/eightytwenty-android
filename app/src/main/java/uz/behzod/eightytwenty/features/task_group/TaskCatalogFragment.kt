25a7c7f2abcd74bd1b5965e1b5e2727cc79cc629
behzoddev <behzoddev@gmail.com>
2022-11-06 19:18:49 +0900

    [refactor::] Update TaskCatalogFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/task_group/TaskCatalogFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/task_group/TaskCatalogFragment.kt
@@ -2,24 +2,18 @@ package uz.behzod.eightytwenty.features.task_group
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.data.local.entities.TaskCatalogEntity
 import uz.behzod.eightytwenty.databinding.FragmentCatalogTaskBinding
-import uz.behzod.eightytwenty.utils.extension.gone
-import uz.behzod.eightytwenty.utils.extension.hide
-import uz.behzod.eightytwenty.utils.extension.navController
-import uz.behzod.eightytwenty.utils.extension.show
+import uz.behzod.eightytwenty.utils.extension.*
 import uz.behzod.eightytwenty.utils.view.viewBinding
 
 @AndroidEntryPoint
@@ -35,10 +29,6 @@ class TaskCatalogFragment : Fragment(R.layout.fragment_catalog_task) {
 
         setupUI()
         observeState()
-
-
-        //  addNewCatalog()
-
         onClickNewCatalog()
         onNavigateToTask()
         onNavigateToSearchTask()
@@ -48,8 +38,7 @@ class TaskCatalogFragment : Fragment(R.layout.fragment_catalog_task) {
     private fun setupUI() {
         adapter = TaskCatalogAdapter()
         binding.rvTaskCatalogs.adapter = adapter
-
-        // fetchCatalogs()
+        binding.etNewCategory.addTextChangedListener { viewModel.modifyTaskGroupName(it.asStringOrEmpty()) }
     }
 
     private fun onNavigateToTask() {
@@ -69,27 +58,6 @@ class TaskCatalogFragment : Fragment(R.layout.fragment_catalog_task) {
         }
     }
 
-//    private fun fetchCatalogs() = lifecycleScope.launch {
-//        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-//            viewModel.uiState.collect { state ->
-//                when (state) {
-//                    TaskCatalogUiState.Empty -> {
-//
-//                    }
-//                    is TaskCatalogUiState.Failure -> {
-//
-//                    }
-//                    TaskCatalogUiState.Loading -> {
-//
-//                    }
-//                    is TaskCatalogUiState.Success -> {
-//                        adapter.submitList(state.data)
-//                    }
-//                }
-//            }
-//        }
-//    }
-
     private fun onClickNewCatalog() {
         binding.btnNewCategoryTask.setOnClickListener {
             binding.btnNewCategory.show()
@@ -97,27 +65,13 @@ class TaskCatalogFragment : Fragment(R.layout.fragment_catalog_task) {
             binding.btnCancel.show()
             binding.btnNewCategoryTask.hide()
         }
-    }
 
-//    private fun addNewCatalog() {
-//        binding.btnNewCategory.setOnClickListener {
-//            binding.etNewCategory.show()
-//            binding.btnNewCategory.setOnClickListener {
-//                insertNewCatalog().run {
-//                    Toast.makeText(
-//                        requireContext(),
-//                        "Category is successfully saved",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//
-//                }
-//
-//            }
-//        }
-//    }
-
-    private fun setupView() {
-
+        binding.btnNewCategory.setOnClickListener {
+            binding.etNewCategory.show()
+            binding.btnNewCategory.setOnClickListener {
+                viewModel.insertTaskGroup()
+            }
+        }
     }
 
     private fun observeState() {
@@ -138,23 +92,12 @@ class TaskCatalogFragment : Fragment(R.layout.fragment_catalog_task) {
             binding.etNewCategory.gone()
         }
 
-        if (state.isAddedFailure) {
-
-        }
-
         if (state.isSuccess) {
             getTaskGroups(groups)
         }
 
-
     }
 
     private fun getTaskGroups(value: List<TaskCatalogEntity>) = adapter.submitList(value)
-//    private fun insertNewCatalog() {
-//        viewModel.insertCatalog(
-//            TaskCatalogEntity(
-//                name = binding.etNewCategory.text.toString().trim()
-//            )
-//        )
-//    }
+
 }
