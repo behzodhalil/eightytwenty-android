841064b1e0b0605313abede6e74abec30d07e125
behzoddev <behzoddev@gmail.com>
2022-11-06 19:18:11 +0900

    [refactor::] Update ZonedDateTimeConverter.kt

--- app/src/main/java/uz/behzod/eightytwenty/data/local/db/ZonedDateTimeConverter.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/db/ZonedDateTimeConverter.kt
@@ -7,16 +7,18 @@ import java.time.format.DateTimeFormatter
 
 class ZonedDateTimeConverter {
     @TypeConverter
-    fun fromZoneDateTime(timestamp: ZonedDateTime): String {
-        val formatter =
-            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withZone(ZoneId.systemDefault())
-        return formatter.format(timestamp)
+    fun fromZonedDateTime(dateTime: ZonedDateTime?): String? {
+        val formatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.systemDefault())
+        return if (dateTime != null) formatter.format(dateTime) else null
     }
 
     @TypeConverter
-    fun toZoneDateTime(timestamp: String): ZonedDateTime {
-        val formatter =
-            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withZone(ZoneId.systemDefault())
-        return ZonedDateTime.parse(timestamp, formatter)
+    fun toZonedDateTime(value: String?): ZonedDateTime? {
+        val formatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.systemDefault())
+        return if (value != null) ZonedDateTime.parse(value, formatter) else null
+    }
+
+    companion object {
+        private const val pattern = "dd/MM/yyyy HH:mm:ss"
     }
 }
