e1fb012380e37b2cd86161480ee6ea4e6040407b
behzoddev <behzoddev@gmail.com>
2022-11-06 19:23:39 +0900

    [feat::] Add ProductivityViewHolder.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/select_productivity/ProductivityViewHolder.kt
@@ -0,0 +1,6 @@
+package uz.behzod.eightytwenty.features.select_productivity
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+sealed class ProductivityViewHolder(val VB: ViewBinding): RecyclerView.ViewHolder(VB.root)
