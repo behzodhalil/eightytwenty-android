0d16c3242448ba4f4a3dad32adeca6bcbcaca153
behzoddev <behzoddev@gmail.com>
2022-11-06 19:25:20 +0900

    [feat::] Add FetchTasksNearTime.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/task/FetchTasksNearTime.kt
@@ -0,0 +1,8 @@
+package uz.behzod.eightytwenty.domain.interactor.task
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.TaskEntity
+
+interface FetchTasksNearTime {
+    fun execute(): Flow<List<TaskEntity>>
+}
