f2def562278d57aafff8885a0cbce0db5d6b05f4
behzoddev <behzoddev@gmail.com>
2022-11-06 19:20:17 +0900

    [refactor::] Update ProductivityFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/productivity/ProductivityFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/productivity/ProductivityFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import kr.sns.ui_expandable_view.ExpandableSelectionView
@@ -12,6 +11,9 @@ import uz.behzod.eightytwenty.R
 import uz.behzod.eightytwenty.databinding.FragmentProductivityBinding
 import uz.behzod.eightytwenty.domain.model.HabitDomainModel
 import uz.behzod.eightytwenty.domain.model.NoteDomainModel
+import uz.behzod.eightytwenty.features.setting.SettingFragment
+import uz.behzod.eightytwenty.utils.extension.navigateTo
+import uz.behzod.eightytwenty.utils.extension.transaction
 import uz.behzod.eightytwenty.utils.view.viewBinding
 
 @AndroidEntryPoint
@@ -31,25 +33,23 @@ class ProductivityFragment : Fragment(R.layout.fragment_productivity) {
             "https://www.pngmart.com/files/12/Boy-Emoji-Avatar-PNG.png"
         binding.avatarView.load(url)
         setupRecyclerView()
+        binding.btnStart.setOnClickListener { route(ProductivityRoute.SelectProductivityRoute) }
+        binding.avatarView.setOnClickListener { route(ProductivityRoute.SettingRoute) }
     }
 
     private fun setupRecyclerView() {
         val notes = listOf(
-            NoteDomainModel(id = 1,title = "Test1", description = "Test1"),
-            NoteDomainModel(id = 2,title = "Test2", description = "Test2")
+            NoteDomainModel(id = 1, title = "Test1", description = "Test1"),
+            NoteDomainModel(id = 2, title = "Test2", description = "Test2")
         )
 
         val habits = listOf(
             HabitDomainModel("Test", description = "test"),
             HabitDomainModel("Test", description = "test")
         )
-        val concatAdapterConfig = ConcatAdapter.Config.Builder()
-            .setIsolateViewTypes(false)
-            .build()
 
-
-        habitAdapter = ProductivityHabitAdapter(habits,"Привычки",1)
-        noteAdapter = ProductivityAdapter(notes,"Заметки",3)
+        habitAdapter = ProductivityHabitAdapter(habits, "Привычки", 1)
+        noteAdapter = ProductivityAdapter(notes, "Заметки", 3)
         binding.rvConcatAdapter.setAdapter(noteAdapter)
         binding.rvConcatAdapter.setState(ExpandableSelectionView.State.Expanded)
         binding.rvHabitsAdapter.setAdapter(habitAdapter)
@@ -64,7 +64,19 @@ class ProductivityFragment : Fragment(R.layout.fragment_productivity) {
 
     }
 
-    private fun navigateToSetting() {}
+    private fun route(route: ProductivityRoute) {
+        when (route) {
+            ProductivityRoute.SelectProductivityRoute -> {
+                val direction =
+                    ProductivityFragmentDirections.actionProductivityFragmentToSelectProductivityFragment()
+                navigateTo(direction)
+            }
+            ProductivityRoute.SettingRoute -> {
+                val direction = SettingFragment.instance()
+                transaction(direction)
+            }
+        }
+    }
 
 
 }
