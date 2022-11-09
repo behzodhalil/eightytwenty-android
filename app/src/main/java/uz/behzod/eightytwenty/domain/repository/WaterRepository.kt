7c525e7c215db759ef8cdfbbbe5d41a2bf5308d4
behzoddev <behzoddev@gmail.com>
2022-11-09 22:06:11 +0900

    [feat::] Add WaterRepository.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/WaterRepository.kt
@@ -0,0 +1,11 @@
+package uz.behzod.eightytwenty.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+
+interface WaterRepository {
+    suspend fun insertWater(water: WaterEntity): Long
+    suspend fun updateWater(water: WaterEntity)
+    fun fetchWaterAfterTimestamp(timestamp: Long): Flow<List<WaterEntity>>
+    fun fetchWaters(): Flow<List<WaterEntity>>
+}
