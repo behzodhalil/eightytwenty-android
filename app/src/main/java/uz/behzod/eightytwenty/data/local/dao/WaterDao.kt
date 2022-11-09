dae9a6f8a071e4e922c8fdcc9b835eb9ef00e51a
behzoddev <behzoddev@gmail.com>
2022-11-09 22:04:53 +0900

    [feat::] Add WaterDao.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/dao/WaterDao.kt
@@ -0,0 +1,21 @@
+package uz.behzod.eightytwenty.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+
+@Dao
+interface WaterDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(water: WaterEntity): Long
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(water: WaterEntity)
+    @Query("SELECT * FROM water_table WHERE water_timestamp >:timestamp ORDER BY water_timestamp DESC")
+    fun fetchWaterAfterTimestamp(timestamp: Long): Flow<List<WaterEntity>>
+    @Query("SELECT * FROM water_table ORDER BY water_uid")
+    fun fetchWaters(): Flow<List<WaterEntity>>
+}
