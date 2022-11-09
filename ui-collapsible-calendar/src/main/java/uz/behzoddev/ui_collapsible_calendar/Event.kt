27e5e5a3303ec93332d9e9261d8fa9e76df83c5f
behzoddev <behzoddev@gmail.com>
2022-11-09 22:03:52 +0900

    [chore::] Update chore

--- ui-collapsible-calendar/src/main/java/uz/behzoddev/ui_collapsible_calendar/Event.kt
+++
@@ -1,8 +0,0 @@
-package uz.behzoddev.ui_collapsible_calendar
-
-data class Event(
-    val year: Int,
-    val month: Int,
-    val day: Int,
-    val color: Int = 0,
-)
--- ui-collapsible-calendar/src/main/java/uz/behzoddev/ui_collapsible_calendar/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package uz.behzoddev.ui_collapsible_calendar.utils
-
-object Constants {
-    const val TIME_PATTERN = ""
-}
