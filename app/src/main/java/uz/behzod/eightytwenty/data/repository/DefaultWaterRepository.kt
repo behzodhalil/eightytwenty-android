d67f9399e23927dbc4111fae10ef9450c09d16d3
behzoddev <behzoddev@gmail.com>
2022-11-09 22:08:49 +0900

    [feat::] Add DefaultWaterRepository.kt

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/repository/DefaultWaterRepository.kt
@@ -0,0 +1,28 @@
+package uz.behzod.eightytwenty.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+import uz.behzod.eightytwenty.data.source.LocalSourceManager
+import uz.behzod.eightytwenty.domain.repository.WaterRepository
+import javax.inject.Inject
+
+class DefaultWaterRepository @Inject constructor(
+    private val sourceManager: LocalSourceManager
+): WaterRepository {
+
+    override suspend fun insertWater(water: WaterEntity): Long {
+        return sourceManager.insertWater(water)
+    }
+
+    override suspend fun updateWater(water: WaterEntity) {
+        return sourceManager.updateWater(water)
+    }
+
+    override fun fetchWaterAfterTimestamp(timestamp: Long): Flow<List<WaterEntity>> {
+        return sourceManager.fetchWaterAfterTimestamp(timestamp)
+    }
+
+    override fun fetchWaters(): Flow<List<WaterEntity>> {
+        return sourceManager.fetchWaters()
+    }
+}
