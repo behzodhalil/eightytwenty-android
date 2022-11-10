66ab6d4122996a7046fa70df482729f2914b669e
behzoddev <behzoddev@gmail.com>
2022-11-10 21:41:40 +0900

    [refactor::] Update FetchWaters.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/bill/InsertBill.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/bill/InsertBill.kt
@@ -1,4 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.bill
 
+import uz.behzod.eightytwenty.data.local.entities.reminder.BillEntity
+
 interface InsertBill {
+    suspend fun execute(bill: BillEntity): Long
 }
