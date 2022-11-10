25ada2db7e25271cc1fa7fc826c17609ebb2639a
behzoddev <behzoddev@gmail.com>
2022-11-10 21:38:02 +0900

    [feat::] Add DefaultDeleteBill.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/repository/WaterRepository.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/repository/WaterRepository.kt
@@ -6,6 +6,7 @@ import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
 interface WaterRepository {
     suspend fun insertWater(water: WaterEntity): Long
     suspend fun updateWater(water: WaterEntity)
+    suspend fun deleteWater(water: WaterEntity)
     fun fetchWaterAfterTimestamp(timestamp: Long): Flow<List<WaterEntity>>
     fun fetchWaters(): Flow<List<WaterEntity>>
 }
