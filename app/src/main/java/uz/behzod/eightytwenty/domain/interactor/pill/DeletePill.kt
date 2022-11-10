c3b231a878f2b3350b8726a5cfbca7d23b161579
behzoddev <behzoddev@gmail.com>
2022-11-10 21:40:59 +0900

    [refactor::] Update DeletePill.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/pill/DeletePill.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/pill/DeletePill.kt
@@ -1,4 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.pill
 
+import uz.behzod.eightytwenty.data.local.entities.reminder.PillEntity
+
 interface DeletePill {
+    suspend fun execute(pill: PillEntity)
 }
