22e5c1294c7ee6a7d8215f45f7e97ed34d9feefb
behzoddev <behzoddev@gmail.com>
2022-11-10 21:41:04 +0900

    [refactor::] Update DeleteWater.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/DeleteWater.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/DeleteWater.kt
@@ -1,4 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.water
 
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+
 interface DeleteWater {
+    suspend fun deleteWater(water: WaterEntity)
 }
