094d07fc9ae4163649228222f1037eb6fdc9d310
behzoddev <behzoddev@gmail.com>
2022-11-10 21:41:17 +0900

    [refactor::] Update FetchBills.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/bill/FetchBills.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/bill/FetchBills.kt
@@ -1,4 +1,8 @@
 package uz.behzod.eightytwenty.domain.interactor.bill
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.reminder.BillEntity
+
 interface FetchBills {
+    fun execute(): Flow<List<BillEntity>>
 }
