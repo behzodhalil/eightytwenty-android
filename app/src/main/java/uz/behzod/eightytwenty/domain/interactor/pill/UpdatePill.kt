ca606204f1cc45cb795056fe15ffe0fa75fb78c5
behzoddev <behzoddev@gmail.com>
2022-11-10 21:42:45 +0900

    [refactor::] Update UpdatePill.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/pill/UpdatePill.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/pill/UpdatePill.kt
@@ -1,4 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.pill
 
+import uz.behzod.eightytwenty.data.local.entities.reminder.PillEntity
+
 interface UpdatePill {
+    suspend fun execute(pill: PillEntity)
 }
