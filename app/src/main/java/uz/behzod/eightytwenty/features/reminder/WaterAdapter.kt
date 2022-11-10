b84c4f706b92a537fcf76638d373c850969a13dc
behzoddev <behzoddev@gmail.com>
2022-11-10 21:43:03 +0900

    [refactor::] Update WaterAdapter.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/reminder/WaterAdapter.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/reminder/WaterAdapter.kt
@@ -1,4 +1,42 @@
 package uz.behzod.eightytwenty.features.reminder
 
-class WaterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+import uz.behzod.eightytwenty.databinding.ViewHolderWaterItemBinding
+
+class WaterAdapter: ListAdapter<WaterEntity,WaterAdapter.WaterViewHolder>(diffUtil) {
+
+    companion object {
+        private val diffUtil = object : DiffUtil.ItemCallback<WaterEntity>() {
+            override fun areItemsTheSame(oldItem: WaterEntity, newItem: WaterEntity): Boolean {
+                return oldItem.uid == newItem.uid
+            }
+
+            override fun areContentsTheSame(oldItem: WaterEntity, newItem: WaterEntity): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+    inner class WaterViewHolder(
+        val binding: ViewHolderWaterItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(water: WaterEntity) {
+            binding.tvWaterQuantity.text = water.quantity.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WaterViewHolder {
+        val binding = ViewHolderWaterItemBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false
+        )
+        return WaterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: WaterViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
 }
