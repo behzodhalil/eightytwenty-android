16489a727e3190c18bbc2e67c190aaddffa225d2
behzoddev <behzoddev@gmail.com>
2022-11-10 21:41:33 +0900

    [refactor::] Update FetchWaters.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/FetchWaters.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/FetchWaters.kt
@@ -1,4 +1,8 @@
 package uz.behzod.eightytwenty.domain.interactor.water
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+
 interface FetchWaters {
+    fun fetchWaters(): Flow<List<WaterEntity>>
 }
