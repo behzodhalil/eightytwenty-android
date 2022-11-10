6dbb57f68f75d750c07469549829d265605f3a94
behzoddev <behzoddev@gmail.com>
2022-11-10 21:42:50 +0900

    [refactor::] Update UpdateWater.kt

--- app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/UpdateWater.kt
+++ app/src/main/java/uz/behzod/eightytwenty/domain/interactor/water/UpdateWater.kt
@@ -1,4 +1,7 @@
 package uz.behzod.eightytwenty.domain.interactor.water
 
+import uz.behzod.eightytwenty.data.local.entities.reminder.WaterEntity
+
 interface UpdateWater {
+    suspend fun updateWater(water: WaterEntity)
 }
