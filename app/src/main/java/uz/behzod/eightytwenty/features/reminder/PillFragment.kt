0912c7c6d815e3210dbe81c9e63b99e30ce8d955
behzoddev <behzoddev@gmail.com>
2022-11-18 09:33:42 +0900

    [refactor::] Update PillFragment.kt

--- app/src/main/java/uz/behzod/eightytwenty/features/reminder/PillFragment.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/reminder/PillFragment.kt
@@ -1,6 +1,34 @@
 package uz.behzod.eightytwenty.features.reminder
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import uz.behzod.eightytwenty.R
+import uz.behzod.eightytwenty.databinding.FragmentPillBinding
+import uz.behzod.eightytwenty.utils.extension.navigateTo
+import uz.behzod.eightytwenty.utils.view.viewBinding
 
-class PillFragment: Fragment() {
+@AndroidEntryPoint
+class PillFragment : Fragment(R.layout.fragment_pill) {
+
+    private val binding: FragmentPillBinding by viewBinding(FragmentPillBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.btnAddPill.setOnClickListener { route(PillRoute.AddPillRoute) }
+    }
+
+    private fun route(route: PillRoute) {
+        when (route) {
+            PillRoute.AddPillRoute -> {
+                val direction = ReminderFragmentDirections.actionReminderFragmentToAddPillFragment()
+                navigateTo(direction)
+            }
+        }
+    }
 }
