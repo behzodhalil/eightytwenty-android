03bf796aae0d9fff7a8ef5d9a60ac41f7296453f
behzoddev <behzoddev@gmail.com>
2022-11-18 09:37:06 +0900

    [feat::]  Add stringArray extension function for Fragment

--- app/src/main/java/uz/behzod/eightytwenty/utils/extension/FragmentExt.kt
+++ app/src/main/java/uz/behzod/eightytwenty/utils/extension/FragmentExt.kt
@@ -1,10 +1,12 @@
 package uz.behzod.eightytwenty.utils.extension
 
+import androidx.annotation.ArrayRes
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import com.afollestad.materialdialogs.utils.MDUtil.getStringArray
 
 val Fragment.navController: NavController
     get() = this.findNavController()
@@ -30,3 +32,7 @@ fun Fragment.transaction(fragment: DialogFragment) {
 
 val Fragment.supportFragmentManager
     get() = requireActivity().supportFragmentManager
+
+fun Fragment.stringArray(@ArrayRes array: Int): Array<String> {
+    return this.requireContext().getStringArray(array)
+}
