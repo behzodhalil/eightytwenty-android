7b07e14706b211fcd443ba14e766ea9c0b2a5c58
behzoddev <behzoddev@gmail.com>
2022-11-18 09:36:21 +0900

    [refactor::]  Update RepositoryModule

--- app/src/main/java/uz/behzod/eightytwenty/di/RepositoryModule.kt
+++ app/src/main/java/uz/behzod/eightytwenty/di/RepositoryModule.kt
@@ -16,49 +16,49 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun providesNoteRepository(
-        repo: NoteRepositoryImpl
+        repo: NoteRepositoryImpl,
     ): NoteRepository
 
     @Binds
     @Singleton
     fun providesNoteCategoryRepository(
-        repo: NoteCategoryRepositoryImpl
+        repo: NoteCategoryRepositoryImpl,
     ): NoteCategoryRepository
 
     @Binds
     @Singleton
     fun providesHabitRepository(
-        repository: HabitRepositoryImpl
+        repository: HabitRepositoryImpl,
     ): HabitRepository
 
     @Binds
     @Singleton
     fun providesHabitRecommendRepository(
-        repository: HabitRecommendRepositoryImpl
+        repository: HabitRecommendRepositoryImpl,
     ): HabitRecommendRepository
 
     @Binds
     @Singleton
     fun providesScheduleRepository(
-        repository: ScheduleRepositoryImpl
+        repository: ScheduleRepositoryImpl,
     ): ScheduleRepository
 
     @Binds
     @Singleton
     fun providesTaskCatalogRepository(
-        repository: TaskCatalogRepositoryImpl
+        repository: TaskCatalogRepositoryImpl,
     ): TaskCatalogRepository
 
     @Binds
     @Singleton
     fun providesTaskRepository(
-        repository: TaskRepositoryImpl
+        repository: TaskRepositoryImpl,
     ): TaskRepository
 
     @Binds
     @Singleton
     fun providesAttachmentRepository(
-        repository: AttachmentRepositoryImpl
+        repository: AttachmentRepositoryImpl,
     ): AttachmentRepository
 
     @get:Binds
@@ -68,4 +68,17 @@ interface RepositoryModule {
     @get:Binds
     @get:Singleton
     val DefaultLoginRepository.bindLogin: LoginRepository
+
+    @get: Binds
+    @get: Singleton
+    val DefaultPillRepository.bindPillRepository: PillRepository
+
+    @get:Binds
+    @get:Singleton
+    val DefaultBillRepository.bindBillRepository: BillRepository
+
+    @get:Binds
+    @get:Singleton
+    val DefaultWaterRepository.bindWaterRepository: WaterRepository
+
 }
