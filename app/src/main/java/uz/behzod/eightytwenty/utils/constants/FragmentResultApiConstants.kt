4485e1e80b654dfec327808db3f1138b474b893e
behzoddev <behzoddev@gmail.com>
2022-11-24 13:47:02 +0900

    [refactor::] Update FragmentResultApiConstants.kt

--- app/src/main/java/uz/behzod/eightytwenty/utils/constants/FragmentResultApiConstants.kt
+++ app/src/main/java/uz/behzod/eightytwenty/utils/constants/FragmentResultApiConstants.kt
@@ -1,15 +1,30 @@
 package uz.behzod.eightytwenty.utils.constants
 
+/**
+ * This object represents related to Fragment API keys.
+ *
+ * The API keys starts with KEY.
+ * The API argument starts with ARG.
+ */
 object FragmentResultApiConstants {
+    /**
+     * This keys represent related to note.
+     */
     const val KEY_NOTE_TITLE = "key_note_title"
     const val ARG_NOTE_TITLE = "arg_note_title"
+
     const val KEY_NOTE_DESC = "key_note_desc"
     const val ARG_NOTE_DESC = "arg_note_desc"
+
     const val KEY_NOTE_TIMESTAMP = "key_note_timestamp"
     const val ARG_NOTE_TIMESTAMP = "arg_note_timestamp"
 
+    /**
+     * This keys represent related to task.
+     */
     const val KEY_TASK_UID = "key_task_uid"
     const val ARG_TASK_UID = "arg_task_uid"
+
     const val KEY_TASK_NAME = "key_task_name"
     const val ARG_TASK_NAME = "arg_task_name"
 
