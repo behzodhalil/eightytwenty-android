c298d14b56d720f5a376e5c44d347dd393499422
behzoddev <behzoddev@gmail.com>
2022-11-24 13:42:36 +0900

    [refactor::] Update PillState

--- app/src/main/java/uz/behzod/eightytwenty/features/reminder/PillState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/reminder/PillState.kt
@@ -1,5 +1,12 @@
 package uz.behzod.eightytwenty.features.reminder
 
+import uz.behzod.eightytwenty.core.state.ViewState
+import uz.behzod.eightytwenty.data.local.entities.reminder.PillEntity
+
 data class PillState(
-    val isSuccess: Boolean = false
-)
+    val pills: List<PillEntity> = emptyList(),
+    val isSuccess: Boolean = false,
+    val isFailed: Boolean = false,
+    val isEmpty: Boolean = false,
+    val isLoading: Boolean = false
+): ViewState
