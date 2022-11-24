cb19fc64beec0bc120eedb566dde54edd2fe9002
behzoddev <behzoddev@gmail.com>
2022-11-24 13:33:20 +0900

    [refactor::]  Add additional states for AddBillState

--- app/src/main/java/uz/behzod/eightytwenty/features/add_bill/AddBillState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/add_bill/AddBillState.kt
@@ -1,11 +1,28 @@
 package uz.behzod.eightytwenty.features.add_bill
 
 import uz.behzod.eightytwenty.core.state.ViewState
+import uz.behzod.eightytwenty.utils.extension.Empty
+import uz.behzod.eightytwenty.utils.extension.Zero
+import java.time.ZonedDateTime
 
+/**
+ * It represents the state of
+ */
 data class AddBillState(
-    val isSuccess: Boolean = false
+    val name: String = String.Empty,
+    val amount: Long = Long.Zero,
+    val timestamp: ZonedDateTime? = null,
+    val biller: String = String.Empty,
+    val isDatePicked: Boolean = false,
+    val isSuccess: Boolean = false,
+    val isSaved: Boolean = false,
+    val isSaveFailed: Boolean = false
+
 ): ViewState {
     companion object {
+        /**
+         * It represents an empty state of [AddBillState].
+         */
         val empty = AddBillState()
     }
 }
