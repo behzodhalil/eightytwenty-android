e7fdb8c0b03d729976b831570b6e967d828f43f0
behzoddev <behzoddev@gmail.com>
2022-11-24 13:39:26 +0900

    [feat::] Add to represents quantity and image for enum class

---
+++ app/src/main/java/uz/behzod/eightytwenty/utils/constants/Water.kt
@@ -0,0 +1,11 @@
+package uz.behzod.eightytwenty.utils.constants
+
+import uz.behzod.eightytwenty.R
+
+enum class Water(val quantity: Int, val image: Int) {
+    HOT_CUP(200,R.drawable.hot_cup),
+    MUG(400, R.drawable.mug),
+    BOTTLE(600,R.drawable.water_bottle),
+    MINERAL_WATER(800,R.drawable.mineral_water),
+    GLASS(1000,R.drawable.water_glass)
+}
