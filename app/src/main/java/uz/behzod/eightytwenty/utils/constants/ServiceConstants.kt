0a3948d83f4421fb5286903b8909db866156d2a1
behzoddev <behzoddev@gmail.com>
2022-11-24 13:49:34 +0900

    [refactor::] Update service constants

--- app/src/main/java/uz/behzod/eightytwenty/utils/constants/ServiceConstants.kt
+++ app/src/main/java/uz/behzod/eightytwenty/utils/constants/ServiceConstants.kt
@@ -1,9 +1,14 @@
 package uz.behzod.eightytwenty.utils.constants
 
+/**
+ * This object represents related to Service constants.
+ */
 object  ServiceConstants {
     const val SERVICE_ACTION_START = "SERVICE_ACTION_START"
     const val SERVICE_ACTION_CANCEL = "SERVICE_ACTION_CANCEL"
     const val SERVICE_ACTION_WITH_BROADCAST = "SERVICE_ACTION_WITH_BROADCAST"
+
     const val SERVICE_EXTRA_UID = "SERVICE_EXTRA_UID"
+
     const val FILE_ATTACHMENT = "FILE_ATTACHMENT"
 }
