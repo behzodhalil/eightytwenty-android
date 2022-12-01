3d450b572350c26b5cdf5dd37edd8c5236d06863
behzoddev <behzoddev@gmail.com>
2022-12-01 20:54:13 +0900

    update note viewmodel

--- app/src/main/java/uz/behzod/eightytwenty/features/note/NoteWithReduxViewModel.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/note/NoteWithReduxViewModel.kt
@@ -4,55 +4,40 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import uz.behzod.eightytwenty.core.ReduxViewModel
-import uz.behzod.eightytwenty.domain.interactor.note.FetchAllNoteRelation
-import uz.behzod.eightytwenty.domain.interactor.note.FetchNotes
 import uz.behzod.eightytwenty.domain.interactor.note.FetchNotesByCategoryId
-import uz.behzod.eightytwenty.domain.interactor.note_category.FetchAllCategoriesAndNotes
-import uz.behzod.eightytwenty.utils.extension.printDebug
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteWithReduxViewModel @Inject constructor(
-    private val iFetchNotes: FetchNotes,
-    private val iFetchAllNoteRelation: FetchAllNoteRelation,
-    private val iFetchNotesByCategoryId: FetchNotesByCategoryId,
-    private val iFetchCategoryAndNotes: FetchAllCategoriesAndNotes
+    private val iFetchNotesByCategoryId: FetchNotesByCategoryId
 ) : ReduxViewModel<NoteViewState>(initialState = NoteViewState()) {
-
-    fun fetchNotes() {
-        iFetchAllNoteRelation.execute()
-            .onEach { result ->
-                if (result.isNotEmpty()) {
-                    modifyState { state ->
-                        state.copy(
-                            notes = result,
-                            isLoading = false,
-                            isEmpty = false,
-                            onSuccess = true
-                        )
-                    }
-                    printDebug { "Result is $result" }
-                } else {
-                    modifyState { state ->
-                        state.copy(
-                            notes = emptyList(),
-                            isLoading = false,
-                            isEmpty = true
-                        )
-                    }
-                }
-            }.launchIn(viewModelScope)
-
-    }
 
 
     fun fetchNotesByUid() {
         val groupUid = currentState.groupUid
-        printDebug { "Category uid is $groupUid" }
-        iFetchNotesByCategoryId.invoke(groupUid)
-            .onEach {
 
+        iFetchNotesByCategoryId.invoke(groupUid)
+            .onStart { modifyState { state ->
+                state.copy(
+                    isLoading = true
+                )
+            } }
+            .onEach {
+                if (it.isNotEmpty()) {
+                    modifyState { state ->
+                        state.copy(
+                            isLoading = false,
+                            isSuccess = true,
+                            notes = it
+                        )
+                    }
+                } else {
+                    modifyState { state -> state.copy(
+                        isEmpty = true
+                    ) }
+                }
             }
             .launchIn(viewModelScope)
 
