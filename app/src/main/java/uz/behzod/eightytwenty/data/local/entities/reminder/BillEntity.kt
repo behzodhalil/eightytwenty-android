ae8c81436da89ea0ed5c239e6148dcf02c18b9e4
behzoddev <behzoddev@gmail.com>
2022-12-02 17:22:15 +0900

    add bill entity

--- app/src/main/java/uz/behzod/eightytwenty/data/local/entities/reminder/BillEntity.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/reminder/BillEntity.kt
@@ -3,40 +3,42 @@ package uz.behzod.eightytwenty.data.local.entities.reminder
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import uz.behzod.eightytwenty.utils.constants.DatabaseConstants.Bill
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
 import java.time.ZonedDateTime
 
-@Entity(tableName = "bill_table")
+@Entity(tableName = Bill.BILL_TABLE_NAME)
 data class BillEntity(
     /**
      * It represents the amount of bill.
      */
-    @ColumnInfo(name = "bill_amount")
+    @ColumnInfo(name = Bill.BILL_AMOUNT)
     val amount: Long = Long.Zero,
     /**
      * It represents the type of bill.
      */
-    @ColumnInfo(name = "bill_type")
+    @ColumnInfo(name = Bill.BILL_TYPE)
     val type: String = String.Empty,
     /**
      * It represents the date of bill.
      */
-    @ColumnInfo(name = "bill_date")
+    @ColumnInfo(name = Bill.BILL_TIMESTAMP)
     val timestamp: ZonedDateTime? = null,
     /**
      * It represents the duration of bill.
      */
-    @ColumnInfo(name = "bill_duration")
+    @ColumnInfo(name = Bill.BILL_DURATION)
     val duration: Long = Long.Zero,
     /**
      * It represents the name of bill.
      */
+    @ColumnInfo(name = Bill.BILL_NAME)
     val name: String = String.Empty,
     /**
      * It represents the id of bill.
      */
-    @ColumnInfo(name = "bill_uid")
+    @ColumnInfo(name = Bill.BILL_UID)
     @PrimaryKey(autoGenerate = true)
     val uid: Long = Long.Zero
 )
