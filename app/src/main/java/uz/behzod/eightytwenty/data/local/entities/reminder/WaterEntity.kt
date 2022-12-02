a0d461834240027748f25a71dbd7eff9e26b1b6e
behzoddev <behzoddev@gmail.com>
2022-12-02 17:47:12 +0900

    update water entity

--- app/src/main/java/uz/behzod/eightytwenty/data/local/entities/reminder/WaterEntity.kt
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/reminder/WaterEntity.kt
@@ -3,10 +3,12 @@ package uz.behzod.eightytwenty.data.local.entities.reminder
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import uz.behzod.eightytwenty.utils.constants.DatabaseConstants
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
+import java.time.ZonedDateTime
 
-@Entity(tableName = "water_table")
+@Entity(tableName = DatabaseConstants.Water.WATER_TABLE_NAME)
 data class WaterEntity(
     /**
      * It represents the quantity of water.
@@ -18,11 +20,23 @@ data class WaterEntity(
      * It represents the timestamp of water.
      */
     @ColumnInfo(name = "water_timestamp")
-    val timestamp: String = String.Empty,
+    val timestamp: ZonedDateTime? = null,
+
+    /**
+     * It represents the frequency of water.
+     */
+    @ColumnInfo(name = "water_frequency")
+    val frequency: String = String.Empty,
+
+    /**
+     * It represents the reminder of time.
+     */
+    @ColumnInfo(name = "water_reminder_time")
+    val reminderTime: ZonedDateTime? = null,
     /**
      * It represents the id of water.
      */
     @ColumnInfo(name = "water_uid")
     @PrimaryKey(autoGenerate = true)
-    val uid: Long = Long.Zero
+    val uid: Long = Long.Zero,
 )
