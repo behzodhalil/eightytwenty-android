bf5483be0e0f9ade2d15066198a59ec6e00f3df8
behzoddev <behzoddev@gmail.com>
2022-12-02 17:20:15 +0900

    add water state

--- app/src/main/java/uz/behzod/eightytwenty/features/add_water/AddWaterState.kt
+++ app/src/main/java/uz/behzod/eightytwenty/features/add_water/AddWaterState.kt
@@ -3,11 +3,13 @@ package uz.behzod.eightytwenty.features.add_water
 import uz.behzod.eightytwenty.core.state.ViewState
 import uz.behzod.eightytwenty.utils.extension.Empty
 import uz.behzod.eightytwenty.utils.extension.Zero
+import java.time.ZonedDateTime
 
 data class AddWaterState(
-    val quantity: Int = Int.Zero,
-    val image: Int = Int.Zero,
+    val quantity: Long = Long.Zero,
     val frequency: String = String.Empty,
+    val reminderTime: ZonedDateTime? = null,
+    val isDatePicked: Boolean = false,
     val isSaved: Boolean = false,
     val isSaveFailed: Boolean  = false
 ): ViewState
