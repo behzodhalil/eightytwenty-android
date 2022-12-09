383342be4c06ca0500da44d883baabd17841a9ed
behzoddev <behzoddev@gmail.com>
2022-12-09 17:53:31 +0900

    add task recent adapter

---
+++ app/src/main/java/uz/behzod/eightytwenty/features/task/TaskRecentAdapter.kt
@@ -0,0 +1,35 @@
+package uz.behzod.eightytwenty.features.task
+
+import android.view.View
+import android.view.ViewGroup
+import kr.sns.ui_expandable_view.ExpandableItemAdapter
+import kr.sns.ui_expandable_view.ExpandableSelectionView
+
+class TaskRecentAdapter(
+    val title: String
+) : ExpandableItemAdapter{
+
+    override fun inflateHeaderView(parent: ViewGroup): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun inflateItemView(parent: ViewGroup): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun bindItemView(itemView: View, position: Int, selected: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun bindHeaderView(headerView: View, selectedIndices: List<Int>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewStateChanged(headerView: View, state: ExpandableSelectionView.State) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemsCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
