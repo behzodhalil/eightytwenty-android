f9eac1eb5cd1e7b03c27a18785e13da12b20ddd7
Behzod Halil <behzoddev@gmail.com>
2022-12-29 14:00:46 +0900

    add note fts

---
+++ app/src/main/java/uz/behzod/eightytwenty/data/local/entities/NoteFTS.kt
@@ -0,0 +1,20 @@
+package uz.behzod.eightytwenty.data.local.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Fts4
+import uz.behzod.eightytwenty.utils.extension.Empty
+import java.time.ZonedDateTime
+
+@Entity(tableName = "note_fts_table")
+@Fts4(contentEntity = NoteEntity::class)
+data class NoteFTS(
+    @ColumnInfo(name = "note_fts_title")
+    val title: String = String.Empty,
+    @ColumnInfo(name = "note_fts_description")
+    val description: String = String.Empty,
+    @ColumnInfo(name = "note_fts_timestamp")
+    val timestamp: ZonedDateTime = ZonedDateTime.now(),
+    @ColumnInfo(name = "note_fts_is_trashed")
+    val isTrashed: Boolean = false,
+)
