5fa13fc629db0bd2bf7b88113ebf68c75600829d
Behzod Halil <behzoddev@gmail.com>
2023-04-20 18:23:20 +0900

    Add Fonts

---
+++ core/features/src/main/java/io/behzod/features/Fonts.kt
@@ -0,0 +1,14 @@
+package io.behzod.features
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium, FontStyle.Normal)
+)
+
+
