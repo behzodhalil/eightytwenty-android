a4e379995d0a270c092bed61bbff413b8112bccd
Behzod Halil <behzoddev@gmail.com>
2023-04-20 18:23:10 +0900

    Add Indicator

---
+++ features/onboarding/src/main/java/io/behzod/onboarding/Indicator.kt
@@ -0,0 +1,33 @@
+package io.behzod.onboarding
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Indicator(isSelected: Boolean) {
+    val widthAnimation = animateDpAsState(
+        targetValue = if (isSelected) 25.dp else 10.dp,
+        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy)
+    )
+
+    Box(modifier = Modifier
+        .height(10.dp)
+        .width(widthAnimation.value)
+        .clip(CircleShape)
+        .background(
+            color = if (isSelected) MaterialTheme.colors.primary else Color(0XFFF8E2E7)
+        )
+    )
+}
