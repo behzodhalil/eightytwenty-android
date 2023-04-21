5b68bf783005f0fedba82657db66e677e552df2b
Behzod Halil <behzoddev@gmail.com>
2023-04-21 11:51:18 +0900

    Add Authentication title

---
+++ features/auth/ui/src/main/java/io/behzod/ui/AuthenticationTitle.kt
@@ -0,0 +1,31 @@
+package io.behzod.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import io.behzod.features.Roboto
+
+@Composable
+fun AuthenticationTitle(
+    modifier: Modifier = Modifier,
+    authenticationMode: AuthenticationMode,
+) {
+    Text(
+        text = stringResource(
+            id =
+            if (authenticationMode == AuthenticationMode.SIGN_IN) {
+                io.behzod.features.R.string.sign_in_to_account
+            } else {
+                io.behzod.features.R.string.sign_up_to_account
+            }
+        ),
+        color = colorResource(id = io.behzod.features.R.color.black),
+        fontSize = 20.sp,
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Bold
+    )
+}
