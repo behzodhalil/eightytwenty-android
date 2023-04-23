4f3287cb63d28d8da203ae9695e6a123779d5650
behzoddev <behzoddev@gmail.com>
2023-04-24 06:41:17 +0900

    Add AuthenticationForm.kt

--- features/auth/ui/src/main/java/io/behzod/ui/AuthenticationForm.kt
+++ features/auth/ui/src/main/java/io/behzod/ui/AuthenticationForm.kt
@@ -1,6 +1,9 @@
 package io.behzod.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -8,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -18,9 +22,14 @@ fun AuthenticationForm(
     email: String?,
     name: String?,
     password: String?,
+    enabledAuthentication: Boolean,
+    completedPasswordRequirements: List<PasswordRequirements>,
     onEmailChanged: (email: String) -> Unit,
     onNameChanged: (name: String) -> Unit,
     onPasswordChanged: (password: String) -> Unit,
+    onAuthenticate: () -> Unit,
+    onNextClick: () -> Unit,
+    onToggleMode: () -> Unit,
 ) {
     Column(
         modifier = modifier
@@ -38,53 +47,73 @@ fun AuthenticationForm(
         val focusRequest = FocusRequester()
 
         Spacer(modifier = modifier.height(16.dp))
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 24.dp)
+        Column(
+            horizontalAlignment =
+            Alignment.CenterHorizontally
         ) {
-            Column(
-                modifier = Modifier.padding(16.dp),
-                horizontalAlignment =
-                Alignment.CenterHorizontally
+            EmailInput(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequest),
+                email = email,
+                onEmailChanged = onEmailChanged
             ) {
-                EmailInput(
+                focusRequest.requestFocus()
+            }
+
+            if (authenticationMode == AuthenticationMode.SIGN_UP) {
+                Spacer(modifier = modifier.height(16.dp))
+                NameInput(
                     modifier = modifier
                         .fillMaxWidth()
                         .focusRequester(focusRequest),
-                    email = email,
-                    onEmailChanged = onEmailChanged
-                ) {
-                    focusRequest.requestFocus()
-                }
-
-                if (authenticationMode == AuthenticationMode.SIGN_UP) {
-                    Spacer(modifier = modifier.height(16.dp))
-                    NameInput(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .focusRequester(focusRequest),
-                        name = name,
-                        onNameChanged = { name ->
-                            onNameChanged(name)
-                        }) {
-                        focusRequest.requestFocus()
-                    }
-                }
-
-                Spacer(modifier = modifier.height(16.dp))
-                PasswordInput(
-                    modifier = modifier
-                        .focusRequester(focusRequest),
-                    password = password,
-                    onPasswordChanged = { password ->
-                        onPasswordChanged(password)
+                    name = name,
+                    onNameChanged = { name ->
+                        onNameChanged(name)
                     }) {
                     focusRequest.requestFocus()
                 }
             }
 
+            Spacer(modifier = modifier.height(16.dp))
+            PasswordInput(
+                modifier = modifier
+                    .focusRequester(focusRequest),
+                password = password,
+                onPasswordChanged = { password ->
+                    onPasswordChanged(password)
+                }) {
+                onAuthenticate()
+            }
+
+            Spacer(modifier = modifier.height(16.dp))
+
+            AnimatedVisibility(visible = authenticationMode == AuthenticationMode.SIGN_UP) {
+                PasswordRequirement(
+                    modifier = modifier.fillMaxWidth(),
+                    requirements = completedPasswordRequirements)
+            }
+
+            Spacer(modifier = modifier.height(56.dp))
+
+            AuthenticationButton(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(75.dp)
+                    .padding(12.dp),
+                enableAuthentication = enabledAuthentication
+            ) {
+                onNextClick()
+            }
         }
+        Spacer(modifier = Modifier.weight(1f))
+
+        if (authenticationMode == AuthenticationMode.SIGN_IN) {
+            ToggleAuthenticationMode() {
+                onToggleMode()
+            }
+        }
+        Spacer(modifier = modifier.height(56.dp))
 
     }
 }
@@ -98,9 +127,14 @@ fun AuthenticationSignInFormPreview() {
             email = "behzoddev@gmail.com",
             name = "",
             password = "",
+            enabledAuthentication = false,
             onEmailChanged = {},
             onNameChanged = {},
-            onPasswordChanged = {}
+            onPasswordChanged = {},
+            onAuthenticate = {},
+            completedPasswordRequirements = emptyList(),
+            onNextClick = {},
+            onToggleMode = {}
         )
     }
 
@@ -115,9 +149,16 @@ fun AuthenticationSignUpFormPreview() {
             email = "behzoddev@gmail.com",
             name = "",
             password = "",
+            enabledAuthentication = false,
             onEmailChanged = {},
             onNameChanged = {},
-            onPasswordChanged = {}
+            onPasswordChanged = {},
+            onAuthenticate = {},
+            completedPasswordRequirements = listOf(PasswordRequirements.CAPITAL_LETTER,
+                PasswordRequirements.NUMBER,
+                PasswordRequirements.EIGHT_CHARACTERS),
+            onNextClick = {},
+            onToggleMode = {}
         )
     }
 
